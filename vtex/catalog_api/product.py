f642bd5bd3f9b7d0377b105269a9b58e69ae1b29
BredgaryV <bred.valenzuela@gmail.com>
2021-09-18 17:06:23 -0300

    MOD: prueba

--- vtex/catalog_api/product.py
+++ vtex/catalog_api/product.py
@@ -1,16 +1,20 @@
+import requests
+import json
+import os
+import re
+from datetime import datetime
 from google.cloud import bigquery
-
 
 client = bigquery.Client()
 
 # Perform a query.
 QUERY = (
-    'SELECT id, count(id) as total FROM `shopstar-datalake.landing_zone.shopstar_vtex_category` ')
+    'SELECT id FROM `shopstar-datalake.landing_zone.shopstar_vtex_category` ')
 
 query_job = client.query(QUERY)  # API request
 rows = query_job.result()  # Waits for query to finish
 
-#print("total={}".format(row["total"]))
+#print("total={}".format(row["count(id)"]))
 
 #for row in rows:
 #    string =  json.dumps(row.id)
