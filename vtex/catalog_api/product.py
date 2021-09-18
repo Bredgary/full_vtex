ea3c8f831487d53b5211fc323b7b168619572341
BredgaryV <bred.valenzuela@gmail.com>
2021-09-18 17:03:02 -0300

    ADD: product.py

--- vtex/catalog_api/product.py
+++ vtex/catalog_api/product.py
@@ -5,11 +5,16 @@ client = bigquery.Client()
 
 # Perform a query.
 QUERY = (
-    'SELECT id FROM `shopstar-datalake.landing_zone.shopstar_vtex_category` '
-    'LIMIT 5')
+    'SELECT id, count(id) as total FROM `shopstar-datalake.landing_zone.shopstar_vtex_category` ')
 
 query_job = client.query(QUERY)  # API request
 rows = query_job.result()  # Waits for query to finish
 
+print("total={}".format(row["total"]))
+
 for row in rows:
-    print(row.id)
+    string =  json.dumps(row.id)
+    system("touch /home/bred_valenzuela/full_vtex/vtex/catalog_api/idsProducts.json")
+    text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/idsProducts.json", "w")
+    text_file.write(string)
+    text_file.close()
