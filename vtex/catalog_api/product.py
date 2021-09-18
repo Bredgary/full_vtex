80cd9360caae5e759a44b48b0b42988ba0ebfeb5
BredgaryV <bred.valenzuela@gmail.com>
2021-09-18 20:06:59 -0300

    MOD: prueba

--- vtex/catalog_api/product.py
+++ vtex/catalog_api/product.py
@@ -8,8 +8,7 @@ from google.cloud import bigquery
 
 url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/GetProductAndSkuIds"
 headers = {"Content-Type": "application/json",    "Accept": "application/json",    "X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA",    "X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-querystring = {"categoryId":"1","_from":"1","_to":"10"}
-response = requests.request("GET", url, headers=headers, params=querystring)
+formatoJson = []
 listIdCategory = []
 client = bigquery.Client()
 cantidad = 0
@@ -37,9 +36,12 @@ idsCategory=open("idsProducts.json","r")
 idsCategory.read()
 system("rm idsProducts.json")
 
-for x in idsCategory:
-    ids = str(x)
-    querystring = {"categoryId":ids,"_from":"1","_to":"10"}
-    response = requests.request("GET", url, headers=headers, params=querystring)
+for i in range[2]
+    for x in idsCategory:
+        ids = str(x)
+        querystring = {"categoryId":"1","_from":"1","_to":"10"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        formatoJson.append(response.text)
+
     
-print(response.text)
+print(formatoJson)
