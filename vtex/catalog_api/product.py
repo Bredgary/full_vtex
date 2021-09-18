fbf5ef83dd6707f5fae8f353d1ef7de1108e3649
BredgaryV <bred.valenzuela@gmail.com>
2021-09-18 20:17:14 -0300

    MOD: prueba

--- vtex/catalog_api/product.py
+++ vtex/catalog_api/product.py
@@ -8,6 +8,13 @@ from google.cloud import bigquery
 
 url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/GetProductAndSkuIds"
 headers = {"Content-Type": "application/json",    "Accept": "application/json",    "X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA",    "X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+response2 = requests.request("GET", url, headers=headers, params=querystring)
+formJson = json.loads(response2.text)
+total = formJson["total"]
+tableDetails = []
+
+
+
 formatoJson = []
 listIdCategory = []
 client = bigquery.Client()
@@ -39,7 +46,8 @@ system("rm idsProducts.json")
 for i in range(2):
     for x in idsCategory:
         ids = str(x)
-        querystring = {"categoryId":"1","_from":"1","_to":"10"}
+        print("Paginas totales: "+str(total))
+        querystring = {"categoryId":"441","_from":"1","_to":"10"}
         response = requests.request("GET", url, headers=headers, params=querystring)
         formatoJson.append(response.text)
 
