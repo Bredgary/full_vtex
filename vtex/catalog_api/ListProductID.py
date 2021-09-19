90de8468ce4131efd79df388d59b03aa3356b21a
BredgaryV <bred.valenzuela@gmail.com>
2021-09-19 15:20:43 -0300

    MOD: prueba

--- vtex/catalog_api/ListProductID.py
+++ vtex/catalog_api/ListProductID.py
@@ -59,12 +59,13 @@ query_job = client.query(QUERY)  # API request
 rows = query_job.result()  # Waits for query to finish
 
 for row in rows:
-    get_productIFD(row.id)
+    #get_productIFD(row.id)
+    print(row.id)
 
-for x in productList[0]:
-    cargandoIdProducto.appened(x)
+#for x in productList[0]:
+#    cargandoIdProducto.appened(x)
 
-print(cargandoIdProducto)
+
 
 
 
