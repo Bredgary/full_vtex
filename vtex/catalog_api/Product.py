971f0fcb22516f6f6a789f137872aeb1ee0b03f1
BredgaryV <bred.valenzuela@gmail.com>
2021-09-19 18:49:58 -0300

    MOD: prueba

--- vtex/catalog_api/Product.py
+++ vtex/catalog_api/Product.py
@@ -39,6 +39,9 @@ def get_product(id):
     response = requests.request("GET", url, headers=headers)
     jsonF = json.loads(response.text)
     cargaProduct.append(jsonF)
+    for order in cargaProduct:
+        for k, v in order.items():
+            order[k] = replace_blank_dict(v)
     return cargaProduct
 
 
@@ -70,36 +73,35 @@ for row in rows:
 
 for ids in productoID:
     get_product(ids)
+    print(cargaProduct)
 
-for order in cargaProduct:
-        for k, v in order.items():
-            order[k] = replace_blank_dict(v)
 
-string = json.dumps(cargaProduct)
-text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista.json", "w")
-text_file.write(string)
-text_file.close() 
 
-system("cat lista.json | jq -c '.[]' > Product.json")
+#tring = json.dumps(cargaProduct)
+#text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista.json", "w")
+#text_file.write(string)
+#text_file.close() 
 
-print("Cargando a BigQuery")
-client = bigquery.Client()
-filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/Product.json'
-dataset_id = 'landing_zone'
-table_id = 'shopstar_vtex_product'
-dataset_ref = client.dataset(dataset_id)
-table_ref = dataset_ref.table(table_id)
-job_config = bigquery.LoadJobConfig()
-job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-job_config.autodetect = True
-with open(filename, "rb") as source_file:
-    job = client.load_table_from_file(
-        source_file,
-        table_ref,
-        location="southamerica-east1",  # Must match the destination dataset location.
-    job_config=job_config,)  # API request
-job.result()  # Waits for table load to complete.
-print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-system("rm lista.json")
-system("rm Product.json")
+#system("cat lista.json | jq -c '.[]' > Product.json")
+
+#print("Cargando a BigQuery")
+#client = bigquery.Client()
+#filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/Product.json'
+#dataset_id = 'landing_zone'
+#table_id = 'shopstar_vtex_product'
+#dataset_ref = client.dataset(dataset_id)
+#table_ref = dataset_ref.table(table_id)
+#job_config = bigquery.LoadJobConfig()
+#job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+#job_config.autodetect = True
+#with open(filename, "rb") as source_file:
+#    job = client.load_table_from_file(
+#        source_file,
+#        table_ref,
+#        location="southamerica-east1",  # Must match the destination dataset location.
+#    job_config=job_config,)  # API request
+#job.result()  # Waits for table load to complete.
+#print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+#system("rm lista.json")
+3#system("rm Product.json")
 print("finalizado")
