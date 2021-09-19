7bd6b5c61349d87bdc50238187cc090f5a906bcd
BredgaryV <bred.valenzuela@gmail.com>
2021-09-19 15:13:29 -0300

    MOD: prueba

--- vtex/catalog_api/ListProductID.py
+++ vtex/catalog_api/ListProductID.py
@@ -11,6 +11,7 @@ client = bigquery.Client()
 listIdCategory = []
 productF = []
 productList = []
+cargandoIdProducto = []
 
 def replace_blank_dict(d):
     if not d:
@@ -35,11 +36,7 @@ def get_product(id):
         }
     response = requests.request("GET", url, headers=headers)
     jsonF = json.loads(response.text)  
-    productF.append(jsonF)
-    for order in OrderF:
-        for k, v in order.items():
-            order[k] = replace_blank_dict(v)
-    return productF
+    return jsonF
 
 def get_productIFD(id):
     url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/GetProductAndSkuIds"
@@ -51,8 +48,9 @@ def get_productIFD(id):
         "X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"
         }
     response = requests.request("GET", url, headers=headers, params=querystring)
-    jsonF = json.loads(response.text)  
-    productList.append(jsonF)
+    jsonF = json.loads(response.text)
+    data = jsonF["data"]
+    productList.append(data)
     return productList
 
 QUERY = (
@@ -61,36 +59,48 @@ query_job = client.query(QUERY)  # API request
 rows = query_job.result()  # Waits for query to finish
 
 for row in rows:
-    listIdCategory.append(row.id)
+    get_productIFD(row.id)
 
-for i in listIdCategory:
-    get_productIFD(i)
+for x in productList[0]:
+    cargandoIdProducto.appened(x)
 
-
-tableProduct =  json.dumps(productF)
-text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/temp.json", "w")
-text_file.write(tableProduct)
-text_file.close() 
-system("cat temp.json | jq -c '.[]' > Product.json")
+print(cargandoIdProducto)
 
 
-client = bigquery.Client()
-filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/Product.json'
-dataset_id = 'landing_zone'
-table_id = 'shopstar_vtex_product'
-dataset_ref = client.dataset(dataset_id)
-table_ref = dataset_ref.table(table_id)
-job_config = bigquery.LoadJobConfig()
-job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-job_config.autodetect = True
-with open(filename, "rb") as source_file:
-    job = client.load_table_from_file(
-        source_file,
-        table_ref,
-        location="southamerica-east1",  # Must match the destination dataset location.
-    job_config=job_config,)  # API request
-job.result()  # Waits for table load to complete.
-print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-system("rm DetailOrdersFinal.json")
-system("rm temp.json")
-print("finalizado")
+
+
+#for i in listIdCategory:
+#    productF.append(jsonF)
+#    for order in OrderF:
+#        for k, v in order.items():
+#            order[k] = replace_blank_dict(v)
+#    get_productIFD(i)
+#
+
+#tableProduct =  json.dumps(productF)
+#text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/temp.json", "w")
+#text_file.write(tableProduct)
+#text_file.close() 
+#system("cat temp.json | jq -c '.[]' > Product.json")
+
+
+#client = bigquery.Client()
+#filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/Product.json'
+#dataset_id = 'landing_zone'
+#table_id = 'shopstar_vtex_product'
+#dataset_ref = client.dataset(dataset_id)
+#table_ref = dataset_ref.table(table_id)
+#job_config = bigquery.LoadJobConfig()
+#job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+#job_config.autodetect = True
+#with open(filename, "rb") as source_file:
+#    job = client.load_table_from_file(
+#        source_file,
+#        table_ref,
+#        location="southamerica-east1",  # Must match the destination dataset location.
+#    job_config=job_config,)  # API request
+#job.result()  # Waits for table load to complete.
+#print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+#system("rm DetailOrdersFinal.json")
+#system("rm temp.json")
+#print("finalizado")
