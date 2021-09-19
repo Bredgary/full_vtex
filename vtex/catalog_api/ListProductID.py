0f421c1dbed4c64e0e4de56a1b65721849433678
BredgaryV <bred.valenzuela@gmail.com>
2021-09-19 02:16:03 -0300

    MOD: prueba

--- vtex/catalog_api/ListProductID.py
+++ vtex/catalog_api/ListProductID.py
@@ -43,21 +43,30 @@ def get_product(id):
       productList.append(i)
     return productList
 
-#def total(id):
-#    url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/GetProductAndSkuIds"
-#    querystring = {"categoryId":""+str(id)+"","_from":"1","_to":"10"}
-#    headers = {
-#        "Content-Type": "application/json",
-#        "Accept": "application/json",
-#        "X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA",
-#        "X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"
-#    }
-#    response = requests.request("GET", url, headers=headers, params=querystring)
-#    data = response.text.encode('utf8')
-#    formatoJson = json.loads(response.text)
-#    range = formatoJson["range"]
-#    total = range["total"]
-#    return total
+def replace_blank_dict(d):
+    if not d:
+        return None
+    if type(d) is list:
+        for list_item in d:
+            if type(list_item) is dict:
+                for k, v in list_item.items():
+                    list_item[k] = replace_blank_dict(v)
+    if type(d) is dict:
+        for k, v in d.items():
+            d[k] = replace_blank_dict(v)
+    return d
+
+def get_product(id):
+	url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/product/441"
+	headers = {
+    	"Content-Type": "application/json",
+    	"Accept": "application/json",
+    	"X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA",
+    	"X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"
+	}
+	response = requests.request("GET", url, headers=headers)
+    data = response.text.encode('utf8')
+    return data
 
 
 formJson = []
