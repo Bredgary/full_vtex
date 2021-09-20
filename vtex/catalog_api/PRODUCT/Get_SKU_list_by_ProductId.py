db322cf0774dd3c2afdf39cced0506472bb1ca0e
BredgaryV <bred.valenzuela@gmail.com>
2021-09-20 19:51:00 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_SKU_list_by_ProductId.py
+++ vtex/catalog_api/PRODUCT/Get_SKU_list_by_ProductId.py
@@ -7,7 +7,7 @@ from os import system
 from google.cloud import bigquery
 
 client = bigquery.Client()
-'''
+
 productList = [] 
 temp = ""
 headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
@@ -30,27 +30,28 @@ for row in rows:
     break
 
 
+cargando = json.loads(productList.content)
+
+print(type(cargando))
+'''
 string = json.dumps(productList)
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista1.json", "w")
 text_file.write(string)
 text_file.close() 
 system("cat lista.json | jq -c '.[]' > lista.json")
 
-'''
+
+
 print("Cargando a BigQuery")
 client = bigquery.Client()
-filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista.json'
+filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/context.json'
 dataset_id = 'landing_zone'
-table_id = 'shopstar_vtex_list_sku'
+table_id = 'shopstar_vtex_sku_list_by_productid'
 dataset_ref = client.dataset(dataset_id)
 table_ref = dataset_ref.table(table_id)
-job_config = bigquery.LoadJobConfig(
-    schema=[
-        bigquery.SchemaField("Id", "INTEGER"),
-        bigquery.SchemaField("ProductId", "INTEGER"),
-    ],
-    source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
-)
+job_config = bigquery.LoadJobConfig()
+job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+job_config.autodetect = True
 with open(filename, "rb") as source_file:
     job = client.load_table_from_file(
         source_file,
@@ -62,3 +63,4 @@ print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id)
 system("rm lista.json")
 system("rm context.json")
 print("finalizado")
+'''
