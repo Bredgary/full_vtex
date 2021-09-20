ff6dfb5704a358bb3f2aecc75eee9c6f1552ea81
BredgaryV <bred.valenzuela@gmail.com>
2021-09-20 07:11:25 -0300

    MOD: prueba

--- vtex/catalog_api/GetSKUlistbyProductId.py
+++ vtex/catalog_api/GetSKUlistbyProductId.py
@@ -28,6 +28,24 @@ for row in rows:
     get_sku_list(str(row.id),headers)
     break
 
+def replace_blank_dict(d):
+    if not d:
+        return None
+    if type(d) is list:
+        for list_item in d:
+            if type(list_item) is dict:
+                for k, v in list_item.items():
+                    list_item[k] = replace_blank_dict(v)
+    if type(d) is dict:
+        for k, v in d.items():
+            d[k] = replace_blank_dict(v)
+    return d
+
+for order in productList:
+    for k, v in order.items():
+        order[k] = replace_blank_dict(v)
+
+
 string = json.dumps(productList)
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista.json", "w")
 text_file.write(string)
