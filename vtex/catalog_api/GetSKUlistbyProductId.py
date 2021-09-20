79ebe15ecd7e6114254fe986d6a1760be51730b9
BredgaryV <bred.valenzuela@gmail.com>
2021-09-20 08:29:31 -0300

    MOD: Prueba

--- vtex/catalog_api/GetSKUlistbyProductId.py
+++ vtex/catalog_api/GetSKUlistbyProductId.py
@@ -34,6 +34,14 @@ text_file.write(string)
 text_file.close() 
 system("cat lista1.json | jq -c '.[]' > lista.json")
 
+
+print("Cargando a BigQuery")
+client = bigquery.Client()
+filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista.json'
+dataset_id = 'landing_zone'
+table_id = 'shopstar_vtex_list_sku'
+dataset_ref = client.dataset(dataset_id)
+table_ref = dataset_ref.table(table_id)
 job_config = bigquery.LoadJobConfig(
     schema=[
         bigquery.SchemaField("Id", "INTEGER"),
@@ -41,36 +49,6 @@ job_config = bigquery.LoadJobConfig(
     ],
     source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
 )
-uri = "/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista.json"
-table_id = "`shopstar-datalake.landing_zone.shopstar_vtex_listsku`"
-
-load_job = client.load_table_from_uri(
-    uri,
-    table_id,
-    location="US",  # Must match the destination dataset location.
-    job_config=job_config,
-)  # Make an API request.
-
-load_job.result()  # Waits for the job to complete.
-
-destination_table = client.get_table(table_id)
-print("Loaded {} rows.".format(destination_table.num_rows))
-'''
-system("cat lista.json | jq -c '.[]' > lista1.json")
-system("sed 's/ //g' lista1.json > lista2.json")
-system("cat lista2.json | tr '\n' ' ' > context.json")
-
-
-print("Cargando a BigQuery")
-client = bigquery.Client()
-filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/context.json'
-dataset_id = 'landing_zone'
-table_id = 'shopstar_vtex_list_sku'
-dataset_ref = client.dataset(dataset_id)
-table_ref = dataset_ref.table(table_id)
-job_config = bigquery.LoadJobConfig()
-job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-job_config.autodetect = True
 with open(filename, "rb") as source_file:
     job = client.load_table_from_file(
         source_file,
