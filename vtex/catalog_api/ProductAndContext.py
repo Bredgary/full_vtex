51dd5051304d017b21ac873757b3e6bcbc73a849
BredgaryV <bred.valenzuela@gmail.com>
2021-09-20 00:15:40 -0300

    MOD: Prueba

--- vtex/catalog_api/ProductAndContext.py
+++ vtex/catalog_api/ProductAndContext.py
@@ -13,6 +13,7 @@ def get_contex(id):
     url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/ProductGet/"""+str(id)+""
     headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
     response = requests.request("GET", url, headers=headers)
+    jsonF = json.loads(response.text)
     return jsonF
 
 def replace_blank_dict(d):
