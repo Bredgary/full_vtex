a5d99a873c37272f5fbc62e56e86458c4504d790
BredgaryV <bred.valenzuela@gmail.com>
2021-09-20 07:47:31 -0300

    MOD: prueba

--- vtex/catalog_api/GetSKUlistbyProductId.py
+++ vtex/catalog_api/GetSKUlistbyProductId.py
@@ -28,19 +28,31 @@ for row in rows:
     get_sku_list(str(row.id),headers)
 
 
-string = productList
-text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista.json", "wb")
+string = json.dumps(productList)
+text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista.json", "w")
 text_file.write(string)
 text_file.close() 
 
-# Opening JSON file
-f = open('lista.json',)
- 
-# returns JSON object as
-# a dictionary
-data = json.load(f)
+job_config = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("Id", "INTEGER"),
+        bigquery.SchemaField("ProductId", "INTEGER"),
+    ],
+    source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+)
+uri = "/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista.json"
 
-print(type(data))
+load_job = client.load_table_from_uri(
+    uri,
+    table_id,
+    location="US",  # Must match the destination dataset location.
+    job_config=job_config,
+)  # Make an API request.
+
+load_job.result()  # Waits for the job to complete.
+
+destination_table = client.get_table(table_id)
+print("Loaded {} rows.".format(destination_table.num_rows))
 '''
 system("cat lista.json | jq -c '.[]' > lista1.json")
 system("sed 's/ //g' lista1.json > lista2.json")
