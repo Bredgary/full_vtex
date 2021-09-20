4b0bd7878d2be6b9bb0b8c8f7dd1847134719aa7
BredgaryV <bred.valenzuela@gmail.com>
2021-09-20 20:02:01 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_SKU_list_by_ProductId.py
+++ vtex/catalog_api/PRODUCT/Get_SKU_list_by_ProductId.py
@@ -12,11 +12,26 @@ productList = []
 temp = ""
 headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 
+def replace_blank_dict(d):
+    if not d:
+        return None
+    if type(d) is list:
+        for list_item in d:
+            if type(list_item) is dict:
+                for k, v in list_item.items():
+                    list_item[k] = replace_blank_dict(v)
+    if type(d) is dict:
+        for k, v in d.items():
+            d[k] = replace_blank_dict(v)
+    return d
 
 def get_sku_list(id,headers):
     url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitByProductId/"""+str(id)+""
     response = requests.request("GET", url, headers=headers)
     formatoJson = json.loads(response.content)
+    for order in productList:
+        for k, v in order.items():
+            order[k] = replace_blank_dict(v)
     productList.append(formatoJson)
 
     
@@ -31,9 +46,7 @@ for row in rows:
     break
 
 
-
-print(type(formatoJson))
-print(type(productList))
+print("Fin")
 '''
 string = json.dumps(productList)
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista1.json", "w")
