9b4bd391c0b0c9a41b1eaf2a76dd8d7de277ea1c
BredgaryV <bred.valenzuela@gmail.com>
2021-09-20 02:40:24 -0300

    ADD: ProductReview

---
+++ vtex/catalog_api/GetProductReviewRatebyProductID.py
@@ -0,0 +1,81 @@
+import requests
+import json
+import os
+import re
+from datetime import datetime
+from os import system
+from google.cloud import bigquery
+
+client = bigquery.Client()
+productList = [] 
+temp = ""
+
+def get_review(id):
+    url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/1243/specification"
+
+	headers = {
+		"Content-Type": "application/json",
+		"Accept": "application/json",
+		"X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA",
+		"X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"
+	}
+
+	response = requests.request("GET", url, headers=headers)
+    jsonF = json.loads(response.text)
+    return jsonF
+
+
+def replace_blank_dict(d):
+    if not d:
+        return None
+    if type(d) is list:
+        for list_item in d:
+            if type(list_item) is dict:
+                for k, v in list_item.items():
+                    list_item[k] = replace_blank_dict(v)
+    if type(d) is dict:
+        for k, v in d.items():
+            d[k] = replace_blank_dict(v)
+    return d
+
+QUERY = (
+    'SELECT RefId FROM `shopstar-datalake.landing_zone.shopstar_vtex_product` ')
+query_job = client.query(QUERY)  # API request
+rows = query_job.result()  # Waits for query to finish
+
+for row in rows:
+    temp = get_review(str(row.id))
+    productList.append(temp)
+
+for order in productList:
+    for k, v in order.items():
+        order[k] = replace_blank_dict(v)
+
+string = json.dumps(productList)
+text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/lista.json", "w")
+text_file.write(string)
+text_file.close() 
+
+system("cat lista.json | jq -c '.[]' > context.json")
+
+print("Cargando a BigQuery")
+client = bigquery.Client()
+filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/context.json'
+dataset_id = 'landing_zone'
+table_id = 'shopstar_product_Specification'
+dataset_ref = client.dataset(dataset_id)
+table_ref = dataset_ref.table(table_id)
+job_config = bigquery.LoadJobConfig()
+job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+job_config.autodetect = True
+with open(filename, "rb") as source_file:
+    job = client.load_table_from_file(
+        source_file,
+        table_ref,
+        location="southamerica-east1",  # Must match the destination dataset location.
+    job_config=job_config,)  # API request
+job.result()  # Waits for table load to complete.
+print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+system("rm context.json")
+system("rm lista.json")
+print("finalizado")
