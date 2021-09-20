22fd14c2f19df0fb1cf7434481e37a463ebfa153
BredgaryV <bred.valenzuela@gmail.com>
2021-09-19 22:24:26 -0300

    MOD: prueba

--- vtex/catalog_api/Product.py
+++ vtex/catalog_api/Product.py
@@ -66,9 +66,9 @@ rows = query_job.result()  # Waits for query to finish
 
 for row in rows:
     temp = get_productIFD(str(row.id))
-    load.append(temp)
+    #load.append(temp)
 
-string = json.dumps(load)
+string = json.dumps(str(temp))
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/load","wb")
 text_file.write(string)
 text_file.close()   
