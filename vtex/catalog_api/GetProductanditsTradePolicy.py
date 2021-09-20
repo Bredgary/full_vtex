fa2218eb8c8ff7958e340ebe8c9c927cd192d3eb
BredgaryV <bred.valenzuela@gmail.com>
2021-09-20 00:35:24 -0300

    MOD: prueba

--- vtex/catalog_api/GetProductanditsTradePolicy.py
+++ vtex/catalog_api/GetProductanditsTradePolicy.py
@@ -11,7 +11,12 @@ productList = []
 
 def get_policy(id):
     url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/productget/"""+str(id)+""
-	headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA",
+        "X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"
+        }
     response = requests.request("GET", url, headers=headers)
     jsonF = json.loads(response.text)
     return jsonF
