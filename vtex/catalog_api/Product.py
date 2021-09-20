bf731f7727f346462ac5ddefddf767c4c502e690
BredgaryV <bred.valenzuela@gmail.com>
2021-09-19 22:30:36 -0300

    MOD: prueba

--- vtex/catalog_api/Product.py
+++ vtex/catalog_api/Product.py
@@ -68,9 +68,10 @@ for row in rows:
     temp = get_productIFD(str(row.id))
     load.append(temp)
 
-string = json.dumps(str(load))
+with open(load, 'rb') as f:
+    lines = [x.decode('utf8').strip() for x in f.readlines()]
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/load","wb")
-text_file.write(string)
+text_file.write(lines)
 text_file.close()   
 
 print("Fin")
