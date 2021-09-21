02af5d95f2e44cfa2507a9deb0b245523466f0aa
BredgaryV <bred.valenzuela@gmail.com>
2021-09-21 13:54:33 -0300

    MOD: Prueba

--- vtex/catalog_api/PRODUCT/Get_SKU_list_by_ProductId.py
+++ vtex/catalog_api/PRODUCT/Get_SKU_list_by_ProductId.py
@@ -32,7 +32,7 @@ def replace_blank_dict(d):
 
 def get_sku_list(id,headers):
     url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitByProductId/"""+str(id)+""
-    response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", url, headers=headers) 
     formatoJson = json.loads(response.text)
     for order in formatoJson:
         for k, v in order.items():
@@ -45,16 +45,16 @@ query_job = client.query(QUERY)  # API request
 rows = query_job.result()  # Waits for query to finish
 
 for row in rows:
-    temp.append(get_sku_list(str(row.id),headers))
+    temp.append(get_sku_list(str(row),headers))
     contador = contador+1
     if contador == 5:
         break
 
-def listToStringWithoutBrackets(temp):
+def listToStringWithoutBrackets(list1):
     return str(list1).replace('[','').replace(']','') 
+
 temp2 = listToStringWithoutBrackets(temp)
 
-print(temp2)
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/context.json", "w")
 text_file.write(temp2)
 text_file.close() 
