0de59bcf452363b3779e6debd90215bdd62dbc61
BredgaryV <bred.valenzuela@gmail.com>
2021-09-21 17:22:44 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_SKU_list_by_ProductId.py
+++ vtex/catalog_api/PRODUCT/Get_SKU_list_by_ProductId.py
@@ -18,15 +18,12 @@ registro = 0
 temp2 = "" 
 list1 = ""
 headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-url2 = ""
-num = 0
+
 
 def get_sku_list(id,headers):
-    url1 = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitByProductId/"""+(str(id))+""
-    url2 = url1
-    print(url2)
-    #response = requests.request("GET", url, headers=headers) 
-    #print(response.text)
+    url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitByProductId/"""+(str(id))+""
+    response = requests.request("GET", url, headers=headers) 
+    print(response.text)
     #data = response.text.encode('utf8')
     #temp.append(response.text)
     
