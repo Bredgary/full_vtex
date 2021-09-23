2f41fa6050cae8662209f41424bd150cf077663b
BredgaryV <bred.valenzuela@gmail.com>
2021-09-23 17:41:54 -0300

    MOD: prueba

---
+++ vtex/orders/ORDERS/Get_order.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import requests
+import json
+import os
+import re
+from datetime import datetime
+from os import system
+from google.cloud import bigquery
