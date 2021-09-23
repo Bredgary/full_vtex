79e92223c2971d732e81df146b2e8c56fafa9330
BredgaryV <bred.valenzuela@gmail.com>
2021-09-23 18:08:45 -0300

    MOD: prueba

--- vtex/orders/ORDERS/HistoyListOrders.py
+++ vtex/orders/ORDERS/HistoyListOrders.py
@@ -11,7 +11,7 @@ import numpy as np
 import cv2 as cv
 
 img = np.zeros ((320, 320, 3), np.uint8)
-imprimir img.shape
+#imprimir img.shape
 
 point_size = 1
 point_color = (0, 0, 255) 
@@ -30,10 +30,6 @@ def get_list_order(creationDateFrom,creationDateTo):
 		"X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA",
 		"X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 	response = requests.request("GET", url, headers=headers, params=querystring)
-	
-	text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders/ORDERS/JsonHistory/ordenes-"+creationDateFrom+"-"+creationDateTo+".json", "w")
-	text_file.write(response.text)
-	text_file.close() 
 	for point in points_list:
 		cv.circle(img, point, point_size, point_color, thickness)
 		cv.circle(img, (160, 160), 60, point_color, 0)
@@ -41,6 +37,9 @@ def get_list_order(creationDateFrom,creationDateTo):
 		cv.imshow('image', img)
 		cv.waitKey (10000) 
 	cv.destroyAllWindows()
+	text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders/ORDERS/JsonHistory/ordenes-"+creationDateFrom+"-"+creationDateTo+".json", "w")
+	text_file.write(response.text)
+	text_file.close() 
 
 get_list_order("2021-01-01","2021-01-10")
 print("Primera semana finalizada")
