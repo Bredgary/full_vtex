473b378c234969fca0783c06afd5ef70d1f345e1
BredgaryV <bred.valenzuela@gmail.com>
2021-09-24 19:26:34 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
@@ -11,17 +11,17 @@ import sys
 import uuid
 import self
 
+credentials = GoogleCredentials.get_application_default()
+service = build('bigquery', 'v2', credentials = credentials)
+
 table = "prueba"
 data = "test"
 project_id = "shopstar-datalake"
 dataset_id = "landing_zone"
 
-my_schema = [
-  {
-    "name": "Id",
-    "type": "STRING"
-  }
-]
+data = {u'days_validated': '20', u'days_trained': '80', u'navigated_score': '1', u'description': 'First trial of top seller alg. No filter nor any condition is applied. Skus not present in train count as rank=0.5', u'init_cv_date': '2016-03-06', u'metric': 'rank', u'unix_date': '1461610020241117', u'purchased_score': '10', u'result': '0.32677139316724546', u'date': '2016-04-25', u'carted_score': '3', u'end_cv_date': '2016-03-25'}
+schema = {u'fields': [{u'type': u'STRING', u'name': u'date', u'mode': u'NULLABLE'}, {u'type': u'INTEGER', u'name': u'unix_date', u'mode': u'NULLABLE'}, {u'type': u'STRING', u'name': u'init_cv_date', u'mode': u'NULLABLE'}, {u'type': u'STRING', u'name': u'end_cv_date', u'mode': u'NULLABLE'}, {u'type': u'INTEGER', u'name': u'days_trained', u'mode': u'NULLABLE'}, {u'type': u'INTEGER', u'name': u'days_validated', u'mode': u'NULLABLE'}, {u'type': u'INTEGER', u'name': u'navigated_score', u'mode': u'NULLABLE'}, {u'type': u'INTEGER', u'name': u'carted_score', u'mode': u'NULLABLE'}, {u'type': u'INTEGER', u'name': u'purchased_score', u'mode': u'NULLABLE'}, {u'type': u'STRING', u'name': u'description', u'mode': u'NULLABLE'}, {u'type': u'STRING', u'name': u'metric', u'mode': u'NULLABLE'}, {u'type': u'FLOAT', u'name': u'result', u'mode': u'NULLABLE'}]}
+
 stream_data(self,table,table,my_schema)
 
 def stream_data(self, table, data, schema):
