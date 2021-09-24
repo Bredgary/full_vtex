7091f64f186ce598caeaea7d9e49c74373079e19
BredgaryV <bred.valenzuela@gmail.com>
2021-09-24 19:05:44 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
@@ -25,36 +25,24 @@ my_schema = [
 stream_data(self,table,table,my_schema)
 
 def stream_data(self, table, data, schema):
-    # first checks if table already exists. If it doesn't, then create it
-    r = self.service.tables().list(projectId=your_project_id,
-                                     datasetId=your_dataset_id).execute()
-    table_exists = [row['tableReference']['tableId'] for row in
-                    r['tables'] if
-                    row['tableReference']['tableId'] == table]
+    r = self.service.tables().list(projectId=your_project_id,datasetId=your_dataset_id).execute()
+    table_exists = [row['tableReference']['tableId'] for row in r['tables'] if row['tableReference']['tableId'] == table]
     if not table_exists:
         body = {
-            'tableReference': {
+            'tableReference':
+            {
                 'tableId': table,
                 'projectId': project_id,
                 'datasetId': dataset_id
             },
-            'schema': schema
+            schema': schema
         }
-        self.service.tables().insert(projectId=project_id,
-                                     datasetId=dataset_id,
-                                     body=body).execute()
-
-    # with table created, now we can stream the data
-    # to do so we'll use the tabledata().insertall() function.
-    body = {
-        'rows': [
-            {
-                'json': data,
-                'insertId': str(uuid.uuid4())
-            }
-        ]
+        self.service.tables().insert(projectId=project_id,datasetId=dataset_id,body=body).execute()
+        
+        body = {'rows': [{'json': data,'insertId': str(uuid.uuid4())}]
     }
-    self.service.tabledata().insertAll(projectId=project_id),
-                                       datasetId=dataset_id,
-                                       tableId=table,
-                                         body=body).execute(num_retries=5)
+    self.service.tabledata().insertAll(projectId=project_id),datasetId=dataset_id,tableId=table,body=body).execute(num_retries=5)
+
+
+
+    
