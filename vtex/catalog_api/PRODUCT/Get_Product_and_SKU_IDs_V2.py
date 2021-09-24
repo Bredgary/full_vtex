79bd751ec498f42f92534072dc39625c0f2a9e85
BredgaryV <bred.valenzuela@gmail.com>
2021-09-24 19:08:57 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
@@ -41,6 +41,3 @@ def stream_data(self, table, data, schema):
         body = {'rows': [{'json': data,'insertId': str(uuid.uuid4())}]
     }
     self.service.tabledata().insertAll(projectId=project_id),datasetId=dataset_id,tableId=table,body=body).execute(num_retries=5)
-
-
-
