66f78c785f9cb92153ad88d2cebe21f17c51207a
BredgaryV <bred.valenzuela@gmail.com>
2021-09-24 01:03:50 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
@@ -11,14 +11,30 @@ data_to = 50
 headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 ids = 0
 
+def replace_blank_dict(d):
+    if not d:
+        return None
+    if type(d) is list:
+        for list_item in d:
+            if type(list_item) is dict:
+                for k, v in list_item.items():
+                    list_item[k] = replace_blank_dict(v)
+    if type(d) is dict:
+        for k, v in d.items():
+            d[k] = replace_blank_dict(v)
+    return d
+
 def get_productIFD(id,data_from,data_to,headers):
     url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/GetProductAndSkuIds"
     querystring = {"categoryId":""+str(id)+"","_from":""+str(data_from)+"","_to":""+str(data_to)+""}
     response = requests.request("GET", url, headers=headers, params=querystring)
-	formatoJson = json.loads(response.text)
+	for order in response:
+		for k, v in order.items():
+			order[k] = replace_blank_dict(v)
 	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/"+str(ids+1)+"_productFrom_"+str(data_from)+"_ProductTo_"+str(data_to)+"_categoryID_"+str(id)+".json", "w")
-	text_file.write(formatoJson)
+	text_file.write(response.text)
 	text_file.close()
+	formatoJson = json.loads(response.text)
     data = formatoJson["data"]
     if data:
 		data_from = data_from + 50
