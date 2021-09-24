f8f9a563c45a4fb8343db83c3c391befc7bc4bb7
BredgaryV <bred.valenzuela@gmail.com>
2021-09-24 01:20:11 -0300

    MOD:prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
@@ -18,22 +18,22 @@ def get_productIFD(id,data_from,data_to,headers):
     response = requests.request("GET", url, headers=headers, params=querystring)
     jsonF = json.loads(response.text)
     data = jsonF["data"]
-	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/"+str(ids+1)+"_productFrom_"+str(data_from)+"_ProductTo_"+str(data_to)+"_categoryID_"+str(id)+".json", "w")
-	text_file.write(data)
-	text_file.close()
-    if data:
-		data_from = data_from + 50
-		data_to = data_to +50
-		get_productIFD(id,data_from,data_to,headers)
-    else:
-		u_data_from = str(data_from)
-		u_data_to str(data_to)
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ultimoRegistroFromCargado__"+u_data_from+".json", "w")
-		text_file.write(u_data_from)
-		text_file.close()
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ultimoRegistroToCargado__"+u_data_to+".json", "w")
-		text_file.write(u_data_to)
-		text_file.close()
+	#text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/"+str(ids+1)+"_productFrom_"+str(data_from)+"_ProductTo_"+str(data_to)+"_categoryID_"+str(id)+".json", "w")
+	#text_file.write(data)
+	#text_file.close()
+    #if data:
+	#	data_from = data_from + 50
+    #	data_to = data_to +50
+	#	get_productIFD(id,data_from,data_to,headers)
+    #else:
+	#	u_data_from = str(data_from)
+	#	u_data_to str(data_to)
+	#	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ultimoRegistroFromCargado__"+u_data_from+".json", "w")
+	#	text_file.write(u_data_from)
+	#	text_file.close()
+	#	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ultimoRegistroToCargado__"+u_data_to+".json", "w")
+	#	text_file.write(u_data_to)
+	#	text_file.close()
 
 QUERY = (
     'SELECT id FROM `shopstar-datalake.landing_zone.shopstar_vtex_detail_category` ')
@@ -42,4 +42,5 @@ rows = query_job.result()
 
 for row in rows:
     get_productIFD(str(row.id),data_from,data_to,headers)
+	break
   
