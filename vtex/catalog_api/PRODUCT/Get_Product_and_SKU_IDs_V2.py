20ca0c91b97acc8fe2784bce9f175ce7ed7ac515
BredgaryV <bred.valenzuela@gmail.com>
2021-09-24 22:13:28 -0300

    MDO: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
@@ -9,13 +9,22 @@ client = bigquery.Client()
 #     "cloud-samples-tests", "us", "test", "test"
 # )
 table_id = "shopstar-datalake.landing_zone.prueba2"
-job_config = bigquery.LoadJobConfig(
-    autodetect=True, source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-)
-uri = "gs://vtex/CATALOG_API/list_product_id/HistoryGetProductID/0_productID_categoryID_441.json"
-load_job = client.load_table_from_uri(
-    uri, table_id, job_config=job_config
-)  # Make an API request.
-load_job.result()  # Waits for the job to complete.
-destination_table = client.get_table(table_id)
-print("Loaded {} rows.".format(destination_table.num_rows))
+
+client = bigquery.Client()
+filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/0_productID_categoryID_441.json'
+dataset_id = 'landing_zone'
+table_id = 'prueba2'
+dataset_ref = client.dataset(dataset_id)
+table_ref = dataset_ref.table(table_id)
+job_config = bigquery.LoadJobConfig()
+job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+job_config.autodetect = True
+with open(filename, "rb") as source_file:
+    job = client.load_table_from_file(
+        source_file,
+        table_ref,
+        location="southamerica-east1",  # Must match the destination dataset location.
+    job_config=job_config,)  # API request
+job.result()  # Waits for table load to complete.
+print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+print("finalizado")
