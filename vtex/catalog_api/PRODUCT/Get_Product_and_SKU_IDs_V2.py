a7981edc7ab95d50c828ed1d1120df80aae0c0a1
BredgaryV <bred.valenzuela@gmail.com>
2021-09-24 22:05:46 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
@@ -4,12 +4,6 @@ client = bigquery.Client()
 
 table_id = "shopstar-datalake.landing_zone.prueba2"
 
-job_config = bigquery.LoadJobConfig(
-    schema=[
-        bigquery.SchemaField("name", "STRING"),
-        bigquery.SchemaField("post_abbr", "STRING"),
-    ],
-)
 
 body = io.BytesIO(b"Washington,WA")
 
@@ -18,8 +12,7 @@ previous_rows = client.get_table(table_id).num_rows
 assert previous_rows > 0
 
 job_config = bigquery.LoadJobConfig(
-    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
-    source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+    autodetect=True, source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
 )
 
 uri = "gs://vtex/CATALOG_API/list_product_id/HistoryGetProductID/0_productID_categoryID_441.json"
