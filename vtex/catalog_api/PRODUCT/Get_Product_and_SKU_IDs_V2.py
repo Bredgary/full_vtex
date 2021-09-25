3a1eaafe406e22d1e4c26fb3e6d8ee063ab338b5
BredgaryV <bred.valenzuela@gmail.com>
2021-09-25 04:25:18 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
@@ -10,4 +10,4 @@ uri = "gs://vtex/CATALOG_API/list_product_id/HistoryGetProductID/prueba.json"
 load_job = client.load_table_from_uri(uri, table_id, job_config=job_config)
 load_job.result() 
 destination_table = client.get_table(table_id)
-print("Loaded {} rows.".format(destination_table.num_rows))
+print("Loaded {} rows.".format(destination_table.num_rows)) 
