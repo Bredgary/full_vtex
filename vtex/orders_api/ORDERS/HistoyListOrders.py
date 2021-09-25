22f1310c37c6fc93e4120743699187a1d3118b17
BredgaryV <bred.valenzuela@gmail.com>
2021-09-25 03:22:49 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/HistoyListOrders.py
+++ vtex/orders_api/ORDERS/HistoyListOrders.py
@@ -15,13 +15,18 @@ print("comenzando_trabajo")
 
 def get_list_order():
 	url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders"
-	querystring = {"f_creationDate":"creationDate:[2021-07-01T02:00:00.000Z TO 2021-07-31T01:59:59.999Z]","f_hasInputInvoice":"false"}
+	querystring = {"f_creationDate":"creationDate:[2021-01-01T02:00:00.000Z TO 2021-01-31T01:59:59.999Z]","f_hasInputInvoice":"false"}
 	headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 	response = requests.request("GET", url, headers=headers, params=querystring)
 	jsonF = json.loads(response.text)
 	data = jsonF["list"]
-	with open('/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/HistoryListOrders/ListaOrdenesJulio.json', 'wb') as fp:
+	with open('/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/HistoryListOrders/ListaOrdenesEnero.json', 'wb') as fp:
 		pickle.dump(data,fp)
+
+	with open ('/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/HistoryListOrders/ListaOrdenesEnero.json', 'rb') as fp:
+		itemlist = pickle.load(fp)
+	
+	print(itemlist)
 
 get_list_order()
 
