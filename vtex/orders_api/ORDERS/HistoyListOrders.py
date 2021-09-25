cfa2a08b3b8b3c036132ef56d03dfee978de1d12
BredgaryV <bred.valenzuela@gmail.com>
2021-09-25 01:30:36 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/HistoyListOrders.py
+++ vtex/orders_api/ORDERS/HistoyListOrders.py
@@ -20,7 +20,8 @@ def get_list_order():
 	response = requests.request("GET", url, headers=headers, params=querystring)
 	jsonF = json.loads(response.text)
 	data = jsonF["list"]
-	#idsProducts = json.dumps(data)
+	idsProducts = json.dumps(data)
+	print(idsProducts)
 	#text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/HistoryListOrders/"+str(registro)+"_mes_enero.json", "w")
 	#text_file.write(idsProducts)
 	#text_file.close()
