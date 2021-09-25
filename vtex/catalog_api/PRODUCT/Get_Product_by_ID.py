ca166d1424c096910fc256f93e8845e26d1419a2
BredgaryV <bred.valenzuela@gmail.com>
2021-09-25 15:27:38 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_by_ID.py
+++ vtex/catalog_api/PRODUCT/Get_Product_by_ID.py
@@ -46,9 +46,9 @@ def get_product(id):
         }
     response = requests.request("GET", url, headers=headers)
     jsonF = json.loads(response.text)
-    for order in jsonF:
-        for k, v in order.items():
-            order[k] = replace_blank_dict(v)
+    #for order in jsonF:
+    #    for k, v in order.items():
+    #        order[k] = replace_blank_dict(v)
     return jsonF
 
 for x in formatoJson:
