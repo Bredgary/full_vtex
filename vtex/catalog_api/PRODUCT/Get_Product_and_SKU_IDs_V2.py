ff28567ae85bd0c7445f903fe9bab5a9238143b9
BredgaryV <bred.valenzuela@gmail.com>
2021-09-24 22:08:19 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
@@ -1,26 +1,26 @@
-import io
 from google.cloud import bigquery
+
 client = bigquery.Client()
 
-table_id = "shopstar-datalake.landing_zone.prueba2"
 
-
-body = io.BytesIO(b"Washington,WA")
-
-client.load_table_from_file(body, table_id, job_config=job_config).result()
-previous_rows = client.get_table(table_id).num_rows
-assert previous_rows > 0
-
+# Set the encryption key to use for the destination.
+# TODO: Replace this key with a key you have created in KMS.
+#kms_key_name = "projects/{}/locations/{}/keyRings/{}/cryptoKeys/{}".format(
+#     "cloud-samples-tests", "us", "test", "test"
+# )
 job_config = bigquery.LoadJobConfig(
     autodetect=True, source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
 )
-
 uri = "gs://vtex/CATALOG_API/list_product_id/HistoryGetProductID/0_productID_categoryID_441.json"
 load_job = client.load_table_from_uri(
     uri, table_id, job_config=job_config
-) 
-
-load_job.result() 
-
+)  # Make an API request.
+load_job.result()  # Waits for the job to complete.
 destination_table = client.get_table(table_id)
 print("Loaded {} rows.".format(destination_table.num_rows))
+
+
+
+
+
+table_id = "shopstar-datalake.landing_zone.prueba2"
