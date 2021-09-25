f1cda57f8f5ebfdab82f59fda7a3bc2c34312c72
BredgaryV <bred.valenzuela@gmail.com>
2021-09-25 03:58:44 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs_V2.py
@@ -1,41 +1,36 @@
+#import io
 from google.cloud import bigquery
-import json
-
 client = bigquery.Client()
 
 
-# Set the encryption key to use for the destination.
-# TODO: Replace this key with a key you have created in KMS.
-#kms_key_name = "projects/{}/locations/{}/keyRings/{}/cryptoKeys/{}".format(
-#     "cloud-samples-tests", "us", "test", "test"
-# )
+shopstar-datalake.landing_zone.test
 
 
-f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/0_productID_categoryID_441.json','r')
-data_from_string = f_01.read()
-
-temp = json.loads(data_from_string)
-print(type(temp))
-
-
-
-table_id = "shopstar-datalake.landing_zone.prueba2"
-
-client = bigquery.Client()
+#body = io.BytesIO(b"Washington,WA")
 filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/0_productID_categoryID_441.json'
+table_id = 'shopstar_vtex_prueba'
 dataset_id = 'landing_zone'
-table_id = 'prueba2'
 dataset_ref = client.dataset(dataset_id)
 table_ref = dataset_ref.table(table_id)
-job_config = bigquery.LoadJobConfig()
-job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+
+#client.load_table_from_file(body, table_id, job_config=job_config).result()
+#previous_rows = client.get_table(table_id).num_rows
+#assert previous_rows > 0
+
+job_config = bigquery.LoadJobConfig(
+    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+    source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+)
+
 job_config.autodetect = True
 with open(filename, "rb") as source_file:
-    job = client.load_table_from_file(
+    load_job = client.load_table_from_file(
         source_file,
         table_ref,
         location="southamerica-east1",  # Must match the destination dataset location.
-    job_config=job_config,)  # API request
-job.result()  # Waits for table load to complete.
-print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-print("finalizado")
+    job_config=job_config,).
+
+load_job.result()  # Waits for the job to complete.
+
+destination_table = client.get_table(table_id)
+print("Loaded {} rows.".format(destination_table.num_rows))
