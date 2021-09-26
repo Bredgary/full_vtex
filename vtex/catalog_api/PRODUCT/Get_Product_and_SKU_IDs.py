f09c25cc00923fd426c49a9d54072b9835701202
BredgaryV <bred.valenzuela@gmail.com>
2021-09-26 09:56:21 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
@@ -70,7 +70,7 @@ QUERY = (
 query_job = client.query(QUERY) 
 rows = query_job.result()  
 
-get_productIFD("826",data_from,data_to,ids)
+get_productIFD("1427",data_from,data_to,ids)
 
 #for row in rows:
 #	get_productIFD(str(row.id),data_from,data_to,ids)
