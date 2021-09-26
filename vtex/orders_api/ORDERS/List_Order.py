12f06cc26783e1881678735dcd91435685916271
BredgaryV <bred.valenzuela@gmail.com>
2021-09-26 17:30:23 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -17,7 +17,7 @@ dia1 = int(dia) - 25
 dia2 = int(dia) - 24
 
 limite = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-temporal = []
+temporal = {}
 list_orders=[]
 count = 0
 
@@ -31,8 +31,10 @@ def get_list(pag):
 
 for i in limite:
     x = get_list(i)
-    print(x)
-    print(type(x))
+    temporal.update(x)
+
+print(temporal)
+print(type(temporal))
 
 '''
 data_from = int(data_from_string)
