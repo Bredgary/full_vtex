32e6fa320004bbe904a0822b5732fe92577ed4ab
BredgaryV <bred.valenzuela@gmail.com>
2021-09-26 23:23:17 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -40,7 +40,7 @@ for i in limite:
 
 string = json.dumps(formatoList)
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/order_list.json", "w")
-text_file.write()
+text_file.write(string)
 text_file.close()
 system("cat order_list.json | jq -c '.[]' > temp.json")
 system("cat temp.json | jq -c '.[]' > tabla.json")
