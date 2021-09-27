0d6201a785ee34db7cfd1c159fd6767196bb6161
BredgaryV <bred.valenzuela@gmail.com>
2021-09-26 23:29:55 -0300

    MOD: prueba

--- shell-script/details_orders.sh
+++ shell-script/details_orders.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-{try
+
+#function try()
+##{
+#    [[ $- = *e* ]]; SAVED_OPT_E=$?
+#    set +e
+#}
+
 python3 /home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/List_Order.py 
 #python3 /home/bred_valenzuela/full_vtex/stop_compute_engine.py 
-
-}{ catch
-    echo "Finalizo con problemas"
-}
-
 echo "Finalizado"
