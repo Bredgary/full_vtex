72c70daac7afd8b3328a46f907faab425870cf0d
BredgaryV <bred.valenzuela@gmail.com>
2021-09-26 23:55:56 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -35,8 +35,6 @@ def get_list(pag):
 	headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 	response = requests.request("GET", url, headers=headers, params=querystring)
 	formatoJson = json.loads(response.text)
-    for x in formatoJson["list"]:
-        get_order(x["orderId"])
 	return formatoJson
 
 for i in limite:
