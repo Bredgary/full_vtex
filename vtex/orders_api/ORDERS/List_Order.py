d5a09dae1a3018b0811f900df0fcfc025486d1c4
BredgaryV <bred.valenzuela@gmail.com>
2021-09-27 23:59:48 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -8,23 +8,6 @@ from google.cloud import bigquery
 from itertools import chain
 from collections import defaultdict
 
-day = datetime.today().strftime('%d')
-mouth = datetime.today().strftime('%m')
-year = datetime.today().strftime('%y')
-dayFrom = int(day) - 25
-dayTo = int(day) - 24
-
-limite = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-temporal = {}
-list_orders=[]
-formatoJson = {}
-formatoList = []
-listDetails = []
-listTemp = []
-count = 0
-list_order = []
-
-
 def get_order(ids):
     url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(ids)+""
     headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
@@ -80,35 +63,18 @@ def orderDetails_and_list():
             break
 
 def main():
+    day = datetime.today().strftime('%d')
+    mouth = datetime.today().strftime('%m')
+    year = datetime.today().strftime('%y')
+    dayFrom = int(day) - 24
+    dayTo = int(day) - 23
+    limite = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+    formatoJson = {}
+    formatoList = []
+    listDetails = []
+    list_order = []
     orderDetails_and_list()
     load_big_query(listDetails,'order')
     load_big_query(list_order,'list_order')
 
 main()
-
-'''
-string2 = json.dumps(listDetails)
-text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/order_details.json", "w")
-text_file.write(string2)
-text_file.close()
-system("cat order_details.json | jq -c '.[]' > tablaDetail.json")
-
-print("Cargando a BigQuery Details Orders")
-client = bigquery.Client()
-filename = '/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/tabla.json'
-dataset_id = 'landing_zone'
-table_id = 'shopstar_vtex_order'
-dataset_ref = client.dataset(dataset_id)
-table_ref = dataset_ref.table(table_id)
-job_config = bigquery.LoadJobConfig()
-job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-job_config.autodetect = True
-with open(filename, "rb") as source_file:
-    job = client.load_table_from_file(
-        source_file,
-        table_ref,
-        location="southamerica-east1",  # Must match the destination dataset location.
-    job_config=job_config,)  # API request
-job.result()  # Waits for table load to complete.
-print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-'''
