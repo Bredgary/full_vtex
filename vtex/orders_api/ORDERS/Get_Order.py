f9b48c47a12279e64578e6e6b1ba5afe9690e9e2
BredgaryV <bred.valenzuela@gmail.com>
2021-09-27 22:34:03 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/Get_Order.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-import requests
-import json
-import os
-import re
-from datetime import datetime
-from os import system
-from google.cloud import bigquery
