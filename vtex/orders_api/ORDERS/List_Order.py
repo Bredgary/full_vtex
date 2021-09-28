c12822bc3889cab56aaa8832ceb8539621e826e1
BredgaryV <bred.valenzuela@gmail.com>
2021-09-28 20:26:59 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -12,8 +12,8 @@ from collections import defaultdict
 day = datetime.today().strftime('%d')
 mouth = datetime.today().strftime('%m')
 year = datetime.today().strftime('%y')
-dayFrom = int(day) - 27
-dayTo = int(day) - 26
+dayFrom = int(day) - 26
+dayTo = int(day) - 25
 limite = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 formatoJson = {}
 formatoList = []
@@ -43,6 +43,8 @@ def get_order(ids):
     for k, v in formatoJ.items():
         order[k] = replace_blank_dict(v)
     listDetails.append(formatoJ)
+    count += 1
+    print(str(count)+" Detallle de orden almacenado")
 
 def get_list(pag):
     url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/?page="+str(pag)+""
