98ba9dd440ae91ca1e0c456f6e15431566c74fd5
BredgaryV <bred.valenzuela@gmail.com>
2021-09-28 19:20:08 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -60,12 +60,9 @@ for i in limite:
     else:
         break
 
-
-
-
 string = json.dumps(listDetails)
-characters = "@"
-string = ''.join( x for x in string if x not in characters)
+#characters = "@"
+#string = ''.join( x for x in string if x not in characters)
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/temp.json", "w")
 text_file.write(string)
 text_file.close()
