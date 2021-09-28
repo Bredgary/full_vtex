7c9707df50dd19720af682d223a43f050ab84621
BredgaryV <bred.valenzuela@gmail.com>
2021-09-28 19:12:24 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -20,6 +20,19 @@ formatoList = []
 listDetails = []
 list_order = []
 
+def replace_blank_dict(d):
+    if not d:
+        return None
+    if type(d) is list:
+        for list_item in d:
+            if type(list_item) is dict:
+                for k, v in list_item.items():
+                    list_item[k] = replace_blank_dict(v)
+    if type(d) is dict:
+        for k, v in d.items():
+            d[k] = replace_blank_dict(v)
+    return d
+
 def get_order(ids):
     url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(ids)+""
     headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
@@ -48,18 +61,7 @@ for i in limite:
         break
 
 
-def replace_blank_dict(d):
-    if not d:
-        return None
-    if type(d) is list:
-        for list_item in d:
-            if type(list_item) is dict:
-                for k, v in list_item.items():
-                    list_item[k] = replace_blank_dict(v)
-    if type(d) is dict:
-        for k, v in d.items():
-            d[k] = replace_blank_dict(v)
-    return d
+
 '''
 string = json.dumps(listDetails)
 characters = "@"
