d6eb4761f56b8c1b60afb749b0170a8d744ce96a
BredgaryV <bred.valenzuela@gmail.com>
2021-09-28 21:20:56 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -12,8 +12,8 @@ from collections import defaultdict
 day = datetime.today().strftime('%d')
 mouth = datetime.today().strftime('%m')
 year = datetime.today().strftime('%y')
-dayFrom = int(day) - 27
-dayTo = int(day) - 26
+dayFrom = int(day) - 26
+dayTo = int(day) - 25
 limite = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 formatoJson = {}
 formatoList = []
