d06bf1888710545e1302df00e987eb9bfa718369
BredgaryV <bred.valenzuela@gmail.com>
2021-09-29 00:00:44 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -13,7 +13,10 @@ day = datetime.today().strftime('%d')
 mouth = datetime.today().strftime('%m')
 year = datetime.today().strftime('%y')
 dayFrom = int(day) - 20
-dayTo = int(day) - 18
+dayTo = int(day) - 17
+
+print("Cargando Fecha: 20"+str(year)+"-"+str(mouth)+"-"+str(dayFrom)+" al 20"+str(year)+"-"+str(mouth)+"-"+str(dayTo)+"")
+
 limite = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 formatoJson = {}
 formatoList = []
