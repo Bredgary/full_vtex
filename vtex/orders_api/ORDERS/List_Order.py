6638446cdfd5684df697727362a2f1a46e31b681
BredgaryV <bred.valenzuela@gmail.com>
2021-09-29 00:44:23 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -20,8 +20,8 @@ listDetails = []
 list_order = []
 order = {}
 count = 0
-dayFrom = int(day) - 16
-dayTo = int(day) - 15
+dayFrom = int(day) - 15
+dayTo = int(day) - 14
 
 def replace_blank_dict(d):
     if not d:
