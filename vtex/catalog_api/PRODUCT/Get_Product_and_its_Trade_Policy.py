2ccd185a26224f179d745b761011af08ecff1638
BredgaryV <bred.valenzuela@gmail.com>
2021-09-30 20:08:30 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_its_Trade_Policy.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_its_Trade_Policy.py
@@ -10,29 +10,39 @@ client = bigquery.Client()
 productList = []
 listIdProductAndContext = []
 listaIDS = []
+f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/delimitador3.txt','r')
+data_from_string = f_01.read()
+delimitador = int(data_from_string)
 count = 0
 
-def get_policy(id,count):
-    if count >= 56448:
-        print("Comenzando: "+str(count))
-        url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/productget/"""+str(id)+""
-        headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-        response = requests.request("GET", url, headers=headers)
-        jsonF = json.loads(response.text)
-        string = json.dumps(jsonF)
-        text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/temp3/"+str(count)+"_get_policy.json", "w")
-        text_file.write(string)
-        text_file.close()
-        print("Terminando: "+str(count)) 
+def get_policy(id,count,delimitador):
+    if count >= delimitador:
+        try:
+            print("delimitador: "+str(delimitador))
+            url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/productget/"""+str(id)+""
+            headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+            response = requests.request("GET", url, headers=headers)
+            jsonF = json.loads(response.text)
+            string = json.dumps(jsonF)
+            text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/Trade_policy/"+str(count)+"_get_policy.json", "w")
+            text_file.write(string)
+            text_file.close()
+            print("Terminando: "+str(count)) 
+        except:
+            delimitador = count
+            text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/delimitador3.txt", "w")
+            text_file.write(str(delimitador))
+            text_file.close()
+            system("python3 Get_Product_and_its_Trade_Policy.py")
 
-f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/lista.json','r')
+f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/id_producto.json','r')
 data_from_string = f_01.read()
 
 formatoJSon = json.loads(data_from_string)
 
 for i in formatoJSon:
     count +=1
-    get_policy(i,count)
+    get_policy(i,count,delimitador)
     
 
 print(str(count)+" registro almacenado "+str(i))
