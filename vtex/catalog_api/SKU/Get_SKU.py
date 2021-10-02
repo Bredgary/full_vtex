8513b1a22e4087f8cf597251d5cd5d09e2bbda4e
BredgaryV <bred.valenzuela@gmail.com>
2021-10-02 20:58:55 -0300

    MOD: prueba

--- vtex/catalog_api/SKU/Get_SKU.py
+++ vtex/catalog_api/SKU/Get_SKU.py
@@ -43,9 +43,8 @@ def operacion_fenix(count):
 	f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU/id_sku.json','r')
 	data_from_string = f_01.read()
 	listaIDS.append(data_from_string)
-	print(type(listaIDS))
-	for i in formatoJSon:
-	#	count += 1
+	for i in listaIDS:
+		count += 1
 		sku = get_sku(i,count,delimitador)
 		break
 	print(str(count)+" registro almacenado.")
