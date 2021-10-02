64adeac9cfcdb1a4f04033fba648c0a25c3c0e5b
BredgaryV <bred.valenzuela@gmail.com>
2021-10-02 11:54:36 -0300

    MOD: prueba

--- vtex/catalog_api/SKU/List_all_SKU_IDs.py
+++ vtex/catalog_api/SKU/List_all_SKU_IDs.py
@@ -58,6 +58,7 @@ for x in range(countDir):
     uri = "/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU/IdSku/"+str(count)+"_IdSku.json"
     f_03 = open (uri,'r')
     ids_string = f_03.read()
+    ids_string.replace('[','').replace(']','')
     listaID.append(ids_string)
     #print("ID type: " +type(listaID))
     print("ID: " +str(listaID))
