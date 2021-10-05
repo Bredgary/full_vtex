aa20aeecbf5eeeb6000a306b2f760108b5b5cf4c
BredgaryV <bred.valenzuela@gmail.com>
2021-10-04 23:06:18 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/Get_orderDetails_and_OrderList.py
+++ vtex/orders_api/ORDERS/Get_orderDetails_and_OrderList.py
@@ -47,7 +47,7 @@ def get_order(ids):
 
 def get_list(pag):
     url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/?page="+str(pag)+""
-    querystring = {"f_creationDate":"creationDate:[2021-09-20T02:00:00.000Z TO 2021-09-21T01:59:59.999Z]","f_hasInputInvoice":"false"}
+    querystring = {"f_creationDate":"creationDate:[2021-09-21T02:00:00.000Z TO 2021-09-22T01:59:59.999Z]","f_hasInputInvoice":"false"}
     headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
     response = requests.request("GET", url, headers=headers, params=querystring)
     formatoJson = json.loads(response.text)
