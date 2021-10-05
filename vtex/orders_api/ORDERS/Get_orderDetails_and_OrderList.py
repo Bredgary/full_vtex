8a2dc19c4522d8c4e4061e96c5ab6a08e33266c5
BredgaryV <bred.valenzuela@gmail.com>
2021-10-04 21:06:07 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/Get_orderDetails_and_OrderList.py
+++ vtex/orders_api/ORDERS/Get_orderDetails_and_OrderList.py
@@ -60,8 +60,9 @@ for i in limite:
     count = count + 1
     print(str(count)+" Pagina recorrida")
     x = get_list(i)
-    if bool(x["list"]):
-        list_order.append(x["list"])
+    print(x)
+    #if bool(x["list"]):
+    #    list_order.append(x["list"])
 
 
 string = json.dumps(listDetails)
