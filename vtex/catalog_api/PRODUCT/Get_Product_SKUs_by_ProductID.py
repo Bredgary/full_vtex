3980c4d0799684f353e95c07e385de923990b0a5
BredgaryV <bred.valenzuela@gmail.com>
2021-10-07 04:33:02 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_SKUs_by_ProductID.py
+++ vtex/catalog_api/PRODUCT/Get_Product_SKUs_by_ProductID.py
@@ -63,7 +63,7 @@ for x in range(countDir):
         uri = "/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/SKUs_by_id/"+str(registro)+"_skus_by_Product.json"
         f_03 = open (uri,'r')
         ids_string = f_03.read()
-        if ids_string is "ProductId not found":
+        if ids_string == "ProductId not found":
             print("hola")
         else:
             print("mal")
