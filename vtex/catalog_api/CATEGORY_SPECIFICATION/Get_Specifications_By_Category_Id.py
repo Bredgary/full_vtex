3bf0b133c206d74534b31b3376d078f93691ce3d
BredgaryV <bred.valenzuela@gmail.com>
2021-10-07 18:48:41 -0300

    MOD: prueba

--- vtex/catalog_api/CATEGORY_SPECIFICATION/Get_Specifications_By_Category_Id.py
+++ vtex/catalog_api/CATEGORY_SPECIFICATION/Get_Specifications_By_Category_Id.py
@@ -10,7 +10,7 @@ client = bigquery.Client()
 listaID = []
 listIdSkuAndContext =[]
 registro = 0
-
+'''
 f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/CATEGORY_SPECIFICATION/delimitador.txt','r')
 data_from_string = f_01.read()
 delimitador = int(data_from_string)
@@ -52,19 +52,21 @@ operacion_fenix(count)
 
 '''
 
-DIR = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU/SKU/'
+DIR = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/CATEGORY_SPECIFICATION/CATEGORY_SPECIFICATION/'
 countDir = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
 
 for x in range(countDir):
 	registro +=1
-	uri = "/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU/SKU/"+str(registro)+"_sku.json"
+	uri = "/home/bred_valenzuela/full_vtex/vtex/catalog_api/CATEGORY_SPECIFICATION/CATEGORY_SPECIFICATION/"+str(registro)+"category_specification.json"
 	f_03 = open (uri,'r')
 	ids_string = f_03.read()
 	formatoJson = json.loads(ids_string)
-	listaID.append(formatoJson)
-	print("SKU Almacenados: " +str(registro))
+	print(formatoJson)
+	break
+	#listaID.append(formatoJson)
+	#print("SKU Almacenados: " +str(registro))
 
-
+'''
 string = json.dumps(listaID)
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU/sku.json", "w")
 text_file.write(string)
@@ -92,5 +94,5 @@ job.result()  # Waits for table load to complete.
 print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
 system("rm sku.json")
 print("finalizado")
-'''
 
+'''
