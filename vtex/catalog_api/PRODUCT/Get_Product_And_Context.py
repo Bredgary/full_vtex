b77eda03440d9f8bec38506bfe538f4387aff142
BredgaryV <bred.valenzuela@gmail.com>
2021-10-07 03:56:27 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_And_Context.py
+++ vtex/catalog_api/PRODUCT/Get_Product_And_Context.py
@@ -79,8 +79,9 @@ system("cat temp.json | jq -c '.[]' > tableContext.json")
 
 print("Cargando a BigQuery")
 client = bigquery.Client()
+filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/tableContext.json'
 dataset_id = 'landing_zone'
-table_id = 'shopstar_vtex_policy_product_v2'
+table_id = 'shopstar_vtex_product_and_context'
 dataset_ref = client.dataset(dataset_id)
 table_ref = dataset_ref.table(table_id)
 job_config = bigquery.LoadJobConfig()
