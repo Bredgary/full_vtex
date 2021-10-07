7378afd34e01ab778797cde76d3cfac000d39ac3
BredgaryV <bred.valenzuela@gmail.com>
2021-10-07 18:11:36 -0300

    MOD: prueba

--- vtex/catalog_api/SPECIFICATION_GROUP/List_Specifications_Group_by_Category.py
+++ vtex/catalog_api/SPECIFICATION_GROUP/List_Specifications_Group_by_Category.py
@@ -12,13 +12,15 @@ from google.cloud import bigquery
 client = bigquery.Client()
 productList = []
 listaIDS = []
+listaID = []
 f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/SPECIFICATION_GROUP/delimitador.txt','r')
 data_from_string = f_01.read()
 delimitador = int(data_from_string)
+registro = 0
 count = 0
 mensajeError = '"CategoryId Not Found"'
 
-
+'''
 def get_list_group(id,count,delimitador):
 	jsonF = {}
 	if count >= delimitador:
@@ -52,7 +54,7 @@ def operacion_fenix(count):
 	print(str(count)+" registro almacenado.")
 
 operacion_fenix(count)
-'''
+
 
 
 QUERY = (
@@ -69,3 +71,49 @@ text_file.write(string)
 text_file.close()
 
 '''
+DIR = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SPECIFICATION_GROUP/SPECIFICATION_GROUP/'
+countDir = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+
+for x in range(countDir):
+	registro +=1
+	uri = "/home/bred_valenzuela/full_vtex/vtex/catalog_api/SPECIFICATION_GROUP/SPECIFICATION_GROUP/"+str(registro)+"_group.json"
+	f_03 = open (uri,'r')
+	ids_string = f_03.read()
+	formatoJson = json.loads(ids_string)
+	listaID.append(formatoJson)
+	print("SKU Almacenados: " +str(registro))
+
+
+string = json.dumps(listaID)
+text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SPECIFICATION_GROUP/temp.json", "w")
+text_file.write(string)
+text_file.close() 
+
+system("cat temp.json | jq -c '.[]' > tableGroup.json")
+
+print("Cargando a BigQuery")
+client = bigquery.Client()
+filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SPECIFICATION_GROUP/tableGroup.json'
+dataset_id = 'landing_zone'
+table_id = 'shopstar_vtex_list_group_specifications'
+dataset_ref = client.dataset(dataset_id)
+table_ref = dataset_ref.table(table_id)
+job_config = bigquery.LoadJobConfig()
+job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+job_config.autodetect = True
+with open(filename, "rb") as source_file:
+    job = client.load_table_from_file(
+        source_file,
+        table_ref,
+        location="southamerica-east1",  # Must match the destination dataset location.
+    job_config=job_config,)  # API request
+job.result()  # Waits for table load to complete.
+print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+system("rm sku.json")
+print("finalizado")
+
+
+
+
+
+
