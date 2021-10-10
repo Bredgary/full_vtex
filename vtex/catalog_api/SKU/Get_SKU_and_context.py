52c0708975580b54ecd3b829a90970f8904007de
BredgaryV <bred.valenzuela@gmail.com>
2021-10-10 16:12:38 -0300

    MOD: prueba

--- vtex/catalog_api/SKU/Get_SKU_and_context.py
+++ vtex/catalog_api/SKU/Get_SKU_and_context.py
@@ -59,12 +59,13 @@ DIR = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU/SKUContext'
 countDir = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
 
 for x in range(countDir):
-    uri = "/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU/SKUContext/"+str(x)+"_sku.json"
+	uri = "/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU/SKUContext/"+str(x)+"_sku.json"
 	f_03 = open (uri,'r')
 	ids_string = f_03.read()
 	formatoJson = json.loads(ids_string)
 	listaID.append(formatoJson)
 	print("sku contexts Almacenados: " +str(count))
+
 
 string = json.dumps(listaID)
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU/sku.json", "w")
