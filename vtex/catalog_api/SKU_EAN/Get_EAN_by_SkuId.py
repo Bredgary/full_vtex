a7658c301bc659def3baafa9bdfb8b0a467912bd
BredgaryV <bred.valenzuela@gmail.com>
2021-10-11 17:59:32 -0300

    MOD: prueba

--- vtex/catalog_api/SKU_EAN/Get_EAN_by_SkuId.py
+++ vtex/catalog_api/SKU_EAN/Get_EAN_by_SkuId.py
@@ -23,11 +23,10 @@ def get_ean(id,count,delimitador):
 			url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/stockkeepingunit/"+str(id)+"/ean"
 			headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 			response = requests.request("GET", url, headers=headers)
-			if respose.text != '""':
-				text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/SKU_EAN/"+str(count)+"_sku_ean.json", "w")
-				text_file.write(response.text)
-				text_file.close()
-				print("Get_EAN_by_SkuId.py Terminando: "+str(count))
+			text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/SKU_EAN/"+str(count)+"_sku_ean.json", "w")
+			text_file.write(response.text)
+			text_file.close()
+			print("Get_EAN_by_SkuId.py Terminando: "+str(count))
 		except:
 			delimitador = count
 			text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/delimitador.txt", "w")
