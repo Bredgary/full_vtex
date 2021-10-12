3b77734b08ef248278de174652c060191a6110ec
BredgaryV <bred.valenzuela@gmail.com>
2021-10-11 23:01:47 -0300

    MOD: prueba

---
+++ vtex/catalog_api/COLLECTION_BETA/Get_All_Collections.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import os
+import re
+from datetime import datetime
+from os import system
+from google.cloud import bigquery
+from itertools import chain
+from collections import defaultdict
+
+url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/collection/search"
+querystring = {"page":"1","pageSize":"1","orderByAsc":"true"}
+headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+response = requests.request("GET", url, headers=headers, params=querystring)
+Json = json.loads(response.text)
+paging = Json["paging"]
+total = int(paging["total"])
+pages = int(paging["pages"])
+items = Json["items"]
+
+
+for x in range(pages):
+	print("Hola")
+
