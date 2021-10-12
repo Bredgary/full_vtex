f6482539b11d6fac8d3d80d4d0d2c1a62f9dffb5
BredgaryV <bred.valenzuela@gmail.com>
2021-10-12 00:06:50 -0300

    MOD: prueba

--- vtex/catalog_api/COLLECTION_BETA/Get_All_Collections.py
+++ vtex/catalog_api/COLLECTION_BETA/Get_All_Collections.py
@@ -29,35 +29,38 @@ def get_collection_beta(page,headers,total):
 	text_file.write(result)
 	text_file.close()
 	print("Pagina: "+str(page))
+	#cargando_bigquery()
 
-
+def cargando_bigquery():
+	print("Cargando a BigQuery")
+	system("cat items.json | jq -c '.[]' > tableCollectionBeta.json")
+	client = bigquery.Client()
+	filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/COLLECTION_BETA/tableCollectionBeta.json'
+	dataset_id = 'landing_zone'
+	table_id = 'shopstar_vtex_collection_beta'
+	dataset_ref = client.dataset(dataset_id)
+	table_ref = dataset_ref.table(table_id)
+	job_config = bigquery.LoadJobConfig()
+	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+	job_config.autodetect = True
+	with open(filename, "rb") as source_file:
+		job = client.load_table_from_file(
+			source_file,
+			table_ref,
+			location="southamerica-east1",  # Must match the destination dataset location.
+		job_config=job_config,)  # API request
+	job.result()  # Waits for table load to complete.
+	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+	print("finalizado")
+	system("rm items.json")
+	system("rm tableCollectionBeta.json")
 
 for x in range(pages):
 	start += 1
 	get_collection_beta(start,headers,total)
 	break
 
-system("cat items.json | jq -c '.[]' > tableCollectionBeta.json")
 
-print("Cargando a BigQuery")
-client = bigquery.Client()
-filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/COLLECTION_BETA/tableCollectionBeta.json'
-dataset_id = 'landing_zone'
-table_id = 'shopstar_vtex_collection_beta'
-dataset_ref = client.dataset(dataset_id)
-table_ref = dataset_ref.table(table_id)
-job_config = bigquery.LoadJobConfig()
-job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-job_config.autodetect = True
-with open(filename, "rb") as source_file:
-    job = client.load_table_from_file(
-        source_file,
-        table_ref,
-        location="southamerica-east1",  # Must match the destination dataset location.
-    job_config=job_config,)  # API request
-job.result()  # Waits for table load to complete.
-print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-print("finalizado")
 
 
 
