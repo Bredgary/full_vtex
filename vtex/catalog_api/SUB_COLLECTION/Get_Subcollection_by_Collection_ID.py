4eee2e6111b779a2f74850022376f66734f5b96d
BredgaryV <bred.valenzuela@gmail.com>
2021-10-13 04:19:44 -0300

    MOD: prueba

--- vtex/catalog_api/SUB_COLLECTION/Get_Subcollection_by_Collection_ID.py
+++ vtex/catalog_api/SUB_COLLECTION/Get_Subcollection_by_Collection_ID.py
@@ -12,7 +12,7 @@ productList = []
 
 def get_subcollection(id):
 	#try:
-	url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/collection/"+id+"/subcollection"
+	url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/collection/"+str(id)+"/subcollection"
 	headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 	response = requests.request("GET", url, headers=headers)
 	FJson = json.loads(response.text)
