50f8736f91b608c443387e30555674841ea53395
BredgaryV <bred.valenzuela@gmail.com>
2021-10-13 09:14:05 -0300

    MOD: prueba

--- vtex/catalog_api/SKU_EAN/Get_EAN_by_SkuId.py
+++ vtex/catalog_api/SKU_EAN/Get_EAN_by_SkuId.py
@@ -29,7 +29,7 @@ def get_ean(id,count,delimitador):
 			print("Get_EAN_by_SkuId.py Terminando: "+str(count))
 			cargando_bigquery()
 		except:
-			delimitador = count
+			delimitador = count + 1
 			text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/delimitador.txt", "w")
 			text_file.write(str(delimitador))
 			text_file.close()
