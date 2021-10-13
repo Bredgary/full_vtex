c36ccf4af9a45e8b761aaf37b153cf33fdc81ad5
BredgaryV <bred.valenzuela@gmail.com>
2021-10-13 02:38:53 -0300

    MOD: prueba

--- vtex/catalog_api/SKU_EAN/Get_EAN_by_SkuId.py
+++ vtex/catalog_api/SKU_EAN/Get_EAN_by_SkuId.py
@@ -18,22 +18,23 @@ count = 0
 def get_ean(id,count,delimitador):
 	jsonF = {}
 	if count >= delimitador:
-		#try:
+		try:
 		url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/stockkeepingunit/"+str(id)+"/ean"
 		headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 		response = requests.request("GET", url, headers=headers)
 		temp = response.text
-		idEan = temp.replace("[", "").replace("]","").replace('""',"")
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/SKU_EAN/"+str(count)+"_sku_ean.json", "w")
+		idEan = temp.replace("[", "{").replace("]","}")
+		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/sku_ean.json", "w")
 		text_file.write(idEan)
 		text_file.close()
 		print("Get_EAN_by_SkuId.py Terminando: "+str(count))
-		#except:
-		#	delimitador = count
-		#	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/delimitador.txt", "w")
-		#	text_file.write(str(delimitador))
-		#	text_file.close()
-		#	system("python3 Get_EAN_by_SkuId.py")
+		cargando_bigquery()
+		except:
+			delimitador = count
+			text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/delimitador.txt", "w")
+			text_file.write(str(delimitador))
+			text_file.close()
+			system("python3 Get_EAN_by_SkuId.py")
 
 
 def operacion_fenix(count):
@@ -46,6 +47,29 @@ def operacion_fenix(count):
 		get_ean(i,count,delimitador)
 		break
 	print(str(count)+" registro almacenado.")
+
+
+def cargando_bigquery():
+	print("Cargando a BigQuery")
+	client = bigquery.Client()
+	filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/sku_ean.json'
+	dataset_id = 'landing_zone'
+	table_id = 'shopstar_vtex_ean_id'
+	dataset_ref = client.dataset(dataset_id)
+	table_ref = dataset_ref.table(table_id)
+	job_config = bigquery.LoadJobConfig()
+	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+	job_config.autodetect = True
+	with open(filename, "rb") as source_file:
+		job = client.load_table_from_file(
+			source_file,
+			table_ref,
+			location="southamerica-east1",  # Must match the destination dataset location.
+		job_config=job_config,)  # API request
+	job.result()  # Waits for table load to complete.
+	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+	print("finalizado")
+
 
 operacion_fenix(count)
 
