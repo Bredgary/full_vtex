e5ace024ddbc7a283fa0fde0d757d0c2fcb57aa8
BredgaryV <bred.valenzuela@gmail.com>
2021-10-13 08:00:49 -0300

    MOD: prueba

--- vtex/master_data_api_v2/SEARCH/Search_documents.py
+++ vtex/master_data_api_v2/SEARCH/Search_documents.py
@@ -10,30 +10,31 @@ from itertools import count
 client = bigquery.Client()
 productList = []
 count = 0
-
-'''
-def get_subcollection(id):
-	try:
-		url = "https://mercury.vtexcommercestable.com.br/api/dataentities/CL/search"
-		querystring = {"_fields":"id,firstName,lastName,email,accountId,accountName,dataEntityId","_where":"email is not null"}
-		headers = {"Content-Type": "application/json","Accept": "application/vnd.vtex.ds.v10+json","REST-Range": "resources=0-1","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-		response = requests.request("GET", url, headers=headers, params=querystring)
-		FJson = json.loads(response.text)
-		result = json.dumps(FJson)
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SUB_COLLECTION/temp.json", "w")
-		text_file.write(result)
-		text_file.close()
-		cargando_bigquery()
-	except:
-		print("Vacio")
+x = ""
+num1=0
+num2=1
+def get_search_documents(x,rango1,rango2):
+	#try:
+	url = "https://mercury.vtexcommercestable.com.br/api/dataentities/CL/search"
+	querystring = {"_fields":"id,firstName,lastName,email,accountId,accountName,dataEntityId","_where":"email is not null"}
+	headers = {"Content-Type": "application/json","Accept": "application/vnd.vtex.ds.v10+json","REST-Range": "resources="+num1+"-"+num2+"","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+	response = requests.request("GET", url, headers=headers, params=querystring)
+	FJson = json.loads(response.text)
+	result = json.dumps(FJson)
+	text_file = open("/home/bred_valenzuela/full_vtex/vtex/master_data_api_v2/SEARCH/temp.json", "w")
+	text_file.write(result)
+	text_file.close()
+	cargando_bigquery()
+	#except:
+	#	x = "Limite"
 
 def cargando_bigquery():
-	system("cat temp.json | jq -c '.[]' > sub_collection.json")
+	system("cat temp.json | jq -c '.[]' > search_documents.json")
 	print("Cargando a BigQuery")
 	client = bigquery.Client()
-	filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SUB_COLLECTION/sub_collection.json'
+	filename = '/home/bred_valenzuela/full_vtex/vtex/master_data_api_v2/SEARCH/search_documents.json'
 	dataset_id = 'landing_zone'
-	table_id = 'shopstar_vtex_sub_collection'
+	table_id = 'shopstar_vtex_search_documents'
 	dataset_ref = client.dataset(dataset_id)
 	table_ref = dataset_ref.table(table_id)
 	job_config = bigquery.LoadJobConfig()
@@ -49,17 +50,14 @@ def cargando_bigquery():
 	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
 	print("finalizado")
 
-def operacion_fenix(count):
-	f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/SUB_COLLECTION/id_collecion.json','r')
-	data_from_string = f_01.read()
-	listaIDS = json.loads(data_from_string)
-	for i in listaIDS:
-		get_subcollection(i)
-		count += 1
-		print(str(count)+" registro almacenado.")
+def operacion_fenix(x,num1,num2):
+	for x in iter(int, 1):
+		get_search_documents(x,num1,num2)
+		if x = "Limite":
+			break
+		break
 
-operacion_fenix(count)
-'''
-for x in iter(int, 1):
-    print("Hola")
+operacion_fenix(x,num1,num2)
+
+
 
