d2ca5efc1e020674ab298794c24d65565576a82a
BredgaryV <bred.valenzuela@gmail.com>
2021-10-14 05:02:46 -0300

    MOD: prueba

--- vtex/catalog_api/SKU_EAN/Get_EAN.py
+++ vtex/catalog_api/SKU_EAN/Get_EAN.py
@@ -9,20 +9,19 @@ from google.cloud import bigquery
 client = bigquery.Client()
 productList = []
 count = 0
+mensajeError = "SKU not found."
 
 def get_aen(id):
-	try:
-		url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyean/"+str(id)+""
-		headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-		response = requests.request("GET", url, headers=headers)
-		FJson = json.loads(response.text)
-		result = json.dumps(FJson)
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/temp.json", "w")
-		text_file.write(result)
-		text_file.close()
+	url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyean/"+str(id)+""
+	headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+	response = requests.request("GET", url, headers=headers)
+	FJson = json.loads(response.text)
+	result = json.dumps(FJson)
+	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/temp.json", "w")
+	text_file.write(result)
+	text_file.close()
+	if response.text != mensajeError:
 		cargando_bigquery()
-	except:
-		print("Vacio")
 
 def cargando_bigquery():
 	system("cat temp.json | jq -c '.[]' > eat_table.json")
