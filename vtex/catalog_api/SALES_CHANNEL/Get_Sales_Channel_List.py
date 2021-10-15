b6dea25b3c5faa470ea0b3859d6628b6f2c8bb3c
BredgaryV <bred.valenzuela@gmail.com>
2021-10-15 02:38:10 -0300

    MOD: prueba

---
+++ vtex/catalog_api/SALES_CHANNEL/Get_Sales_Channel_List.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import os
+import re
+from datetime import datetime
+from os import system
+from google.cloud import bigquery
+from itertools import chain
+from collections import defaultdict
+
+
+
+def get_collection_beta():
+	url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/saleschannel/list"
+	headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+	response = requests.request("GET", url, headers=headers)
+	FJson = json.loads(response.text)
+	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/COLLECTION_BETA/items.json", "w")
+	text_file.write(FJson)
+	text_file.close()
+	cargando_bigquery()
+
+def cargando_bigquery():
+	print("Cargando a BigQuery")
+	system("cat items.json | jq -c '.[]' > tableCollectionBeta.json")
+	client = bigquery.Client()
+	filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/COLLECTION_BETA/tableCollectionBeta.json'
+	dataset_id = 'landing_zone'
+	table_id = 'shopstar_vtex_sales_channel_list'
+	dataset_ref = client.dataset(dataset_id)
+	table_ref = dataset_ref.table(table_id)
+	job_config = bigquery.LoadJobConfig()
+	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+	job_config.autodetect = True
+	with open(filename, "rb") as source_file:
+		job = client.load_table_from_file(
+			source_file,
+			table_ref,
+			location="southamerica-east1",  # Must match the destination dataset location.
+		job_config=job_config,)  # API request
+	job.result()  # Waits for table load to complete.
+	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+	print("finalizado")
+	system("rm items.json")
+	system("rm tableCollectionBeta.json")
+
+for x in range(pages):
+	start += 1
+	get_collection_beta(start,headers,total)
