2a4c6b9671236a160dad8958ae917ccfe1b0bfb9
BredgaryV <bred.valenzuela@gmail.com>
2021-10-15 04:35:04 -0300

    MOD: prueba

--- vtex/catalog_api/SALES_CHANNEL/Get_Sales_Channel_by_Id.py
+++ vtex/catalog_api/SALES_CHANNEL/Get_Sales_Channel_by_Id.py
@@ -30,7 +30,7 @@ def cargando_bigquery():
 	client = bigquery.Client()
 	filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SALES_CHANNEL/temp.json'
 	dataset_id = 'landing_zone'
-	table_id = 'shopstar_vtex_sales_channel'
+	table_id = 'test'
 	dataset_ref = client.dataset(dataset_id)
 	table_ref = dataset_ref.table(table_id)
 	job_config = bigquery.LoadJobConfig()
