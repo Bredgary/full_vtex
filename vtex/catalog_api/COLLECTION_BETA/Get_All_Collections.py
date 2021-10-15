fe303a413e16e578b7def821ced0206955365146
BredgaryV <bred.valenzuela@gmail.com>
2021-10-15 08:18:45 -0300

    MOD: prueba

--- vtex/catalog_api/COLLECTION_BETA/Get_All_Collections.py
+++ vtex/catalog_api/COLLECTION_BETA/Get_All_Collections.py
@@ -59,7 +59,20 @@ for x in range(pages):
 	start += 1
 	get_collection_beta(start,headers,total)
 
+'''
+QUERY = (
+    'SELECT FieldId FROM `shopstar-datalake.landing_zone.shopstar_vtex_sku_specification` WHERE FieldId is not null')
+query_job = client.query(QUERY)  
+rows = query_job.result()  
 
+for row in rows:
+    productList.append(row.FieldId)
+
+string = json.dumps(productList)
+text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SPECIFICATION_FIELD/SPECIFICATION_FIELD_ID_2.json", "w")
+text_file.write(string)
+text_file.close()
+'''
 
 
 
