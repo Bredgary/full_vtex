9dd4e90e9834a789c84114d656188a561c719097
BredgaryV <bred.valenzuela@gmail.com>
2021-10-16 01:04:13 -0300

    MOD: prueba

--- vtex/checkout_api/CART_ATTACHMENTS/Get_client_profile_by_email.py
+++ vtex/checkout_api/CART_ATTACHMENTS/Get_client_profile_by_email.py
@@ -9,6 +9,7 @@ from itertools import chain
 from collections import defaultdict
 
 client = bigquery.Client()
+productList=[]
 
 QUERY = (
     'SELECT email FROM `shopstar-datalake.landing_zone.shopstar_vtex_search_documents` WHERE email is not null')
@@ -19,6 +20,6 @@ for row in rows:
     productList.append(row.email)
 
 string = json.dumps(productList)
-text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SPECIFICATION_FIELD/SPECIFICATION_FIELD_ID_2.json", "w")
+text_file = open("/home/bred_valenzuela/full_vtex/vtex/checkout_api/CART_ATTACHMENTS/email.json", "w")
 text_file.write(string)
 text_file.close()
