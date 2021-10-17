ce225a58f18b41bc86724db5cc1b644acb2cd303
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-16 21:46:17 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -8,29 +8,26 @@ from google.cloud import bigquery
 from itertools import chain
 from collections import defaultdict
 
-url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders"
-querystring = {"f_creationDate":"creationDate:[2020-01-01T02:00:00.000Z TO 2021-10-01T01:59:59.999Z]","f_hasInputInvoice":"false"}
-headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-response = requests.request("GET", url, headers=headers, params=querystring)
-FJson = json.loads(response.text)
-paging = json.dumps(FJson["paging"])
-pag = json.loads(paging)
-pages = pag['pages']
-total = pag['total']
-start = 1
 
+fromD = 1
+toD = 2
+page = 1
 
-def get_order_list(pages,total):
-	url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders"
-	querystring = {"page":"30","pageSize":""+str(total)+"","orderByAsc":"true"}
-	headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-	response = requests.request("GET", url, headers=headers, params=querystring)
-	FJson = json.loads(response.text)
-	result = json.dumps(FJson['list'])
-	text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/list.json", "w")
-	text_file.write(result)
-	text_file.close()
-	cargando_bigquery()
+def get_order_list(fromD,toD,page):
+	for x in range(30):
+		url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders?page="+page+""
+		querystring = {"f_creationDate":"creationDate:[2020-01-"+str(fromD)+"T02:00:00.000Z TO 2020-01-"+str(toD)+"T01:59:59.999Z]","f_hasInputInvoice":"false"}
+		headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+		response = requests.request("GET", url, headers=headers, params=querystring)
+		FJson = json.loads(response.text)
+		if bool(FJson['list']):
+			break
+		result = json.dumps(FJson['list'])
+		text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/list.json", "w")
+		text_file.write(result)
+		text_file.close()
+		cargando_bigquery()
+		page += 1
 
 def cargando_bigquery():
 	print("Cargando a BigQuery")
@@ -38,7 +35,7 @@ def cargando_bigquery():
 	client = bigquery.Client()
 	filename = '/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/list.json'
 	dataset_id = 'landing_zone'
-	table_id = 'test_list_order_v2'
+	table_id = 'shopstar_vtex_list_order_v2'
 	dataset_ref = client.dataset(dataset_id)
 	table_ref = dataset_ref.table(table_id)
 	job_config = bigquery.LoadJobConfig()
@@ -54,20 +51,8 @@ def cargando_bigquery():
 	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
 	print("finalizado")
 
-for x in range(1):
-	get_order_list(pages,total)
+for x in range(31):
+	get_order_list(fromD,toD,page)
+	fromD += 1
+	toD += 1
 
-'''
-QUERY = (
-    'SELECT FieldId FROM `shopstar-datalake.landing_zone.shopstar_vtex_sku_specification` WHERE FieldId is not null')
-query_job = client.query(QUERY)  
-rows = query_job.result()  
-
-for row in rows:
-    productList.append(row.FieldId)
-
-string = json.dumps(productList)
-text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SPECIFICATION_FIELD/SPECIFICATION_FIELD_ID_2.json", "w")
-text_file.write(string)
-text_file.close()
-'''
