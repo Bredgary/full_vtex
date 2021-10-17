64248c10e71b1427783f7cdf5d9bcf19501dc03f
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-16 23:02:04 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -33,61 +33,19 @@ def get_order_list(fromD,toD,page):
 
 def cargando_bigquery():
 	print("Cargando a BigQuery")
-	table_id = "shopstar-datalake.landing_zone.shopstar_vtex_list_order_v1"
 	system("cat list.json | jq -c '.[]' > list_table.json")
 	client = bigquery.Client()
 	filename = '/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/list_table.json'
-	#ataset_id = 'landing_zone'
-	#table_id = 'shopstar_vtex_list_order_v1'
-	#dataset_ref = client.dataset(dataset_id)
-	#table_ref = dataset_ref.table(table_id)
-	job_config = bigquery.LoadJobConfig(
-		schema=[
-        bigquery.SchemaField("orderId", "STRING"),
-        bigquery.SchemaField("creationDate", "DATE"),
-		bigquery.SchemaField("clientName", "STRING"),
-		bigquery.SchemaField("items", "STRING"),
-		bigquery.SchemaField("totalValue", "INTEGER"),
-		bigquery.SchemaField("paymentNames", "STRING"),
-		bigquery.SchemaField("status", "STRING"),
-		bigquery.SchemaField("statusDescription", "STRING"),
-		bigquery.SchemaField("marketPlaceOrderId", "STRING"),
-		bigquery.SchemaField("sequence", "INTEGER"),
-		bigquery.SchemaField("salesChannel", "INTEGER"),
-		bigquery.SchemaField("affiliateId", "FLOAT"),
-		bigquery.SchemaField("origin", "STRING"),
-		bigquery.SchemaField("workflowInErrorState", "BOOLEAN"),
-		bigquery.SchemaField("workflowInRetry", "BOOLEAN"),
-		bigquery.SchemaField("lastMessageUnread", "STRING"),
-		bigquery.SchemaField("ShippingEstimatedDate", "STRING"),
-		bigquery.SchemaField("ShippingEstimatedDateMax", "DATE"),
-		bigquery.SchemaField("ShippingEstimatedDateMin", "DATE"),
-		bigquery.SchemaField("orderIsComplete", "BOOLEAN"),
-		bigquery.SchemaField("listId", "STRING"),
-		bigquery.SchemaField("listType", "STRING"),
-		bigquery.SchemaField("authorizedDate", "DATE"),
-		bigquery.SchemaField("callCenterOperatorName", "STRING"),
-		bigquery.SchemaField("totalItems", "INTEGER"),
-		bigquery.SchemaField("currencyCode", "STRING"),
-		bigquery.SchemaField("hostname", "STRING"),
-		bigquery.SchemaField("invoiceOutput", "STRING"),
-		bigquery.SchemaField("invoiceInput", "STRING"),
-		bigquery.SchemaField("lastChange", "DATE"),
-		bigquery.SchemaField("isAllDelivered", "BOOLEAN"),
-		bigquery.SchemaField("isAnyDelivered", "BOOLEAN"),
-		bigquery.SchemaField("giftCardProviders", "STRING"),
-		bigquery.SchemaField("orderFormId", "STRING"),
-		bigquery.SchemaField("paymentApprovedDate", "STRING"),
-		bigquery.SchemaField("readyForHandlingDate", "STRING"),
-		bigquery.SchemaField("deliveryDates", "STRING"),
-    ],
-    source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
-	)
+	ataset_id = 'landing_zone'
+	table_id = 'shopstar_vtex_list_order_v1'
+	dataset_ref = client.dataset(dataset_id)
+	table_ref = dataset_ref.table(table_id)
+	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
 	job_config.autodetect = True
 	with open(filename, "rb") as source_file:
 		load_job = client.load_table_from_file(
 			source_file,
-			table_id,
+			dataset_ref,
 			location="southamerica-east1",  # Must match the destination dataset location.
 		job_config=job_config,)  # API request
 	load_job.result()  # Waits for table load to complete.
