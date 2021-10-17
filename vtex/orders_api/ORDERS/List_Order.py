10b29ff8df43ed3b037696aa9b937be891cfc550
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-16 23:35:26 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -23,7 +23,7 @@ def get_order_list(fromD,toD,page):
 			response = requests.request("GET", url, headers=headers, params=querystring)
 			FJson = json.loads(response.text)
 			res = FJson['list']
-			if bool(res):
+			if not res:
 				break
 			result = json.dumps(res)
 			text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/list.json", "w")
