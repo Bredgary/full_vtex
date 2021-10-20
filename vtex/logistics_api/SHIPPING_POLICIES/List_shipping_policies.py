ac6eded86486bce711e8766c3480e7f2e141683d
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-20 00:28:14 -0300

    MOD: prueba

--- vtex/logistics_api/SHIPPING_POLICIES/List_shipping_policies.py
+++ vtex/logistics_api/SHIPPING_POLICIES/List_shipping_policies.py
@@ -21,11 +21,11 @@ start = 0
 def get_user(start,headers,total):
 	try:
 		url = "https://mercury.vtexcommercestable.com.br/api/logistics/pvt/shipping-policies"
-		querystring = {"page":"1","perPage":"1","total":"39","pages":"1"}
+		querystring = {"total":""+str(total)+"","pages":""+str(start)+""}
 		response = requests.request("GET", url, headers=headers, params=querystring)
 		FJson = json.loads(response.text)
 		result = json.dumps(FJson)
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/license_manager_api/USER/items.json", "w")
+		text_file = open("/home/bred_valenzuela/full_vtex/vtex/logistics_api/SHIPPING_POLICIES/temp.json", "w")
 		text_file.write(result)
 		text_file.close()
 		print("Pagina: "+str(start))
@@ -37,11 +37,11 @@ def get_user(start,headers,total):
 def cargando_bigquery():
 	try:
 		print("Cargando a BigQuery")
-		system("cat items.json | jq -c '.[]' > table_user.json")
+		system("cat temp.json | jq -c '.[]' > table_shipping_policies.json")
 		client = bigquery.Client()
-		filename = '/home/bred_valenzuela/full_vtex/vtex/license_manager_api/USER/table_user.json'
+		filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/SHIPPING_POLICIES/table_shipping_policies.json'
 		dataset_id = 'landing_zone'
-		table_id = 'shopstar_vtex_user'
+		table_id = 'shopstar_vtex_list_shipping_policies'
 		dataset_ref = client.dataset(dataset_id)
 		table_ref = dataset_ref.table(table_id)
 		job_config = bigquery.LoadJobConfig()
