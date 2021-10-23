bd5ae14ef081d10c7447c17a1fc02375bca73a34
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-23 04:51:36 -0300

    MOD: prueba

--- vtex/logistics_api/SHIPPING_POLICIES/List_shipping_policies.py
+++ vtex/logistics_api/SHIPPING_POLICIES/List_shipping_policies.py
@@ -25,29 +25,29 @@ def get_list_shipping():
 	#	print("Error")
 
 def cargando_bigquery():
-	try:
-		print("Cargando a BigQuery")
-		#system("cat temp.json | jq -c '.[]' > table_shipping_policies.json")
-		client = bigquery.Client()
-		filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/SHIPPING_POLICIES/temp.json'
-		dataset_id = 'landing_zone'
-		table_id = 'shopstar_vtex_list_shipping_policies'
-		dataset_ref = client.dataset(dataset_id)
-		table_ref = dataset_ref.table(table_id)
-		job_config = bigquery.LoadJobConfig()
-		job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-		job_config.autodetect = True
-		with open(filename, "rb") as source_file:
-			job = client.load_table_from_file(
-				source_file,
-				table_ref,
-				location="southamerica-east1",  # Must match the destination dataset location.
-			job_config=job_config,)  # API request
-		job.result()  # Waits for table load to complete.
-		print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-		print("finalizado")
-	except:
-		print("Error")
+	#try:
+	print("Cargando a BigQuery")
+	#system("cat temp.json | jq -c '.[]' > table_shipping_policies.json")
+	client = bigquery.Client()
+	filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/SHIPPING_POLICIES/temp.json'
+	dataset_id = 'landing_zone'
+	table_id = 'shopstar_vtex_list_shipping_policies'
+	dataset_ref = client.dataset(dataset_id)
+	table_ref = dataset_ref.table(table_id)
+	job_config = bigquery.LoadJobConfig()
+	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+	job_config.autodetect = True
+	with open(filename, "rb") as source_file:
+		job = client.load_table_from_file(
+			source_file,
+			table_ref,
+			location="southamerica-east1",  # Must match the destination dataset location.
+		job_config=job_config,)  # API request
+	job.result()  # Waits for table load to complete.
+	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+	print("finalizado")
+	#except:
+	#	print("Error")
 
 for x in range(1):
 	get_list_shipping()
