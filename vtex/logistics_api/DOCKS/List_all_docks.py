8a0c36ebc88df2ea3324f9ece1b8e36701498247
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-23 06:35:45 -0300

    MOD: prueba

--- vtex/logistics_api/DOCKS/List_all_docks.py
+++ vtex/logistics_api/DOCKS/List_all_docks.py
@@ -32,27 +32,31 @@ def get_list_docks(start,headers):
 	print("Registro N° "+str(start))
 	cargando_bigquery()
 
+
 def cargando_bigquery():
-	print("Cargando a BigQuery")
-	system("cat temp.json | jq -c '.[]' > listdocks.json")
-	client = bigquery.Client()
-	filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/DOCKS/listdocks.json'
-	dataset_id = 'landing_zone'
-	table_id = 'shopstar_vtex_list_docks'
-	dataset_ref = client.dataset(dataset_id)
-	table_ref = dataset_ref.table(table_id)
-	job_config = bigquery.LoadJobConfig()
-	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-	job_config.autodetect = True
-	with open(filename, "rb") as source_file:
-		job = client.load_table_from_file(
-			source_file,
-			table_ref,
-			location="southamerica-east1",  # Must match the destination dataset location.
-		job_config=job_config,)  # API request
-	job.result()  # Waits for table load to complete.
-	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-	print("finalizado")
+	try:
+		print("Cargando a BigQuery")
+		system("cat temp.json | jq -c '.[]' > listdocks.json")
+		client = bigquery.Client()
+		filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/DOCKS/listdocks.json'
+		dataset_id = 'landing_zone'
+		table_id = 'shopstar_vtex_list_docks'
+		dataset_ref = client.dataset(dataset_id)
+		table_ref = dataset_ref.table(table_id)
+		job_config = bigquery.LoadJobConfig()
+		job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+		#job_config.autodetect = True
+		with open(filename, "rb") as source_file:
+			job = client.load_table_from_file(
+				source_file,
+				table_ref,
+				location="southamerica-east1",  # Must match the destination dataset location.
+			job_config=job_config,)  # API request
+		job.result()  # Waits for table load to complete.
+		print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+		print("finalizado")
+	except:
+		print("Vacio")
 
 for x in range(pages):
 	start += 1
