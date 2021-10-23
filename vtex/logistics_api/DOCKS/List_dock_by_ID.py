5d717b52e5fb39d55d7166c6a55397b729d0916e
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-23 07:40:51 -0300

    MOD: prueba

--- vtex/logistics_api/DOCKS/List_dock_by_ID.py
+++ vtex/logistics_api/DOCKS/List_dock_by_ID.py
@@ -12,7 +12,7 @@ client = bigquery.Client()
 productList = []
 count = 0
 
-def list_dock_by_id(id):
+def list_dock_by_id(id,count):
 	url = "https://mercury.vtexcommercestable.com.br/api/logistics/pvt/configuration/docks/"+str(id)+""
 	headers = {"Accept": "application/json; charset=utf-8","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 	response = requests.request("GET", url, headers=headers)
@@ -21,36 +21,43 @@ def list_dock_by_id(id):
 	text_file = open("/home/bred_valenzuela/full_vtex/vtex/logistics_api/DOCKS/temp.json", "w")
 	text_file.write(result)
 	text_file.close()
-	cargando_bigquery()
+	cargando_bigquery(count,result)
 	
-def cargando_bigquery():
-	print("Cargando a BigQuery")
-	#system("cat items.json | jq -c '.[]' > tableCollectionBeta.json")
-	client = bigquery.Client()
-	filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/DOCKS/temp.json'
-	dataset_id = 'landing_zone'
-	table_id = 'shopstar_vtex_list_dock_by_id'
-	dataset_ref = client.dataset(dataset_id)
-	table_ref = dataset_ref.table(table_id)
-	job_config = bigquery.LoadJobConfig()
-	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-	#job_config.autodetect = True
-	with open(filename, "rb") as source_file:
-		job = client.load_table_from_file(
-			source_file,
-			table_ref,
-			location="southamerica-east1",  # Must match the destination dataset location.
-		job_config=job_config,)  # API request
-	job.result()  # Waits for table load to complete.
-	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-	print("finalizado")
+def cargando_bigquery(count,result):
+	try:
+		print("Cargando a BigQuery")
+		#system("cat items.json | jq -c '.[]' > tableCollectionBeta.json")
+		client = bigquery.Client()
+		filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/DOCKS/temp.json'
+		dataset_id = 'landing_zone'
+		table_id = 'shopstar_vtex_list_dock_by_id'
+		dataset_ref = client.dataset(dataset_id)
+		table_ref = dataset_ref.table(table_id)
+		job_config = bigquery.LoadJobConfig()
+		job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+		#job_config.autodetect = True
+		with open(filename, "rb") as source_file:
+			job = client.load_table_from_file(
+				source_file,
+				table_ref,
+				location="southamerica-east1",  # Must match the destination dataset location.
+			job_config=job_config,)  # API request
+		job.result()  # Waits for table load to complete.
+		print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+		print("finalizado")
+	except:
+		print("Error")
+		text_file = open("/home/bred_valenzuela/full_vtex/vtex/logistics_api/DOCKS/almacenLocal/"+str(count)+"_registro.json", "w")
+		text_file.write(result)
+		text_file.close()
+
 
 def operacion_fenix(count):
 	f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/logistics_api/DOCKS/id_dock.json','r')
 	data_from_string = f_01.read()
 	listaIDS = json.loads(data_from_string)
 	for i in listaIDS:
-		list_dock_by_id(i)
+		list_dock_by_id(i,count)
 		count += 1
 		print(str(count)+" registro almacenado.")
 
