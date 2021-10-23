c7ad6925801f96d5d796404fa002646860b913d8
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-23 04:30:19 -0300

    MOD: prueba

--- vtex/license_manager_api/ACCESS_CONTROL/Check_resource_access.py
+++ vtex/license_manager_api/ACCESS_CONTROL/Check_resource_access.py
@@ -8,28 +8,19 @@ from google.cloud import bigquery
 from itertools import chain
 from collections import defaultdict
 
-url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/collection/search"
-querystring = {"page":"1","pageSize":"100","orderByAsc":"true"}
-headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-response = requests.request("GET", url, headers=headers, params=querystring)
-Json = json.loads(response.text)
-paging = Json["paging"]
-total = int(paging["total"])
-pages = int(paging["pages"])
-listItem = []
-start = 0
+
 
 def get_collection_beta(page,headers,total):
-	url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/collection/search"
-	querystring = {"page":""+str(page)+"","pageSize":""+str(total)+"","orderByAsc":"true"}
-	response = requests.request("GET", url, headers=headers, params=querystring)
+	url = "https://mercury.vtexcommercestable.com.br/api/vlm/account"
+	headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+	response = requests.request("GET", url, headers=headers)
 	FJson = json.loads(response.text)
-	result = json.dumps(FJson["items"])
-	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/COLLECTION_BETA/items.json", "w")
+	result = json.dumps(FJson)
+	text_file = open("/home/bred_valenzuela/full_vtex/vtex/license_manager_api/APP_KEYS/temp.json", "w")
 	text_file.write(result)
 	text_file.close()
-	print("Pagina: "+str(page))
 	cargando_bigquery()
+
 
 def cargando_bigquery():
 	print("Cargando a BigQuery")
