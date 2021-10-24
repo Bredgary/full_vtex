6d91eddc5c4f841d65f1efca98c2e97e2a73978f
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-24 00:25:45 -0300

    MOD: prueba

--- vtex/logistics_api/INVENTORY/List_inventory_by_SKU.py
+++ vtex/logistics_api/INVENTORY/List_inventory_by_SKU.py
@@ -10,46 +10,55 @@ from collections import defaultdict
 
 client = bigquery.Client()
 productList = []
+count=0
 
-'''
-def list_inventory_by_sku(page,headers,total):
-	url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/collection/search"
-	querystring = {"page":""+str(page)+"","pageSize":""+str(total)+"","orderByAsc":"true"}
-	response = requests.request("GET", url, headers=headers, params=querystring)
+def list_inventory_by_sku(id,count):
+	url = "https://mercury.vtexcommercestable.com.br/api/logistics/pvt/inventory/skus/"+str(id)+""
+	response = requests.request("GET", url, headers=headers)
 	FJson = json.loads(response.text)
-	result = json.dumps(FJson["items"])
-	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/COLLECTION_BETA/items.json", "w")
+	result = json.dumps(FJson["balance"])
+	text_file = open("/home/bred_valenzuela/full_vtex/vtex/logistics_api/INVENTORY/temp.json", "w")
 	text_file.write(result)
 	text_file.close()
-	print("Pagina: "+str(page))
-	cargando_bigquery()
+	print("Registro: "+str(count))
+	cargando_bigquery(result,count)
 
-def cargando_bigquery():
-	print("Cargando a BigQuery")
-	system("cat items.json | jq -c '.[]' > tableCollectionBeta.json")
-	filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/COLLECTION_BETA/tableCollectionBeta.json'
-	dataset_id = 'landing_zone'
-	table_id = 'shopstar_vtex_collection_beta'
-	dataset_ref = client.dataset(dataset_id)
-	table_ref = dataset_ref.table(table_id)
-	job_config = bigquery.LoadJobConfig()
-	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-	job_config.autodetect = True
-	with open(filename, "rb") as source_file:
-		job = client.load_table_from_file(
-			source_file,
-			table_ref,
-			location="southamerica-east1",  # Must match the destination dataset location.
-		job_config=job_config,)  # API request
-	job.result()  # Waits for table load to complete.
-	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-	print("finalizado")
-	system("rm items.json")
-	system("rm tableCollectionBeta.json")
+def cargando_bigquery(result,count):
+	try:
+		print("Cargando a BigQuery")
+		system("cat temp.json | jq -c '.[]' > list_inventory.json")
+		client = bigquery.Client()
+		filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/INVENTORY/list_inventory.json'
+		dataset_id = 'landing_zone'
+		table_id = 'shopstar_vtex_list_inventory_by_sku'
+		dataset_ref = client.dataset(dataset_id)
+		table_ref = dataset_ref.table(table_id)
+		job_config = bigquery.LoadJobConfig()
+		job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+		job_config.autodetect = True
+		with open(filename, "rb") as source_file:
+			job = client.load_table_from_file(
+				source_file,
+				table_ref,
+				location="southamerica-east1",  # Must match the destination dataset location.
+			job_config=job_config,)  # API request
+		job.result()  # Waits for table load to complete.
+		print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+		print("finalizado")
+	except:
+		print("Vacio")
+		text_file = open("/home/bred_valenzuela/full_vtex/vtex/logistics_api/INVENTORY/almacenLocal/"+str(count)+"_regstro.json", "w")
+		text_file.write(result)
+		text_file.close()
 
-for x in range(pages):
-	start += 1
-	get_collection_beta(start,headers,total)
+def operacion_fenix(count):
+	f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/logistics_api/INVENTORY/id_sku.json','r')
+	data_from_string = f_01.read()
+	listaIDS = json.loads(data_from_string)
+	for i in listaIDS:
+		list_inventory_by_sku(i,count)
+
+operacion_fenix(count)
 
 '''
 QUERY = (
@@ -64,7 +73,7 @@ string = json.dumps(productList)
 text_file = open("/home/bred_valenzuela/full_vtex/vtex/logistics_api/INVENTORY/id_sku.json", "w")
 text_file.write(string)
 text_file.close()
-
+'''
 
 
 
