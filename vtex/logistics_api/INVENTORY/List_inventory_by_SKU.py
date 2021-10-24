2f137d21549e21b8e971b999930ff10b3d61b0ae
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-24 03:45:30 -0300

    MOD: prueba

--- vtex/logistics_api/INVENTORY/List_inventory_by_SKU.py
+++ vtex/logistics_api/INVENTORY/List_inventory_by_SKU.py
@@ -22,36 +22,36 @@ def list_inventory_by_sku(id,count):
 	text_file.write(result)
 	text_file.close()
 	print("Registro: "+str(count))
-	if count >= 558:
+	if count >= 4411:
 		cargando_bigquery(result,count)
 
 def cargando_bigquery(result,count):
-	try:
-		print("Cargando a BigQuery")
-		system("cat temp.json | jq -c '.[]' > list_inventory.json")
-		client = bigquery.Client()
-		filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/INVENTORY/list_inventory.json'
-		dataset_id = 'landing_zone'
-		table_id = 'shopstar_vtex_list_inventory_by_sku'
-		dataset_ref = client.dataset(dataset_id)
-		table_ref = dataset_ref.table(table_id)
-		job_config = bigquery.LoadJobConfig()
-		job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-		#job_config.autodetect = True
-		with open(filename, "rb") as source_file:
-			job = client.load_table_from_file(
-				source_file,
-				table_ref,
-				location="southamerica-east1",  # Must match the destination dataset location.
-			job_config=job_config,)  # API request
-		job.result()  # Waits for table load to complete.
-		print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-		print("finalizado")
-	except:
-		print("Vacio")
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/logistics_api/INVENTORY/almacenLocal/"+str(count)+"_regstro.json", "w")
-		text_file.write(result)
-		text_file.close()
+	#try:
+	print("Cargando a BigQuery")
+	system("cat temp.json | jq -c '.[]' > list_inventory.json")
+	client = bigquery.Client()
+	filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/INVENTORY/list_inventory.json'
+	dataset_id = 'landing_zone'
+	table_id = 'shopstar_vtex_list_inventory_by_sku'
+	dataset_ref = client.dataset(dataset_id)
+	table_ref = dataset_ref.table(table_id)
+	job_config = bigquery.LoadJobConfig()
+	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+	#job_config.autodetect = True
+	with open(filename, "rb") as source_file:
+		job = client.load_table_from_file(
+			source_file,
+			table_ref,
+			location="southamerica-east1",  # Must match the destination dataset location.
+		job_config=job_config,)  # API request
+	job.result()  # Waits for table load to complete.
+	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+	print("finalizado")
+	#except:
+	#	print("Vacio")
+	#	text_file = open("/home/bred_valenzuela/full_vtex/vtex/logistics_api/INVENTORY/almacenLocal/"+str(count)+"_regstro.json", "w")
+	#	text_file.write(result)
+	#	text_file.close()
 
 def operacion_fenix(count):
 	f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/logistics_api/INVENTORY/id_sku.json','r')
