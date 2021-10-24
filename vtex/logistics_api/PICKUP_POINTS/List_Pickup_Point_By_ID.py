ebc26ced25b59b53d499542b2668ba0a394ad80d
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-24 05:52:28 -0300

    MOD: prueba

---
+++ vtex/logistics_api/PICKUP_POINTS/List_Pickup_Point_By_ID.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import os
+import re
+from datetime import datetime
+from os import system
+from google.cloud import bigquery
+from itertools import chain
+from collections import defaultdict
+
+
+def get_list_pickup_points():
+	url = "https://mercury.vtexcommercestable.com.br/api/logistics/pvt/configuration/pickuppoints/198507d"
+	headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+	response = requests.request("GET", url, headers=headers)
+	FJson = json.loads(response.text)
+	result = json.dumps(FJson)
+	text_file = open("/home/bred_valenzuela/full_vtex/vtex/logistics_api/PICKUP_POINTS/items.json", "w")
+	text_file.write(result)
+	text_file.close()
+	cargando_bigquery()
+
+def cargando_bigquery():
+	print("Cargando a BigQuery")
+	#system("cat items.json | jq -c '.[]' > tablePickup.json")
+	client = bigquery.Client()
+	filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/PICKUP_POINTS/items.json'
+	dataset_id = 'landing_zone'
+	table_id = 'shopstar_vtex_list_pickup_points_by_id'
+	dataset_ref = client.dataset(dataset_id)
+	table_ref = dataset_ref.table(table_id)
+	job_config = bigquery.LoadJobConfig()
+	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+	job_config.autodetect = True
+	with open(filename, "rb") as source_file:
+		job = client.load_table_from_file(
+			source_file,
+			table_ref,
+			location="southamerica-east1",  # Must match the destination dataset location.
+		job_config=job_config,)  # API request
+	job.result()  # Waits for table load to complete.
+	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
+	print("finalizado")
+
+for x in range(1):
+	get_list_pickup_points()
