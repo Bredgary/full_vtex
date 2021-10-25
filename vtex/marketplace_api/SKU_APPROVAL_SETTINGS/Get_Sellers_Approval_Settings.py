59467ea3d360b0810d3f4052c9a600b5bc8d41f8
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-25 02:53:41 -0300

    MOD: prueba

--- vtex/marketplace_api/SKU_APPROVAL_SETTINGS/Get_Sellers_Approval_Settings.py
+++ vtex/marketplace_api/SKU_APPROVAL_SETTINGS/Get_Sellers_Approval_Settings.py
@@ -8,30 +8,35 @@ from google.cloud import bigquery
 from itertools import chain
 from collections import defaultdict
 
+client = bigquery.Client()
+productList = []
+count = 0
 
-def get_list_shipping():
+def get_sellers_approval_settings(x,count):
 	#try:
-	url = "https://mercury.vtexcommercestable.com.br/api/logistics/pvt/shipping-policies"
-	querystring = {"page":"1","perPage":"39"}
+	url = "https://api.vtex.com/mercury/suggestions/configuration/seller/undefined"
+	querystring = {"sellerId":""+x+""}
 	headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 	response = requests.request("GET", url, headers=headers, params=querystring)
-	FJson = json.loads(response.text)
-	result = json.dumps(FJson["items"])
-	text_file = open("/home/bred_valenzuela/full_vtex/vtex/logistics_api/SHIPPING_POLICIES/temp.json", "w")
-	text_file.write(result)
-	text_file.close()
-	cargando_bigquery()
+	if response.text:
+		FJson = json.loads(response.text)
+		result = json.dumps(FJson)
+		text_file = open("/home/bred_valenzuela/full_vtex/vtex/marketplace_api/SKU_APPROVAL_SETTINGS/items2.json", "w")
+		text_file.write(result)
+		text_file.close()
+		cargando_bigquery()
+	else:
+		print("Vacio")
 	#except:
 	#	print("Error")
 
 def cargando_bigquery():
 	#try:
 	print("Cargando a BigQuery")
-	system("cat temp.json | jq -c '.[]' > table_shipping_policies.json")
-	client = bigquery.Client()
-	filename = '/home/bred_valenzuela/full_vtex/vtex/logistics_api/SHIPPING_POLICIES/table_shipping_policies.json'
+	#system("cat temp.json | jq -c '.[]' > table_shipping_policies.json")
+	filename = '/home/bred_valenzuela/full_vtex/vtex/marketplace_api/SKU_APPROVAL_SETTINGS/items2.json'
 	dataset_id = 'landing_zone'
-	table_id = 'shopstar_vtex_list_shipping_policies'
+	table_id = 'shopstar_vtex_autoapprove_status_in_account_settings'
 	dataset_ref = client.dataset(dataset_id)
 	table_ref = dataset_ref.table(table_id)
 	job_config = bigquery.LoadJobConfig()
@@ -49,5 +54,30 @@ def cargando_bigquery():
 	#except:
 	#	print("Error")
 
-for x in range(1):
-	get_list_shipping()
+def operacion_fenix(count):
+	f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/marketplace_api/SKU_APPROVAL_SETTINGS/id_seller.json','r')
+	data_from_string = f_01.read()
+	listaIDS = json.loads(data_from_string)
+	for i in range(int(len(listaIDS))):
+		x = listaIDS[i]
+		get_sellers_approval_settings(x,count)
+		count += 1
+		print(str(count)+" registro almacenado.")
+
+
+operacion_fenix(count)
+
+'''
+QUERY = (
+    'SELECT SellerId FROM `shopstar-datalake.landing_zone.shopstar_vtex_seller`')
+query_job = client.query(QUERY)  
+rows = query_job.result()  
+
+for row in rows:
+    productList.append(row.SellerId)
+
+string = json.dumps(productList)
+text_file = open("/home/bred_valenzuela/full_vtex/vtex/marketplace_api/SKU_APPROVAL_SETTINGS/id_seller.json", "w")
+text_file.write(string)
+text_file.close()
+'''
