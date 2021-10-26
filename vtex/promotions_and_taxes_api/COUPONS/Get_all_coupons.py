03d738e1fbce5cbb29e1bfa22edfeec8e50977a9
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-26 12:02:21 -0300

    MOD: prueba

--- vtex/promotions_and_taxes_api/COUPONS/Get_all_coupons.py
+++ vtex/promotions_and_taxes_api/COUPONS/Get_all_coupons.py
@@ -8,22 +8,24 @@ from google.cloud import bigquery
 from itertools import chain
 from collections import defaultdict
 
-def :
+def all_coupon():
+	url = "https://mercury.vtexcommercestable.com.br/api/rnb/pvt/coupon"
+	headers = {"Accept": "application/json; charset=utf-8","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+	response = requests.request("GET", url, headers=headers)
 	FJson = json.loads(response.text)
 	result = json.dumps(FJson)
-	text_file = open("items.json", "w")
+	text_file = open("/home/bred_valenzuela/full_vtex/vtex/promotions_and_taxes_api/COUPONS//items.json", "w")
 	text_file.write(result)
 	text_file.close()
-	print("Registro: "+str())
 	cargando_bigquery()
 
 def cargando_bigquery():
 	print("Cargando a BigQuery")
-	system("cat items.json | jq -c '.[]' > .json")
+	system("cat items.json | jq -c '.[]' > all_coupon.json")
 	client = bigquery.Client()
-	filename = '.json'
+	filename = '/home/bred_valenzuela/full_vtex/vtex/promotions_and_taxes_api/COUPONS/all_coupon.json'
 	dataset_id = 'landing_zone'
-	table_id = ''
+	table_id = 'vtex_shopstar_all_coupon'
 	dataset_ref = client.dataset(dataset_id)
 	table_ref = dataset_ref.table(table_id)
 	job_config = bigquery.LoadJobConfig()
@@ -38,6 +40,8 @@ def cargando_bigquery():
 	job.result()  # Waits for table load to complete.
 	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
 	print("finalizado")
+
+all_coupon()
 
 '''
 QUERY = (
