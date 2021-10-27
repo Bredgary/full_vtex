c628f88804d380f897efbce27411db69e8621f33
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-27 03:52:12 -0300

    MOD: prueba

--- vtex/search_api/OFFERS/Search_SKU_offers.py
+++ vtex/search_api/OFFERS/Search_SKU_offers.py
@@ -28,14 +28,16 @@ def search_SKU_offers(id,count):
 		if count >= 32:
 			cargando_bigquery()
 	except:
+		print("tabla vacia")
 		url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pub/products/offers/"+str(id[1])+"/sku/"+str(id[0])+""
 		headers = {"Accept": "application/json; charset=utf-8","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 		response = requests.request("GET", url, headers=headers)
-		FJson = json.loads(response.text)
-		result = json.dumps(FJson)
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/search_api/OFFERS/respaldo/items.json", "w")
-		text_file.write(result)
-		text_file.close()
+		if response:
+			FJson = json.loads(response.text)
+			result = json.dumps(FJson)
+			text_file = open("/home/bred_valenzuela/full_vtex/vtex/search_api/OFFERS/respaldo/items.json", "w")
+			text_file.write(result)
+			text_file.close()
 
 def cargando_bigquery():
 	print("Cargando a BigQuery")
