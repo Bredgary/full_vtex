d25a2ba7c8ac53a382b86345098cab659c415eff
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-27 04:44:19 -0300

    MOD: prueba

--- vtex/search_api/FACETS/Search_by_Store_Facets.py
+++ vtex/search_api/FACETS/Search_by_Store_Facets.py
@@ -25,8 +25,9 @@ def search_by_store_facets(id,count):
 	text_file = open("/home/bred_valenzuela/full_vtex/vtex/search_api/FACETS/items.json", "w")
 	text_file.write(result)
 	text_file.close()
-	print("Registro N°: "+str(count))
-	cargando_bigquery()
+	print(type(FJson))
+	#print("Registro N°: "+str(count))
+	#cargando_bigquery()
 	#except:
 	#	url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pub/facets/search/"+str(id)+""
 	#	querystring = {"map":"c"}
