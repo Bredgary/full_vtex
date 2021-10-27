7a36d1527aae323808e4003f78e39c23df51b583
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-27 05:18:24 -0300

    MOD: prueba

--- vtex/search_api/FACETS/Search_by_Store_Facets.py
+++ vtex/search_api/FACETS/Search_by_Store_Facets.py
@@ -26,9 +26,10 @@ def search_by_store_facets(id,count):
 	Summary = FJson["Summary"]
 	while("" in FJson) :
 		FJson.remove("")
-	while("" in Summary["SpecificationFilters"]) :
-		Summary["SpecificationFilters"].remove("")
-	print(Summary)
+	FJson["Summary"].remove("")
+	#while("" in Summary["SpecificationFilters"]) :
+	#	Summary["SpecificationFilters"].remove("")
+	#print(Summary)
 	#result = json.dumps(FJson)
 	#text_file = open("/home/bred_valenzuela/full_vtex/vtex/search_api/FACETS/items.json", "w")
 	#text_file.write(result)
