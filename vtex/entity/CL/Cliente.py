c81d681e1f210e8500a609a448edbad9bb638db5
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-29 04:19:15 -0300

    MOD: prueba

--- vtex/entity/CL/Cliente.py
+++ vtex/entity/CL/Cliente.py
@@ -44,7 +44,7 @@ def format_schema(schema):
 
 
 df = pd.DataFrame(cl_client(),
-columns=['beneficio','beneficio2','document','crearGiftcard','profilePicture','proteccionDatos','terminosCondiciones','terminosPago','isCorporate'])
+columns=['beneficio','beneficio2','document','crearGiftcard','profilePicture','proteccionDatos','terminosCondiciones','terminosPago'])
 df.reset_index(drop=True, inplace=True)
 
 json_data = df.to_json(orient = 'records')
