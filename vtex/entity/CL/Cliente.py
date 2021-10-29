c9c12711f490a0e4bdce6de839e3da951f262d75
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-29 04:53:17 -0300

    MOD: prueba

--- vtex/entity/CL/Cliente.py
+++ vtex/entity/CL/Cliente.py
@@ -44,7 +44,7 @@ def format_schema(schema):
 
 
 df = pd.DataFrame(cl_client(),
-columns=['beneficio','beneficio2','crearGiftcard','profilePicture','proteccionDatos','terminosCondiciones','terminosPago','tradeName','rclastcart','rclastsession','rclastsessiondate','homePhone','phone','stateRegistration','email','userId','firstName','lastName','document','localeDefault','attach','approved','birthDate','businessPhone','carttag','corporateDocument','corporateName','documentType'])
+columns=['beneficio','beneficio2','crearGiftcard','profilePicture','proteccionDatos','terminosCondiciones','terminosPago','tradeName','rclastcart','rclastsession','rclastsessiondate','homePhone','phone','stateRegistration','email','userId','firstName','lastName','document','localeDefault','attach','approved','birthDate','businessPhone','corporateDocument','corporateName','documentType'])
 df.reset_index(drop=True, inplace=True)
 
 json_data = df.to_json(orient = 'records')
