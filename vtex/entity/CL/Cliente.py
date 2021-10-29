14f49c4fcb4e2941278082e852a6f1335bc55510
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-29 04:07:36 -0300

    MOD: prueba

--- vtex/entity/CL/Cliente.py
+++ vtex/entity/CL/Cliente.py
@@ -24,7 +24,7 @@ format = now.strftime('%Y-%m-%d')
 
 def cl_client():
 	url = "https://mercury.vtexcommercestable.com.br/api/dataentities/CL/search"
-	querystring = {"_fields":"id,firstName,lastName,email,accountId,accountName,dataEntityId","_where":"createdIn="+format+""}
+	querystring = {"_fields":"beneficio,beneficio2,id,firstName,lastName,email,accountId,accountName,dataEntityId","_where":"createdIn="+format+""}
 	headers = {
 		"Content-Type": "application/json",
 		"Accept": "application/vnd.vtex.ds.v10+json",
@@ -44,7 +44,7 @@ def format_schema(schema):
 
 
 df = pd.DataFrame(cl_client(),
-columns=['id','firstName','lastName','email','accountId','accountName','dataEntityId'])
+columns=['beneficio','beneficio2''id','firstName','lastName','email','accountId','accountName','dataEntityId'])
 df.reset_index(drop=True, inplace=True)
 
 json_data = df.to_json(orient = 'records')
