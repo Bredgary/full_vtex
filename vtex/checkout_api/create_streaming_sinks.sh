375e61b12c1f04932ecfc8c273c48e78414ef26d
bred <33434436+Bredgary@users.noreply.github.com>
2021-10-31 21:33:36 -0300

    Update create_streaming_sinks.sh

--- vtex/checkout_api/create_streaming_sinks.sh
+++ vtex/checkout_api/create_streaming_sinks.sh
@@ -6,7 +6,7 @@ PROJECT_ID=$(gcloud config get-value project)
 # GCS buckets
 #TODO: Add try/catch for the first bucket since qwiklabs
 gsutil mb -l US gs://streaming_vtex
-gsutil mb -l US -c "COLDLINE_VTEX" gs://$PROJECT_ID-coldlinevtex
+gsutil mb -l US -c "COLDLINE_VTEX" gs://coldlinevtex-coldlinevtex
 
 # BiqQuery Dataset
 bq mk --location=US logs
