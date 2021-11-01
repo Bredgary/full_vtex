0ddd1c0e73410f8afacfcb3658c5fc0a5066287f
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-31 22:14:35 -0300

    MOD: prueba

--- vtex/checkout_api/SHOPPING_CART/Get_current_cart_or_create_a_new_one.py
+++ vtex/checkout_api/SHOPPING_CART/Get_current_cart_or_create_a_new_one.py
@@ -42,15 +42,15 @@ def cargando_bigquery():
     dataset_ref = client.dataset(dataset_id)
     table_ref = dataset_ref.table(table_id)
     job_config = bigquery.LoadJobConfig()
-    job_config.autodetect = True
     job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+    job_config.autodetect = True
     with open(filename, "rb") as source_file:
         job = client.load_table_from_file(
             source_file,
             table_ref,
-            location="southamerica-east1",  
-            job_config=job_config,) 
-	job.result()  
+            location="southamerica-east1",
+            job_config=job_config,)
+        job.result()
 	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
 	print("finalizado")
 
