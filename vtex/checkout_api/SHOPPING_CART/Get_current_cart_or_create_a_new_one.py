61cc74c48109561e4444d9ad70f8f8faec78d2a6
bredg <bredg@LAPTOP-77FGOG5A>
2021-10-31 22:06:41 -0300

    MOD: prueba

--- vtex/checkout_api/SHOPPING_CART/Get_current_cart_or_create_a_new_one.py
+++ vtex/checkout_api/SHOPPING_CART/Get_current_cart_or_create_a_new_one.py
@@ -43,9 +43,9 @@ def cargando_bigquery():
 	dataset_ref = client.dataset(dataset_id)
 	table_ref = dataset_ref.table(table_id)
 	job_config = bigquery.LoadJobConfig()
-	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
     job_config.autodetect = True
-	with open(filename, "rb") as source_file:
+	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+    with open(filename, "rb") as source_file:
 		job = client.load_table_from_file(
 			source_file,
 			table_ref,
