14a04b66e0e425d3fa9a327c5c30d7001bf4290e
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-02 23:54:35 -0300

    MOD: prueba

--- vtex/prueba.py
+++ vtex/prueba.py
@@ -3,3 +3,4 @@ headers={'x-api-key':'dapi879a141a514273c14060301b39ff2ce2'}
 url = "https://4191376270202696.6.gcp.databricks.com/api/2.1/jobs/list"
 response = requests.request("POST", url, headers=headers)
 
+print(response)
