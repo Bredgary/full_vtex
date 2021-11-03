65f0817feda952d0283acd7ff0fbd37d9b0deac5
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-03 01:06:35 -0300

    MOD: prueba

--- vtex/prueba.py
+++ vtex/prueba.py
@@ -1,16 +1,13 @@
 import requests
 
-DOMAIN = 'https://4191376270202696.6.gcp.databricks.com/'
-TOKEN = 'dapi879a141a514273c14060301b39ff2ce2'
+url = "https://4191376270202696.6.gcp.databricks.com/api/2.1/jobs/list"
 
-response = requests.post(
-  'https://%s/api/2.1/jobs/list' % (DOMAIN),
-  headers={'Authorization': 'Bearer %s' % TOKEN},
-)
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "DATABRICKS_TOKEN": "dapi879a141a514273c14060301b39ff2ce2"
+}
 
-if response.status_code == 200:
-  print(response.json())
-else:
-  print("Error launching cluster: %s: %s" % (response.json()["error_code"], response.json()["message"]))
+response = requests.request("GET", url, headers=headers)
 
-
+print(response)
