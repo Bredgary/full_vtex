efaf315b8a2e8a42fb49e81e17c72b4e526c2e60
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-02 23:51:22 -0300

    MOD: prueba

--- vtex/prueba.py
+++ vtex/prueba.py
@@ -1,6 +1,23 @@
 import requests
-headers={'x-api-key':'dapi879a141a514273c14060301b39ff2ce2'}
-url = "https://<databricks-instance>/api/2.1/jobs/list"
-response = requests.request("GET", url, headers=headers)
 
-print(response.text)
+DOMAIN = 'https://4191376270202696.6.gcp.databricks.com/'
+TOKEN = 'dapi879a141a514273c14060301b39ff2ce2'
+
+response = requests.post(
+  'https://%s/api/2.0/clusters/create' % (DOMAIN),
+  headers={'Authorization': 'Bearer %s' % TOKEN},
+  json={
+    "cluster_name": "shopstar",
+    "spark_version": "3.1.1",
+    "node_type_id": "n1-standard-4",
+    "spark_env_vars": {
+      "PYSPARK_PYTHON": "/databricks/python3/bin/python3"
+    },
+    "num_workers": "8.3"
+  }
+)
+
+if response.status_code == 200:
+  print(response.json()['cluster_id'])
+else:
+  print("Error launching cluster: %s: %s" % (response.json()["error_code"], response.json()["message"]))
