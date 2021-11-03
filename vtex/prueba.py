df9ac904dc99e6c1486c024b5eb3d6c8a1ce9fee
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-02 22:01:22 -0300

    MOD: prueba

---
+++ vtex/prueba.py
@@ -0,0 +1,11 @@
+import pycurl
+from io import BytesIO
+
+#b_obj = BytesIO()
+#crl = pycurl.Curl()
+#crl.setopt(crl.URL, 'https://wiki.python.org/moin/BeginnersGuide'
+#crl.setopt(crl.WRITEDATA, b_obj)
+#crl.perform()
+#crl.close()
+#get_body = b_obj.getvalue()
+#print('Output of GET request:\n%s' % get_body.decode('utf8'))
