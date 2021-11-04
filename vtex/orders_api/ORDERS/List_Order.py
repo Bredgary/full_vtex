3ead31bd989cc78cb6753ea68b3fc4909635e975
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-04 20:54:51 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -15,32 +15,7 @@ class Init:
 	yesterday = today - datetime.timedelta(days=1)
 	before_yesterday = today - datetime.timedelta(days=2)
 	
-class Params:
-	orderId:str = None
-	creationDate:str = None
-	clientName:str = None
-	totalValue:str = None
-	paymentNames:str = None
-	status:str = None
-	statusDescription:str = None
-	marketPlaceOrderId:str = None
-	sequence:str = None
-	salesChannel:str = None
-	affiliateId:str = None
-	origin:str = None
-	workflowInErrorState:str = None
-	workflowInRetry:str = None
-	lastMessageUnread:str = None
-	ShippingEstimatedDate:str = None
-	ShippingEstimatedDateMax:str = None
-	ShippingEstimatedDateMin:str = None
-	orderIsComplete:str = None
-	listId:str = None
-	listType:str = None
-	authorizedDate:str = None
-	callCenterOperatorName:str = None
-	totalItems:str = None
-	currencyCode:str = None
+
 
 def format_schema(schema):
     formatted_schema = []
@@ -68,57 +43,124 @@ def paging():
 def run():
     FJson = paging()
     df = pd.DataFrame({
-		Params.orderId: FJson["orderId"]}, index=[0])
+		'orderId': FJson["orderId"]}, index=[0])
 
     df.reset_index(drop=True, inplace=True)
     json_data = df.to_json(orient = 'records')
     json_object = json.loads(json_data)
     print(json_object)
-    '''
-    table_schema = {
-        "name": "orderFormId",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "salesChannel",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "loggedIn",
-        "type": "BOOL",
-        "mode": "NULLABLE"
-    },{
-        "name": "isCheckedIn",
-        "type": "BOOL",
-        "mode": "NULLABLE"
-    },{
-        "name": "storeId",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "checkedInPickupPointId",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "allowManualPrice",
-        "type": "BOOL",
-        "mode": "NULLABLE"
-    },{
-        "name": "canEditData",
-        "type": "BOOL",
-        "mode": "NULLABLE"
-    },{
-        "name": "userProfileId",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "userType",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "value",
-        "type": "INTEGER",
-        "mode": "NULLABLE"}
+    
+    table_schema = [
+		{
+			"name": "orderId",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "creationDate",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "clientName",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "totalValue",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "paymentNames",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "status",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "statusDescription",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "marketPlaceOrderId",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "sequence",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "salesChannel",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "affiliateId",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "origin",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "workflowInErrorState",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "workflowInRetry",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "lastMessageUnread",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "ShippingEstimatedDate",
+			"type": "STRING",
+			"mode": "NULLABLE"
+		},{
+			"name": "ShippingEstimatedDateMax",
+			"type": "STRING",
+			"mode": "NULLABLE"
+			},
+  {
+    "name": "ShippingEstimatedDateMin",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "orderIsComplete",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "listId",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "listType",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "authorizedDate",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "callCenterOperatorName",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "totalItems",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "currencyCode",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  }
+]
 
     project_id = '999847639598'
     dataset_id = 'landing_zone'
@@ -133,7 +175,7 @@ def run():
     job_config.schema = format_schema(table_schema)
     job = client.load_table_from_json(json_object, table, job_config = job_config)
     print(job.result())
-	'''
+	
 run()
 
 
