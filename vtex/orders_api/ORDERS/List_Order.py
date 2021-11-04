6a6fa098ed05af0c9333d29b16c0103b33cb31c3
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-04 01:26:01 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -2,13 +2,14 @@ import requests
 import json
 import os
 import re
-from datetime import datetime
+from datetime import date
+from datetime import timedelta
 from os import system
 from google.cloud import bigquery
 from itertools import chain
 from collections import defaultdict
 
-today = datetime.date.today()
+today = date.today()
 yesterday = today - datetime.timedelta(days=1)
 before_yesterday = today - datetime.timedelta(days=2)
 limiteDePaginacion = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
