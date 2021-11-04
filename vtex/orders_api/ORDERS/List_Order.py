817f2ee9547f9bf6992e276a1cfdf374ac460b2d
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-04 01:16:21 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -9,31 +9,28 @@ from itertools import chain
 from collections import defaultdict
 
 
-fromD = 1
-toD = 2
-page = 1
+yesterday = today - datetime.timedelta(days=1)
+before_yesterday = today - datetime.timedelta(days=2)
+limiteDePaginacion = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 
+print("Ayer: "+str(yesterday))
+print("Antes de ayer: "+str(before_yesterday))
 
-def get_order_list(fromD,toD,page):
-	for x in range(30):
-		try:
-			url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders?page="+str(page)+""
-			querystring = {"f_creationDate":"creationDate:[2020-11-"+str(fromD)+"T02:00:00.000Z TO 2020-11-"+str(toD)+"T01:59:59.999Z]","f_hasInputInvoice":"false"}
-			headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-			response = requests.request("GET", url, headers=headers, params=querystring)
-			FJson = json.loads(response.text)
-			res = FJson['list']
-			result = json.dumps(res)
-			text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/list.json", "w")
-			text_file.write(result)
-			text_file.close() 
-			print("Pagina: "+str(page))
-			print("Desde: "+str(fromD)+" Hasta: "+str(toD))
-			cargando_bigquery()
-			page +=1
-		except:
-			print("Lista Vacia")
-			print("From: "+str(fromD)+" To:"+str(toD))
+'''
+def get_order_list(fromD,toD):
+	url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders?page="+str(page)+""
+	querystring = {"f_creationDate":"creationDate:[20"+str(year)+"-"+str(mouth)+"-"+str(yesterday)+"T02:00:00.000Z TO 20"+str(year)+"-"+str(mouth)+"-"+str(before_yesterday)+"T01:59:59.999Z]","f_hasInputInvoice":"false"}
+	headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+	response = requests.request("GET", url, headers=headers, params=querystring)
+	FJson = json.loads(response.text)
+	res = FJson['list']
+	result = json.dumps(res)
+	text_file = open("/home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/list.json", "w")
+	text_file.write(result)
+	text_file.close() 
+	print("Pagina: "+str(page))
+	print("Desde: "+str(fromD)+" Hasta: "+str(toD))
+	cargando_bigquery()
 
 def cargando_bigquery():
 	print("Cargando a BigQuery")
@@ -46,7 +43,6 @@ def cargando_bigquery():
 	table_ref = dataset_ref.table(table_id)
 	job_config = bigquery.LoadJobConfig()
 	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-	#job_config.autodetect = True
 	with open(filename, "rb") as source_file:
 		job = client.load_table_from_file(
 			source_file,
@@ -61,5 +57,5 @@ for x in range(31):
 	get_order_list(fromD,toD,page)
 	fromD += 1
 	toD += 1
-
+'''
 
