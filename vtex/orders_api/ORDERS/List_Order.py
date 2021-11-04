030d5dedb2d8dfb6fce3af49d2431ef9f698d198
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-04 01:22:07 -0300

    Merge branch 'main' of https://github.com/Bredgary/full_vtex.git into main

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -8,7 +8,7 @@ from google.cloud import bigquery
 from itertools import chain
 from collections import defaultdict
 
-
+today = datetime.date.today()
 yesterday = today - datetime.timedelta(days=1)
 before_yesterday = today - datetime.timedelta(days=2)
 limiteDePaginacion = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
