dec7c0610f8c630602468769c89efbdbae57d420
Wiito1877 <69471943+Wiito1877@users.noreply.github.com>
2021-11-11 14:27:48 -0300

    mod: quote 16454

--- vtex/logistics_api/INVENTORY/List_inventory_by_SKU.py
+++ vtex/logistics_api/INVENTORY/List_inventory_by_SKU.py
@@ -22,7 +22,7 @@ def list_inventory_by_sku(id,count):
 	text_file.write(result)
 	text_file.close()
 	print("Registro: "+str(count))
-	if count >= 14833:
+	if count >= 16454:
 		cargando_bigquery(result,count)
 
 def cargando_bigquery(result,count):
