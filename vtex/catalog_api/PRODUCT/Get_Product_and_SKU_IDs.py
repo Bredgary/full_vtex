132c65e8fab5acd2b0089be9ffd7f681ec9f7319
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-13 15:08:50 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
@@ -27,10 +27,10 @@ def get_params():
 	client = bigquery.Client()
 	QUERY = (
 		'SELECT id FROM `shopstar-datalake.landing_zone.shopstar_vtex_category` where predecessor= 0')
-		query_job = client.query(QUERY)  
-		rows = query_job.result()  
-		for row in rows:
-			init.productList.append(row.id)
+	query_job = client.query(QUERY)  
+	rows = query_job.result()
+	for row in rows:
+		init.productList.append(row.id)
 	for x in init.productList:
 		get_productID(x,1,50)
 
