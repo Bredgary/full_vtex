7059f05e1d637fda3d085007643df9efcdc230ff
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-13 18:02:13 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
@@ -79,7 +79,7 @@ def run():
 	job_config = bigquery.LoadJobConfig()
 	job_config.write_disposition = "WRITE_TRUNCATE"
 	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-	#job_config.schema = format_schema(table_schema)
+	job_config.schema = format_schema(table_schema)
 	job = client.load_table_from_json(json_object, table, job_config = job_config)
 	print(job.result())
 
