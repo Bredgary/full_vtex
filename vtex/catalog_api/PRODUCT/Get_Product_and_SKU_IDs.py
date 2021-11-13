687adf75263f326f76c9235751e7bbd059b3c809
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-13 15:05:54 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
+++ vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
@@ -1,78 +1,89 @@
-import requests
-import json
-import os
-import re
-from datetime import datetime
-from os import system
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import pandas as pd
+import numpy as np
 from google.cloud import bigquery
+import os, json
+from datetime import datetime
+import requests
+from datetime import datetime, timezone
 
-ids = 1
-datatemp = []
-datatemp2 = []
+class init:
+	IDS = []
+	productList = []
 
-f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ultimoRegistroCargado_from.txt','r')
-data_from_string = f_01.read()
-f_02 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ultimoRegistroCargado_to.txt','r')
-data_to_string = f_02.read()
-f_03 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ids.txt','r')
-ids_string = f_03.read()
-
-
-data_from = int(data_from_string)
-data_to = int(data_to_string)
-ids = int(ids_string)
-
-client = bigquery.Client()
-
-def get_productIFD(id,data_from,data_to,ids):
-	datatemp = []
+def get_productID(idCategory,From,To):
 	url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/GetProductAndSkuIds"
-	querystring = {"categoryId":""+str(id)+"","_from":""+str(data_from)+"","_to":""+str(data_to)+""}
+	querystring = {"categoryId":""+str(idCategory)+"","_from":""+str(From)+"",""+str(To)+"":"54"}
 	headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 	response = requests.request("GET", url, headers=headers, params=querystring)
-	jsonF = json.loads(response.text)
-	data = jsonF["data"]
-	for total in data:
-		datatemp.append(total)
-	idsProducts = json.dumps(datatemp)
-	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/"+str(ids)+"_productID_categoryID_"+str(id)+".json", "w")
-	text_file.write(idsProducts)
-	text_file.close()
-	u_data_from = str(data_from)
-	u_data_to = str(data_to)
-	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ultimoRegistroCargado_from.txt", "w")
-	text_file.write(u_data_from)
-	text_file.close()
-	text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ultimoRegistroCargado_to.txt", "w")
-	text_file.write(u_data_to)
-	text_file.close()
-	if bool(data):
-		data_from = data_from + 50
-		data_to = data_to +50
-		ids = ids + 1
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ids.txt", "w")
-		text_file.write(str(ids))
-		text_file.close()
-		print(str(ids)+" ID de producto agregado, categoryID= "+str(id))
-		get_productIFD(id,data_from,data_to,ids)
-	else:
-		u_data_from = str(data_from)
-		u_data_to = str(data_to)
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ultimoRegistroCargado_from_categoryID_"+str(id)+".txt", "w")
-		text_file.write(u_data_from)
-		text_file.close()
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/HistoryGetProductID/ultimoRegistroCargado_to_categoryID_"+str(id)+".txt", "w")
-		text_file.write(u_data_to)
-		text_file.close()
+	Fjson = json.loads(response.text)
+	data = Fjson["data"]
+	for x in data:
+		init.IDS.append(x)
 
-QUERY = (
-    'SELECT id FROM `shopstar-datalake.landing_zone.shopstar_vtex_detail_category` ')
-query_job = client.query(QUERY) 
-rows = query_job.result()  
 
-get_productIFD("1476",data_from,data_to,ids)
+def get_params():
+	client = bigquery.Client()
+	QUERY = (
+		'SELECT id FROM `shopstar-datalake.landing_zone.shopstar_vtex_category` where predecessor= 0')
+		query_job = client.query(QUERY)  
+		rows = query_job.result()  
+		for row in rows:
+			init.productList.append(row.id)
+	for x in init.productList:
+		get_productID(x,1,50)
 
-#for row in rows:
-#	get_productIFD(str(row.id),data_from,data_to,ids)
-#	print("Lista De IDS Cargados")
-#	break
+
+def format_schema(schema):
+    formatted_schema = []
+    for row in schema:
+        formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
+    return formatted_schema
+
+
+def run():
+	get_params()
+	print(init.IDS)
+	'''
+	for x in raiz:
+		df1 = pd.DataFrame({
+			'id': x["id"],
+			'name': x["name"],
+			'url': x["url"],
+			'title': x["Title"],
+			'metaTagDescription': x["MetaTagDescription"],
+			'predecessor': 0,
+			'hasChildren': str(x["hasChildren"])}, index=[0])
+		init.df = init.df.append(df1)
+		son = x["children"]
+		children(son,x["id"])
+
+	df = init.df
+	df.reset_index(drop=True, inplace=True)
+	json_data = df.to_json(orient = 'records')
+	json_object = json.loads(json_data)
+
+	table_schema = {
+		"name": "id",
+		"type": "INTEGER",
+		"mode": "NULLABLE"
+		}
+
+
+	project_id = '999847639598'
+	dataset_id = 'landing_zone'
+	table_id = 'shopstar_vtex_product_ID'
+
+	client  = bigquery.Client(project = project_id)
+	dataset  = client.dataset(dataset_id)
+	table = dataset.table(table_id)
+	job_config = bigquery.LoadJobConfig()
+	job_config.write_disposition = "WRITE_TRUNCATE"
+	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+	job_config.schema = format_schema(table_schema)
+	job = client.load_table_from_json(json_object, table, job_config = job_config)
+	print(job.result())
+'''
+run()
+
