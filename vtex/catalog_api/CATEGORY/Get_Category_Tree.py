54f0120889fccb2141b5e191d7229d35fcbfeb62
bredg <bredg@LAPTOP-77FGOG5A>
2021-11-13 12:55:13 -0300

    MOD: prueba

--- vtex/catalog_api/CATEGORY/Get_Category_Tree.py
+++ vtex/catalog_api/CATEGORY/Get_Category_Tree.py
@@ -58,7 +58,7 @@ def run():
 			'title': x["Title"],
 			'metaTagDescription': x["MetaTagDescription"],
 			'predecessor': 0},
-			'hasChildren': x["hasChildren"]
+			'hasChildren': x["hasChildren"],
 			'successor': x["children"].get("id"), index=[0])
 		break
 	
