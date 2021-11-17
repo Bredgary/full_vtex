f937d0404f47a3daa0d38cfbbb9ecf8ed1a690a4
bredg <bred_valenzuela@gmail.com>
2021-11-17 14:36:50 -0300

    MOD: prueba

--- vtex/catalog_api/SKU/List_all_SKU_IDs.py
+++ vtex/catalog_api/SKU/List_all_SKU_IDs.py
@@ -21,7 +21,7 @@ def get_SKU_ID(page):
     querystring = {"page":""+str(page)+"","pagesize":""+str(init.end)+""}
     response = requests.request("GET", init.url, headers=init.headers, params=querystring)
     Fjson = json.loads(response.text)
-    for x in data:
+    for x in Fjson:
         init.IDS.append(x)
         init.count +=1
         end = init.end
