c9464edc66fda8a707b46cd2e2c9bb96505b6899
bredg <bred_valenzuela@gmail.com>
2021-11-16 21:39:04 -0300

    MOD: prueba

--- vtex/catalog_api/TRADE_POLICY/Get_Trade_Policy_by_Product_ID.py
+++ vtex/catalog_api/TRADE_POLICY/Get_Trade_Policy_by_Product_ID.py
@@ -16,7 +16,7 @@ def get_product(id,reg):
 		url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/product/"+str(id)+"/salespolicy"
 		response = requests.request("GET", url, headers=init.headers)
 		Fjson = json.loads(response.text)
-		print(Fjson)
+		init.productList.append(Fjson)
 	except:
 		print("vacio")
 
