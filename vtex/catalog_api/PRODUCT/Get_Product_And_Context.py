d93ae4774b08c8d75eb5041562341c2a82e4c381
bredg <bred_valenzuela@gmail.com>
2021-11-21 11:46:15 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_And_Context.py
+++ vtex/catalog_api/PRODUCT/Get_Product_And_Context.py
@@ -178,3 +178,5 @@ def run(requests):
     #job_config.schema = format_schema(table_schema)
     job = client.load_table_from_json(json_object, table, job_config = job_config)
     print(job.result())
+    
+run()
