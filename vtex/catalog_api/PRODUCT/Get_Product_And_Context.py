0390e40aaf35080e98e26c5f8b2c1a0cdf3cb1de
bredg <bred_valenzuela@gmail.com>
2021-11-21 19:26:12 -0300

    MOD: prueba

--- vtex/catalog_api/PRODUCT/Get_Product_And_Context.py
+++ vtex/catalog_api/PRODUCT/Get_Product_And_Context.py
@@ -15,7 +15,7 @@ def get_product(id,reg):
     url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/productget/"+str(id)+""
     response = requests.request("GET", url, headers=init.headers)
     Fjson = json.loads(response.text)
-    init.productList.append(Fjson)
+    init.df = pd.DataFrame.from_dict(Fjson, orient='index')
     print("Registro: "+str(reg))
 
 def get_params():
@@ -35,124 +35,12 @@ def get_params():
 
 def run():
     get_params()
-    
-    for x in init.productList:
-        df1 = pd.DataFrame({
-            'id': x["Id"],
-            'name': x["Name"],
-            'departmentId': x["DepartmentId"],
-            'categoryId': x["CategoryId"],
-            'brandId': x["BrandId"],
-            'linkId': x["LinkId"],
-            'refId': x["RefId"],
-            'isVisible': x["IsVisible"],
-            'description': x["Description"],
-            'descriptionShort': x["DescriptionShort"],
-            'releaseDate': x["ReleaseDate"],
-            'keyWords': x["KeyWords"],
-            'title': x["Title"],
-            'isActive': x["IsActive"],
-            'taxCode': x["TaxCode"],
-            'metaTagDescription': x["MetaTagDescription"],
-            'supplierId': x["SupplierId"],
-            'showWithoutStock': x["ShowWithoutStock"],
-            'ListStoreId': x["ListStoreId"],
-            'adWordsRemarketingCode': x["AdWordsRemarketingCode"],
-            'lomadeeCampaignCode': x["LomadeeCampaignCode"]}, index=[0])
-        init.df = init.df.append(df1)
 
     df = init.df
     df.reset_index(drop=True, inplace=True)
     json_data = df.to_json(orient = 'records')
     json_object = json.loads(json_data)
     
-    table_schema = [
-        {
-            "name": "Id",
-            "type": "INTEGER",
-            "mode": "NULLABLE"
-        },{
-            "name": "Name",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "DepartmentId",
-            "type": "INTEGER",
-            "mode": "NULLABLE"
-        },{
-            "name": "CategoryId",
-            "type": "INTEGER",
-            "mode": "NULLABLE"
-        },{
-            "name": "BrandId",
-            "type": "INTEGER",
-            "mode": "NULLABLE"
-        },{
-            "name": "LinkId",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "RefId",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "IsVisible",
-            "type": "BOOLEAN",
-            "mode": "NULLABLE"
-        },{
-            "name": "Description",
-            "type": "FLOAT",
-            "mode": "NULLABLE"
-        },{
-            "name": "DescriptionShort",
-            "type": "FLOAT",
-            "mode": "NULLABLE"
-        },{
-            "name": "ReleaseDate",
-            "type": "DATE",
-            "mode": "NULLABLE"
-        },{
-            "name": "KeyWords",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "Title",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "IsActive",
-            "type": "BOOLEAN",
-            "mode": "NULLABLE"
-        },{
-            "name": "TaxCode",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "MetaTagDescription",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "SupplierId",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "ShowWithoutStock",
-            "type": "BOOLEAN",
-            "mode": "NULLABLE"
-        },{
-            "name": "ListStoreId",
-            "type": "INTEGER",
-            "mode": "REPEATED"
-        },{
-            "name": "AdWordsRemarketingCode",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "LomadeeCampaignCode",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        }]
-
     project_id = '999847639598'
     dataset_id = 'landing_zone'
     table_id = 'shopstar_vtex_product_context'
