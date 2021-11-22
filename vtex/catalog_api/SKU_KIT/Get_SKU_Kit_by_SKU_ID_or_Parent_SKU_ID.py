42dd6d0c289a81c7a5dbf529d0828aa320b94106
bredg <bred_valenzuela@gmail.com>
2021-11-22 19:02:39 -0300

    MOD: prueba

--- vtex/catalog_api/SKU_KIT/Get_SKU_Kit_by_SKU_ID_or_Parent_SKU_ID.py
+++ vtex/catalog_api/SKU_KIT/Get_SKU_Kit_by_SKU_ID_or_Parent_SKU_ID.py
@@ -16,17 +16,17 @@ def sku_context(id,reg):
     	url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/stockkeepingunitkit"
     	querystring = {"skuId":""+str(id)+""}
     	response = requests.request("GET", url, headers=init.headers, params=querystring)
-        Fjson = json.loads(response.text)
-        df1 = pd.DataFrame({
-            'id' : Fjson["Id"],
-            'stockKeepingUnitParent' : Fjson["StockKeepingUnitParent"],
-            'stockKeepingUnitId' : Fjson["StockKeepingUnitId"],
-            'quantity' : Fjson["Quantity"],
-            'unitPrice': Fjson["UnitPrice"]}, index=[0])
-        init.df = init.df.append(df1)
-        print("Registro: "+str(reg))
+    	Fjson = json.loads(response.text)
+    	df1 = pd.DataFrame({
+			'id' : Fjson["Id"],
+			'stockKeepingUnitParent' : Fjson["StockKeepingUnitParent"],
+			'stockKeepingUnitId' : Fjson["StockKeepingUnitId"],
+			'quantity' : Fjson["Quantity"],
+			'unitPrice': Fjson["UnitPrice"]}, index=[0])
+    	init.df = init.df.append(df1)
+    	print("Registro: "+str(reg))
     except:
-        print("Vacio")
+    	print("Vacio")
 
 
 def get_params():
