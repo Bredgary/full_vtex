df6a1cf4acdd98eb02c89fc4f966c9d81c19a1c6
bredg <bred_valenzuela@gmail.com>
2021-11-22 11:27:33 -0300

    MOD: prueba

--- vtex/catalog_api/SKU_EAN/Get_EAN_by_SkuId.py
+++ vtex/catalog_api/SKU_EAN/Get_EAN_by_SkuId.py
@@ -19,8 +19,7 @@ def get_ean(id,reg):
     Fjson = json.loads(response.text)
     for x in Fjson:
     	init.id_ean = x
-    	df1 = pd.DataFrame({'id': init.id_ean}, index=[0])
-    	init.df = init.df.append(df1)
+    	print(init.id_ean)
     print("Registro: "+str(reg))
     print(init.df)
     #except:
@@ -55,13 +54,13 @@ def delete_duplicate():
 def run():
 	#try:
 		get_params()
+		'''
 		df = init.df
 		print(df)
 		df.reset_index(drop=True, inplace=True)
 		json_data = df.to_json(orient = 'records')
 		json_object = json.loads(json_data)
-		print(json_object)
-		'''
+		
 		project_id = '999847639598'
 		dataset_id = 'landing_zone'
 		table_id = 'shopstar_vtex_sku_ean_id'
