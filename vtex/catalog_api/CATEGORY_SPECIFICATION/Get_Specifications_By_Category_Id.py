bb81e7b1ddbcab98c98c16f0df8222354376f1c4
bredg <bred_valenzuela@gmail.com>
2021-11-21 21:46:29 -0300

    MOD: prueba

--- vtex/catalog_api/CATEGORY_SPECIFICATION/Get_Specifications_By_Category_Id.py
+++ vtex/catalog_api/CATEGORY_SPECIFICATION/Get_Specifications_By_Category_Id.py
@@ -42,6 +42,7 @@ def get_params():
     for row in rows:
         get_product(row.id,registro)
         registro += 1
+        break
     
 def delete_duplicate():
     client = bigquery.Client()
@@ -52,26 +53,29 @@ def delete_duplicate():
     print(rows)
 
 def run():
-    get_params()
-    df = init.df
-    df.reset_index(drop=True, inplace=True)
-    json_data = df.to_json(orient = 'records')
-    json_object = json.loads(json_data)
-    
-    project_id = '999847639598'
-    dataset_id = 'landing_zone'
-    table_id = 'shopstar_vtex_category_specification'
-
-    client  = bigquery.Client(project = project_id)
-    dataset  = client.dataset(dataset_id)
-    table = dataset.table(table_id)
-    job_config = bigquery.LoadJobConfig()
-    job_config.write_disposition = "WRITE_TRUNCATE"
-    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job_config.autodetect = True
-    job = client.load_table_from_json(json_object, table, job_config = job_config)
-    print(job.result())
-    delete_duplicate()
+	try:
+	    get_params()
+	    df = init.df
+	    df.reset_index(drop=True, inplace=True)
+	    json_data = df.to_json(orient = 'records')
+	    json_object = json.loads(json_data)
+	    
+	    project_id = '999847639598'
+	    dataset_id = 'landing_zone'
+	    table_id = 'shopstar_vtex_category_specification'
+	
+	    client  = bigquery.Client(project = project_id)
+	    dataset  = client.dataset(dataset_id)
+	    table = dataset.table(table_id)
+	    job_config = bigquery.LoadJobConfig()
+	    job_config.write_disposition = "WRITE_TRUNCATE"
+	    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+	    job_config.autodetect = True
+	    job = client.load_table_from_json(json_object, table, job_config = job_config)
+	    print(job.result())
+	    delete_duplicate()
+	except:
+		print("vacio")
     
 run()
 
