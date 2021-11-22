f81ddf8494efff7a074ba183222a7dc70866ac97
bredg <bred_valenzuela@gmail.com>
2021-11-21 23:48:50 -0300

    MOD: prueba

--- vtex/catalog_api/SKU/Get_SKU_and_context.py
+++ vtex/catalog_api/SKU/Get_SKU_and_context.py
@@ -17,6 +17,9 @@ def sku_context(id,reg):
         response = requests.request("GET", url, headers=init.headers, params=querystring)
         Fjson = json.loads(response.text)
         dimension = Fjson["Dimension"]
+        RealDimension = Fjson["RealDimension"]
+        Attachments = Fjson["Attachments"]
+        Fields = Attachments["Fields"]
         df1 = pd.DataFrame({
             'Id': Fjson["Id"],
             'ProductId': Fjson["ProductId"],
@@ -32,11 +35,22 @@ def sku_context(id,reg):
             'DetailUrl': Fjson["DetailUrl"],
             'CSCIdentification': Fjson["CSCIdentification"],
             'BrandId': Fjson["BrandId"],
-            'cubicweight': dimension["cubicweight"],
-            'height': dimension["height"],
-            'length': dimension["length"],
-            'weight': dimension["weight"],
-            'width': dimension["width"],
+            'dimension_cubicweight': dimension["cubicweight"],
+            'dimension_height': dimension["height"],
+            'dimension_length': dimension["length"],
+            'dimension_weight': dimension["weight"],
+            'RealDimension_realCubicWeight': RealDimension["realCubicWeight"],
+            'RealDimension_realHeight': RealDimension["realHeight"],
+            'RealDimension_realLength': RealDimension["realLength"],
+            'RealDimension_realWeight': RealDimension["realWeight"],
+            'RealDimension_realWidth': RealDimension["realWidth"],
+            'Attachments_Id': Attachments["Id"],
+            'Attachments_Name': Attachments["Name"],
+            'Attachments_IsActive': Attachments["IsActive"],
+            'Attachments_IsRequired': Attachments["IsRequired"],
+            'Fields_FieldName': Fields["FieldName"],
+            'Fields_MaxCaracters': Fields["MaxCaracters"],
+            'Fields_DomainValues': Fields["DomainValues"],
             'BrandName': Fjson["BrandName"]}, index=[0])
         init.df = init.df.append(df1)
         print("Registro: "+str(reg))
