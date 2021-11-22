b5609251271d684405f81e8d252efbedc5f0737d
bredg <bred_valenzuela@gmail.com>
2021-11-22 11:07:41 -0300

    MOD: pruea

--- vtex/catalog_api/SKU_EAN/Get_EAN_by_SkuId.py
+++ vtex/catalog_api/SKU_EAN/Get_EAN_by_SkuId.py
@@ -1,71 +1,79 @@
-import requests
-import json
-import os
-import re
-from datetime import datetime
-from os import system
+import pandas as pd
+import numpy as np
 from google.cloud import bigquery
+import os, json
+from datetime import datetime
+import requests
+from datetime import datetime, timezone
 
-listaEan = []
-f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/delimitador.txt','r')
-data_from_string = f_01.read()
-delimitador = int(data_from_string)
-count = 0
+class init:
+    productList = []
+    df = pd.DataFrame()
+    id_ean
+    headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 
-def get_ean(id,count,delimitador):
-	if count >= delimitador:
-		try:
-			url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/stockkeepingunit/"+str(id)+"/ean"
-			headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-			response = requests.request("GET", url, headers=headers)
-			temp = response.text
-			idEan = temp.replace("[", "{id:").replace("]","}")
-			listaEan.append(idEan)
-			print("Num: "+str(count))
-			#cargando_bigquery()
-		except:
-			delimitador = count + 1                 
-			text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/delimitador.txt", "w")
-			text_file.write(str(delimitador))
-			text_file.close()
-			system("python3 Get_EAN_by_SkuId.py")
+def get_sku(id,reg):
+    try:
+	    url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/stockkeepingunit/"+str(id)+"/ean"
+	    response = requests.request("GET", url, headers=init.headers)
+	    Fjson = json.loads(response.text)
+	    print(Fjson)
+	    print(type(Fjson))
+	    #df1 = pd.DataFrame({'complementTypeId': x["ComplementTypeId"]}, index=[0])
+	    #init.df = init.df.append(df1)
+	    print("Registro: "+str(reg))
+    except:
+    	print("Vacio")
 
-
-def operacion_fenix(count):
-	f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/id_sku.json','r')
-	data_from_string = f_01.read()
-	data_from_string = data_from_string.replace('"', '')
-	listaIDS = json.loads(data_from_string)
-	for i in listaIDS:
-		count += 1
-		get_ean(i,count,delimitador)
-	print(str(count)+" registro almacenado.")
-
-
-def cargando_bigquery():
-	print("Cargando a BigQuery")
-	client = bigquery.Client()
-	filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/sku_ean.json'
-	dataset_id = 'landing_zone'
-	table_id = 'shopstar_vtex_sku_ean_id'
-	dataset_ref = client.dataset(dataset_id)
-	table_ref = dataset_ref.table(table_id)
-	job_config = bigquery.LoadJobConfig()
-	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-	job_config.autodetect = True
-	with open(filename, "rb") as source_file:
-		job = client.load_table_from_file(
-			source_file,
-			table_ref,
-			location="southamerica-east1",  # Must match the destination dataset location.
-		job_config=job_config,)  # API request
-	job.result()  # Waits for table load to complete.
-	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-	print("finalizado")
-
-operacion_fenix(count)
+def get_params():
+    print("Cargando consulta")
+    client = bigquery.Client()
+    QUERY = (
+        'SELECT id FROM `shopstar-datalake.landing_zone.shopstar_vtex_SKU_ID`')
+    query_job = client.query(QUERY)  
+    rows = query_job.result()
+    registro = 1
+    for row in rows:
+        get_sku(row.id,registro)
+        registro += 1
+        break
 
 
-text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_EAN/table_ean_f.txt", "w")
-text_file.write(str(listaEan))
-text_file.close()
+def delete_duplicate():
+	try:
+		print("Borrando duplicados")
+		client = bigquery.Client()
+		QUERY = (
+			'CREATE OR REPLACE TABLE `shopstar-datalake.landing_zone.shopstar_vtex_sku_ean_id` AS SELECT DISTINCT * FROM `shopstar-datalake.landing_zone.shopstar_vtex_sku_ean_id`')
+		query_job = client.query(QUERY)  
+		rows = query_job.result()
+		print(rows)
+	except:
+		print("Consulta no ejecutada")
+
+def run():
+	try:
+		get_params()
+		df = init.df
+		df.reset_index(drop=True, inplace=True)
+		json_data = df.to_json(orient = 'records')
+		json_object = json.loads(json_data)
+		
+		project_id = '999847639598'
+		dataset_id = 'landing_zone'
+		table_id = 'shopstar_vtex_sku_ean_id'
+		
+		client  = bigquery.Client(project = project_id)
+		dataset  = client.dataset(dataset_id)
+		table = dataset.table(table_id)
+		job_config = bigquery.LoadJobConfig()
+		job_config.write_disposition = "WRITE_TRUNCATE"
+		job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+		job_config.autodetect = True
+		job = client.load_table_from_json(json_object, table, job_config = job_config)
+		print(job.result())
+		delete_duplicate()
+	except:
+		print("No se puede ingestar")
+    
+run()
