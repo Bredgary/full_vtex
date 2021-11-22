d4f8ab7dfa9bb9f6e40d234a5b6aa8a77a762d85
bredg <bred_valenzuela@gmail.com>
2021-11-22 00:31:29 -0300

    MOD: prueba

--- vtex/catalog_api/SKU/Get_SKU_and_context.py
+++ vtex/catalog_api/SKU/Get_SKU_and_context.py
@@ -9,49 +9,77 @@ from datetime import datetime, timezone
 class init:
     df = pd.DataFrame()
     headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-
+    url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyid/"+str(id)+""
+    querystring = {"sc":"1"}
+    response = requests.request("GET", url, headers=init.headers, params=querystring)
+    Fjson = json.loads(response.text)
+    ids: Fjson["Id"]
+    ProductId: Fjson["ProductId"]
+    NameComplete: Fjson["NameComplete"]
+    ProductName: Fjson["ProductName"]
+    ProductDescription: Fjson["ProductDescription"]
+    SkuName: Fjson["SkuName"]
+    IsActive: Fjson["IsActive"]
+    IsTransported: Fjson["IsTransported"]
+    IsInventoried: Fjson["IsInventoried"]
+    IsGiftCardRecharge: Fjson["IsGiftCardRecharge"]
+    ImageUrl: Fjson["ImageUrl"]
+    DetailUrl: Fjson["DetailUrl"]
+    CSCIdentification: Fjson["CSCIdentification"]
+    BrandId: Fjson["BrandId"]
+    BrandName: Fjson["BrandName"]
+    dimension = Fjson["Dimension"]
+    if dimension is not "[]":
+        dimension_cubicweight: dimension["cubicweight"]
+        dimension_height: dimension["height"]
+        dimension_length: dimension["length"]
+        dimension_weight: dimension["weight"]
+    RealDimension = Fjson["RealDimension"]
+    if RealDimension is not "[]":
+        RealDimension_realCubicWeight: RealDimension["realCubicWeight"]
+        RealDimension_realHeight: RealDimension["realHeight"]
+        RealDimension_realLength: RealDimension["realLength"]
+        RealDimension_realWeight: RealDimension["realWeight"]
+        RealDimension_realWidth: RealDimension["realWidth"]
+    Attachments = Fjson["Attachments"]
+    if Attachments is not "[]":
+        Attachments_Id: Attachments["Id"]
+        Attachments_Name: Attachments["Name"]
+        Attachments_IsActive: Attachments["IsActive"]
+        Attachments_IsRequired: Attachments["IsRequired"]
+        
 def sku_context(id,reg):
     #try:
-        url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyid/"+str(id)+""
-        querystring = {"sc":"1"}
-        response = requests.request("GET", url, headers=init.headers, params=querystring)
-        Fjson = json.loads(response.text)
-        dimension = Fjson["Dimension"]
-        RealDimension = Fjson["RealDimension"]
-        Attachments = Fjson["Attachments"]
-        print(Attachments)
-        '''
         df1 = pd.DataFrame({
-            'Id': Fjson["Id"],
-            'ProductId': Fjson["ProductId"],
-            'NameComplete': Fjson["NameComplete"],
-            'ProductName': Fjson["ProductName"],
-            'ProductDescription': Fjson["ProductDescription"],
-            'SkuName': Fjson["SkuName"],
-            'IsActive': Fjson["IsActive"],
-            'IsTransported': Fjson["IsTransported"],
-            'IsInventoried': Fjson["IsInventoried"],
-            'IsGiftCardRecharge': Fjson["IsGiftCardRecharge"],
-            'ImageUrl': Fjson["ImageUrl"],
-            'DetailUrl': Fjson["DetailUrl"],
-            'CSCIdentification': Fjson["CSCIdentification"],
-            'BrandId': Fjson["BrandId"],
-            'dimension_cubicweight': dimension["cubicweight"],
-            'dimension_height': dimension["height"],
-            'dimension_length': dimension["length"],
-            'dimension_weight': dimension["weight"],
-            'RealDimension_realCubicWeight': RealDimension["realCubicWeight"],
-            'RealDimension_realHeight': RealDimension["realHeight"],
-            'RealDimension_realLength': RealDimension["realLength"],
-            'RealDimension_realWeight': RealDimension["realWeight"],
-            'RealDimension_realWidth': RealDimension["realWidth"],
-            'Attachments_Id': Attachments[0],
-            'Attachments_Name': Attachments["Name"],
-            'Attachments_IsActive': Attachments["IsActive"],
-            'Attachments_IsRequired': Attachments["IsRequired"],
-            'BrandName': Fjson["BrandName"]}, index=[0])
+            'Id': init.ids,
+            'ProductId': init.ProductId,
+            'NameComplete': init.NameComplete,
+            'ProductName': init.ProductName,
+            'ProductDescription': init.ProductDescription,
+            'SkuName': init.SkuName,
+            'IsActive': init.IsActive,
+            'IsTransported': init.IsTransported,
+            'IsInventoried': init.IsInventoried,
+            'IsGiftCardRecharge': init.IsGiftCardRecharge,
+            'ImageUrl': init.ImageUrl,
+            'DetailUrl': init.DetailUrl,
+            'CSCIdentification': init.CSCIdentification,
+            'BrandId': init.BrandId,
+            'dimension_cubicweight': init.dimension_cubicweight,
+            'dimension_height': init.dimension_height,
+            'dimension_length': init.dimension_length,
+            'dimension_weight': init.dimension_weight,
+            'RealDimension_realCubicWeight': init.RealDimension_realCubicWeight,
+            'RealDimension_realHeight': init.RealDimension_realHeight,
+            'RealDimension_realLength': init.RealDimension_realLength,
+            'RealDimension_realWeight': init.RealDimension_realWeight,
+            'RealDimension_realWidth': init.RealDimension_realWidth,
+            'Attachments_Id': init.Attachments_Id,
+            'Attachments_Name': init.Attachments_Name,
+            'Attachments_IsActive': init.Attachments_IsActive,
+            'Attachments_IsRequired': init.Attachments_IsRequired,
+            'BrandName': init.BrandName}, index=[0])
         init.df = init.df.append(df1)
-        '''
         print("Registro: "+str(reg))
     #except:
     #    print("Vacio")
