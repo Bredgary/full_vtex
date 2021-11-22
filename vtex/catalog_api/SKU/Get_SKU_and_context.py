ad487ab3430572cd644bf5fd16d2a543e9d8ef2c
bredg <bred_valenzuela@gmail.com>
2021-11-22 01:39:24 -0300

    MOD: prueba

--- vtex/catalog_api/SKU/Get_SKU_and_context.py
+++ vtex/catalog_api/SKU/Get_SKU_and_context.py
@@ -12,153 +12,154 @@ class init:
     
         
 def sku_context(id,reg):
-    url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyid/"+str(id)+""
-    querystring = {"sc":"1"}
-    response = requests.request("GET", url, headers=init.headers, params=querystring)
-    Fjson = json.loads(response.text)
-    ids = Fjson["Id"]
-    ProductId = Fjson["ProductId"]
-    NameComplete = Fjson["NameComplete"]
-    ProductName = Fjson["ProductName"]
-    ProductDescription = Fjson["ProductDescription"]
-    SkuName = Fjson["SkuName"]
-    IsActive = Fjson["IsActive"]
-    IsTransported = Fjson["IsTransported"]
-    IsInventoried = Fjson["IsInventoried"]
-    IsGiftCardRecharge = Fjson["IsGiftCardRecharge"]
-    ImageUrl = Fjson["ImageUrl"]
-    DetailUrl = Fjson["DetailUrl"]
-    CSCIdentification = Fjson["CSCIdentification"]
-    BrandId = Fjson["BrandId"]
-    ProductCategoryIds = Fjson["ProductCategoryIds"]
-    ProductGlobalCategoryId = Fjson["ProductGlobalCategoryId"]
-    CommercialConditionId = Fjson["CommercialConditionId"]
-    RewardValue = Fjson["RewardValue"]
-    EstimatedDateArrival = Fjson["EstimatedDateArrival"]
-    MeasurementUnit = Fjson["MeasurementUnit"]
-    UnitMultiplier = Fjson["UnitMultiplier"]
-    InformationSource = Fjson["InformationSource"]
-    ModalType = Fjson["ModalType"]
-    
-    Dimension = Fjson["Dimension"]
-    if Dimension:
-        dimension_cubicweight = Dimension["cubicweight"]
-        dimension_height = Dimension["height"]
-        dimension_length = Dimension["length"]
-        dimension_weight = Dimension["weight"]
-    else:
-        dimension_cubicweight = None
-        dimension_height = None
-        dimension_length = None
-        dimension_weight = None
-    RealDimension = Fjson["RealDimension"]
-    if RealDimension:
-        RealDimension_realCubicWeight = RealDimension["realCubicWeight"]
-        RealDimension_realHeight = RealDimension["realHeight"]
-        RealDimension_realLength = RealDimension["realLength"]
-        RealDimension_realWeight = RealDimension["realWeight"]
-        RealDimension_realWidth = RealDimension["realWidth"]
-    else:
-        RealDimension_realCubicWeight = None
-        RealDimension_realHeight = None
-        RealDimension_realLength = None
-        RealDimension_realWeight = None
-        RealDimension_realWidth = None
-    Attachments = Fjson["Attachments"]
-    if Attachments:
-        Attachments_Id = Attachments["Id"]
-        Attachments_Name = Attachments["Name"]
-        Attachments_IsActive = Attachments["IsActive"]
-        Attachments_IsRequired = Attachments["IsRequired"]
-    else:
-        Attachments_Id = None
-        Attachments_Name = None
-        Attachments_IsActive = None
-        Attachments_IsRequired = None
-    SkuSellers = Fjson["SkuSellers"]
-    for x in SkuSellers:
-        if SkuSellers:
-            SkuSellers_SellerId = x["SellerId"]
-            SkuSellers_StockKeepingUnitId = x["StockKeepingUnitId"]
-            SkuSellers_SellerStockKeepingUnitId = x["SellerStockKeepingUnitId"]
-            SkuSellers_IsActive = x["IsActive"]
-            SkuSellers_FreightCommissionPercentage = x["FreightCommissionPercentage"]
-            SkuSellers_ProductCommissionPercentage = x["ProductCommissionPercentage"]
+    try:
+        url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyid/"+str(id)+""
+        querystring = {"sc":"1"}
+        response = requests.request("GET", url, headers=init.headers, params=querystring)
+        Fjson = json.loads(response.text)
+        ids = Fjson["Id"]
+        ProductId = Fjson["ProductId"]
+        NameComplete = Fjson["NameComplete"]
+        ProductName = Fjson["ProductName"]
+        ProductDescription = Fjson["ProductDescription"]
+        SkuName = Fjson["SkuName"]
+        IsActive = Fjson["IsActive"]
+        IsTransported = Fjson["IsTransported"]
+        IsInventoried = Fjson["IsInventoried"]
+        IsGiftCardRecharge = Fjson["IsGiftCardRecharge"]
+        ImageUrl = Fjson["ImageUrl"]
+        DetailUrl = Fjson["DetailUrl"]
+        CSCIdentification = Fjson["CSCIdentification"]
+        BrandId = Fjson["BrandId"]
+        ProductCategoryIds = Fjson["ProductCategoryIds"]
+        ProductGlobalCategoryId = Fjson["ProductGlobalCategoryId"]
+        CommercialConditionId = Fjson["CommercialConditionId"]
+        RewardValue = Fjson["RewardValue"]
+        EstimatedDateArrival = Fjson["EstimatedDateArrival"]
+        MeasurementUnit = Fjson["MeasurementUnit"]
+        UnitMultiplier = Fjson["UnitMultiplier"]
+        InformationSource = Fjson["InformationSource"]
+        ModalType = Fjson["ModalType"]
+        
+        Dimension = Fjson["Dimension"]
+        if Dimension:
+            dimension_cubicweight = Dimension["cubicweight"]
+            dimension_height = Dimension["height"]
+            dimension_length = Dimension["length"]
+            dimension_weight = Dimension["weight"]
         else:
-            SkuSellers_SellerId = None
-            SkuSellers_StockKeepingUnitId = None
-            SkuSellers_SellerStockKeepingUnitId = None
-            SkuSellers_IsActive = None
-            SkuSellers_FreightCommissionPercentage = None
-            SkuSellers_ProductCommissionPercentage = None
-    Images = Fjson["Images"]
-    if Images:
-        Images_ImageUrl = Images["ImageUrl"]
-        Images_ImageName = Images["ImageName"]
-        Images_FileId = Images["FileId"]
-    else:
-        Images_ImageUrl = None
-        Images_ImageName = None
-        Images_FileId = None
-    AlternateIds = Fjson["AlternateIds"]
-    if AlternateIds:
-        AlternateIds_RefId = AlternateIds["RefId"]
-    else:
-        AlternateIds_Ean = None
-        AlternateIds_RefId = None
-    #try:
-    df1 = pd.DataFrame({
-        'Id': ids,
-        'ProductId': ProductId,
-        'NameComplete': NameComplete,
-        'ProductName': ProductName,
-        'ProductDescription': ProductDescription,
-        'SkuName': SkuName,
-        'IsActive': IsActive,
-        'IsTransported': IsTransported,
-        'IsInventoried': IsInventoried,
-        'IsGiftCardRecharge': IsGiftCardRecharge,
-        'ImageUrl': ImageUrl,
-        'DetailUrl': DetailUrl,
-        'CSCIdentification': CSCIdentification,
-        'BrandId': BrandId,
-        'dimension_cubicweight': dimension_cubicweight,
-        'dimension_height': dimension_height,
-        'dimension_length': dimension_length,
-        'dimension_weight': dimension_weight,
-        'RealDimension_realCubicWeight': RealDimension_realCubicWeight,
-        'RealDimension_realHeight': RealDimension_realHeight,
-        'RealDimension_realLength': RealDimension_realLength,
-        'RealDimension_realWeight': RealDimension_realWeight,
-        'RealDimension_realWidth': RealDimension_realWidth,
-        'Attachments_Id': Attachments_Id,
-        'Attachments_Name': Attachments_Name,
-        'Attachments_IsActive': Attachments_IsActive,
-        'Attachments_IsRequired': Attachments_IsRequired,
-        'SkuSellers_SellerId' : SkuSellers_SellerId,
-        'SkuSellers_StockKeepingUnitId' : SkuSellers_StockKeepingUnitId,
-        'SkuSellers_SellerStockKeepingUnitId' : SkuSellers_SellerStockKeepingUnitId,
-        'SkuSellers_IsActive' : SkuSellers_IsActive,
-        'SkuSellers_FreightCommissionPercentage' : SkuSellers_FreightCommissionPercentage,
-        'SkuSellers_ProductCommissionPercentage' : SkuSellers_ProductCommissionPercentage,
-        'Images_ImageUrl' : Images_ImageUrl,
-        'Images_ImageName' : Images_ImageName,
-        'Images_FileId' : Images_FileId,
-        'AlternateIds_RefId' : AlternateIds_RefId,
-        'ProductCategoryIds' : Fjson["ProductCategoryIds"],
-        'ProductGlobalCategoryId' : Fjson["ProductGlobalCategoryId"],
-        'CommercialConditionId' : Fjson["CommercialConditionId"],
-        'RewardValue' : Fjson["RewardValue"],
-        'EstimatedDateArrival' : Fjson["EstimatedDateArrival"],
-        'MeasurementUnit' : Fjson["MeasurementUnit"],
-        'UnitMultiplier' : Fjson["UnitMultiplier"],
-        'InformationSource' : Fjson["InformationSource"],
-        'ModalType' : Fjson["ModalType"],
-        'BrandName': Fjson["BrandName"]}, index=[0])
-    init.df = init.df.append(df1)
-    print("Registro: "+str(reg))
-    #except:
-    #    print("Vacio")
+            dimension_cubicweight = None
+            dimension_height = None
+            dimension_length = None
+            dimension_weight = None
+        RealDimension = Fjson["RealDimension"]
+        if RealDimension:
+            RealDimension_realCubicWeight = RealDimension["realCubicWeight"]
+            RealDimension_realHeight = RealDimension["realHeight"]
+            RealDimension_realLength = RealDimension["realLength"]
+            RealDimension_realWeight = RealDimension["realWeight"]
+            RealDimension_realWidth = RealDimension["realWidth"]
+        else:
+            RealDimension_realCubicWeight = None
+            RealDimension_realHeight = None
+            RealDimension_realLength = None
+            RealDimension_realWeight = None
+            RealDimension_realWidth = None
+        Attachments = Fjson["Attachments"]
+        if Attachments:
+            Attachments_Id = Attachments["Id"]
+            Attachments_Name = Attachments["Name"]
+            Attachments_IsActive = Attachments["IsActive"]
+            Attachments_IsRequired = Attachments["IsRequired"]
+        else:
+            Attachments_Id = None
+            Attachments_Name = None
+            Attachments_IsActive = None
+            Attachments_IsRequired = None
+        SkuSellers = Fjson["SkuSellers"]
+        for x in SkuSellers:
+            if SkuSellers:
+                SkuSellers_SellerId = x["SellerId"]
+                SkuSellers_StockKeepingUnitId = x["StockKeepingUnitId"]
+                SkuSellers_SellerStockKeepingUnitId = x["SellerStockKeepingUnitId"]
+                SkuSellers_IsActive = x["IsActive"]
+                SkuSellers_FreightCommissionPercentage = x["FreightCommissionPercentage"]
+                SkuSellers_ProductCommissionPercentage = x["ProductCommissionPercentage"]
+            else:
+                SkuSellers_SellerId = None
+                SkuSellers_StockKeepingUnitId = None
+                SkuSellers_SellerStockKeepingUnitId = None
+                SkuSellers_IsActive = None
+                SkuSellers_FreightCommissionPercentage = None
+                SkuSellers_ProductCommissionPercentage = None
+        Images = Fjson["Images"]
+        if Images:
+            Images_ImageUrl = Images["ImageUrl"]
+            Images_ImageName = Images["ImageName"]
+            Images_FileId = Images["FileId"]
+        else:
+            Images_ImageUrl = None
+            Images_ImageName = None
+            Images_FileId = None
+        AlternateIds = Fjson["AlternateIds"]
+        if AlternateIds:
+            AlternateIds_RefId = AlternateIds["RefId"]
+        else:
+            AlternateIds_Ean = None
+            AlternateIds_RefId = None
+        #try:
+        df1 = pd.DataFrame({
+            'Id': ids,
+            'ProductId': ProductId,
+            'NameComplete': NameComplete,
+            'ProductName': ProductName,
+            'ProductDescription': ProductDescription,
+            'SkuName': SkuName,
+            'IsActive': IsActive,
+            'IsTransported': IsTransported,
+            'IsInventoried': IsInventoried,
+            'IsGiftCardRecharge': IsGiftCardRecharge,
+            'ImageUrl': ImageUrl,
+            'DetailUrl': DetailUrl,
+            'CSCIdentification': CSCIdentification,
+            'BrandId': BrandId,
+            'dimension_cubicweight': dimension_cubicweight,
+            'dimension_height': dimension_height,
+            'dimension_length': dimension_length,
+            'dimension_weight': dimension_weight,
+            'RealDimension_realCubicWeight': RealDimension_realCubicWeight,
+            'RealDimension_realHeight': RealDimension_realHeight,
+            'RealDimension_realLength': RealDimension_realLength,
+            'RealDimension_realWeight': RealDimension_realWeight,
+            'RealDimension_realWidth': RealDimension_realWidth,
+            'Attachments_Id': Attachments_Id,
+            'Attachments_Name': Attachments_Name,
+            'Attachments_IsActive': Attachments_IsActive,
+            'Attachments_IsRequired': Attachments_IsRequired,
+            'SkuSellers_SellerId' : SkuSellers_SellerId,
+            'SkuSellers_StockKeepingUnitId' : SkuSellers_StockKeepingUnitId,
+            'SkuSellers_SellerStockKeepingUnitId' : SkuSellers_SellerStockKeepingUnitId,
+            'SkuSellers_IsActive' : SkuSellers_IsActive,
+            'SkuSellers_FreightCommissionPercentage' : SkuSellers_FreightCommissionPercentage,
+            'SkuSellers_ProductCommissionPercentage' : SkuSellers_ProductCommissionPercentage,
+            'Images_ImageUrl' : Images_ImageUrl,
+            'Images_ImageName' : Images_ImageName,
+            'Images_FileId' : Images_FileId,
+            'AlternateIds_RefId' : AlternateIds_RefId,
+            'ProductCategoryIds' : Fjson["ProductCategoryIds"],
+            'ProductGlobalCategoryId' : Fjson["ProductGlobalCategoryId"],
+            'CommercialConditionId' : Fjson["CommercialConditionId"],
+            'RewardValue' : Fjson["RewardValue"],
+            'EstimatedDateArrival' : Fjson["EstimatedDateArrival"],
+            'MeasurementUnit' : Fjson["MeasurementUnit"],
+            'UnitMultiplier' : Fjson["UnitMultiplier"],
+            'InformationSource' : Fjson["InformationSource"],
+            'ModalType' : Fjson["ModalType"],
+            'BrandName': Fjson["BrandName"]}, index=[0])
+        init.df = init.df.append(df1)
+        print("Registro: "+str(reg))
+    except:
+        print("Vacio")
 
 
 def get_params():
@@ -187,7 +188,7 @@ def delete_duplicate():
         print("Query no ejecutada")
 
 def run():
-    #try:
+    try:
         get_params()
         df = init.df
         df.reset_index(drop=True, inplace=True)
@@ -208,7 +209,7 @@ def run():
         job = client.load_table_from_json(json_object, table, job_config = job_config)
         print(job.result())
         delete_duplicate()
-    #except:
-    #    print("vacio")
+    except:
+        print("vacio")
     
 run()
