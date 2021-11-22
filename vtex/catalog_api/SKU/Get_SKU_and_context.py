d577c16bad2727c399f5b760c90d0db76161643a
bredg <bred_valenzuela@gmail.com>
2021-11-22 00:56:30 -0300

    MOD: prueba

--- vtex/catalog_api/SKU/Get_SKU_and_context.py
+++ vtex/catalog_api/SKU/Get_SKU_and_context.py
@@ -31,25 +31,41 @@ def sku_context(id,reg):
     CSCIdentification = Fjson["CSCIdentification"]
     BrandId = Fjson["BrandId"]
     BrandName = Fjson["BrandName"]
-    dimension = Fjson["Dimension"]
-    if dimension is not "[]":
+    Dimension = Fjson["Dimension"]
+    if Dimension:
         dimension_cubicweight = dimension["cubicweight"]
         dimension_height = dimension["height"]
         dimension_length = dimension["length"]
         dimension_weight = dimension["weight"]
+    else:
+        dimension_cubicweight = None
+        dimension_height = None
+        dimension_length = None
+        dimension_weight = None
     RealDimension = Fjson["RealDimension"]
-    if RealDimension is not "[]":
+    if RealDimension:
         RealDimension_realCubicWeight = RealDimension["realCubicWeight"]
         RealDimension_realHeight = RealDimension["realHeight"]
         RealDimension_realLength = RealDimension["realLength"]
         RealDimension_realWeight = RealDimension["realWeight"]
         RealDimension_realWidth = RealDimension["realWidth"]
+    else:
+        RealDimension_realCubicWeight = None
+        RealDimension_realHeight = None
+        RealDimension_realLength = None
+        RealDimension_realWeight = None
+        RealDimension_realWidth = None
     Attachments = Fjson["Attachments"]
     if Attachments:
-        Attachments_Id = Attachments["id"]
-        Attachments_Name = Attachments[1]
-        Attachments_IsActive = Attachments[2]
-        Attachments_IsRequired = Attachments[3]
+        Attachments_Id = Attachments["Id"]
+        Attachments_Name = Attachments["Name"]
+        Attachments_IsActive = Attachments["IsActive"]
+        Attachments_IsRequired = Attachments["IsRequired"]
+    else:
+        Attachments_Id = None
+        Attachments_Name = None
+        Attachments_IsActive = None
+        Attachments_IsRequired = None
     #try:
     df1 = pd.DataFrame({
         'Id': ids,
