a935cbc4fe2a4d229f26f8f41687a349a64de857
bredg <bred_valenzuela@gmail.com>
2021-11-22 12:46:44 -0300

    MOD: prueba

--- vtex/catalog_api/SKU_ATTACHMENT/Get_SKU_Attachment_by_SKUID.py
+++ vtex/catalog_api/SKU_ATTACHMENT/Get_SKU_Attachment_by_SKUID.py
@@ -1,95 +1,79 @@
-import requests
-import json
-import os
-import re
-from datetime import datetime
-from os import system
+import pandas as pd
+import numpy as np
 from google.cloud import bigquery
+import os, json
+from datetime import datetime
+import requests
+from datetime import datetime, timezone
 
-client = bigquery.Client()
-listaID = []
-listIdSkuAndContext =[]
-registro=0
-'''
-DIR = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_ATTACHMENT/SKU_ATTACHMENT/'
-delimitador = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
-count = 0
+class init:
+    productList = []
+    df = pd.DataFrame()
+    headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+
+def get_sku_Attachment(id,reg):
+    try:
+	    url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/stockkeepingunit/"+str(id)+"/attachment"
+	    response = requests.request("GET", url, headers=init.headers)
+	    Fjson = json.loads(response.text)
+	    for x in Fjson:
+	    	df1 = pd.DataFrame({
+				'id': x["Id"],
+				'attachmentId': x["AttachmentId"],
+				'skuId': x["SkuId"]}, index=[0])
+	    	init.df = init.df.append(df1)
+	    print("Registro: "+str(reg))
+    except:
+    	print("Vacio")
+
+def get_params():
+    print("Cargando consulta")
+    client = bigquery.Client()
+    QUERY = (
+        'SELECT id FROM `shopstar-datalake.landing_zone.shopstar_vtex_SKU_ID`')
+    query_job = client.query(QUERY)  
+    rows = query_job.result()
+    registro = 1
+    for row in rows:
+        get_sku_Attachment(row.id,registro)
+        registro += 1
 
 
-def get_sku_Attachment_by_SKUID(id,count,delimitador):
-	jsonF = {}
-	if count >= delimitador:
-		try:
-			url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/stockkeepingunit/"+str(id)+"/attachment"
-			headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-			response = requests.request("GET", url, headers=headers)
-			string = response.text
-			text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_ATTACHMENT/SKU_ATTACHMENT/"+str(count)+"_attachment.json", "w")
-			text_file.write(string)
-			text_file.close()
-			print("Get_SKU_Attachment_by_SKUID.py Terminando: "+str(count))
-		except:
-			delimitador = count
-			text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_ATTACHMENT/delimitador.txt", "w")
-			text_file.write(str(delimitador))
-			text_file.close()
-			system("python3 Get_SKU_Attachment_by_SKUID.py")
-
-
-def operacion_fenix(count):
-	f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_ATTACHMENT/id_sku.json','r')
-	data_from_string = f_01.read()
-	data_from_string = data_from_string.replace('"', '')
-	listaIDS = json.loads(data_from_string)
-	for i in listaIDS:
-		count += 1
-		get_sku_Attachment_by_SKUID(i,count,delimitador)
-	print(str(count)+" registro almacenado.")
-
-operacion_fenix(count)
-
-'''
-DIR = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_ATTACHMENT/SKU_ATTACHMENT/'
-countDir = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
-
-for x in range(countDir):
+def delete_duplicate():
 	try:
-		registro += 1
-		uri = "/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_ATTACHMENT/SKU_ATTACHMENT/"+str(registro)+"_attachment.json"
-		f_03 = open (uri,'r')
-		ids_string = f_03.read()
-		formatoJson = json.loads(ids_string)
-		listaID.append(formatoJson)
-		print("Attachment Almacenados: " +str(registro))
+		print("Borrando duplicados")
+		client = bigquery.Client()
+		QUERY = (
+			'CREATE OR REPLACE TABLE `shopstar-datalake.landing_zone.shopstar_vtex_sku_Attachment` AS SELECT DISTINCT * FROM `shopstar-datalake.landing_zone.shopstar_vtex_sku_Attachment`')
+		query_job = client.query(QUERY)  
+		rows = query_job.result()
+		print(rows)
 	except:
-		continue
+		print("Consulta no ejecutada")
 
-
-string = json.dumps(listaID)
-text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_ATTACHMENT/temp.json", "w")
-text_file.write(string)
-text_file.close() 
-
-system("cat temp.json | jq -c '.[]' > tableSkuAttachment.json")
-
-print("Cargando a BigQuery")
-client = bigquery.Client()
-filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_ATTACHMENT/tableSkuAttachment.json'
-dataset_id = 'landing_zone'
-table_id = 'shopstar_vtex_sku_attachment'
-dataset_ref = client.dataset(dataset_id)
-table_ref = dataset_ref.table(table_id)
-job_config = bigquery.LoadJobConfig()
-job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-job_config.autodetect = True
-with open(filename, "rb") as source_file:
-    job = client.load_table_from_file(
-        source_file,
-        table_ref,
-        location="southamerica-east1",  # Must match the destination dataset location.
-    job_config=job_config,)  # API request
-job.result()  # Waits for table load to complete.
-print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-print("finalizado")
-
-
+def run():
+	try:
+		get_params()
+		df = init.df
+		df.reset_index(drop=True, inplace=True)
+		json_data = df.to_json(orient = 'records')
+		json_object = json.loads(json_data)
+		
+		project_id = '999847639598'
+		dataset_id = 'landing_zone'
+		table_id = 'shopstar_vtex_sku_Attachment'
+		
+		client  = bigquery.Client(project = project_id)
+		dataset  = client.dataset(dataset_id)
+		table = dataset.table(table_id)
+		job_config = bigquery.LoadJobConfig()
+		job_config.write_disposition = "WRITE_TRUNCATE"
+		job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+		job_config.autodetect = True
+		job = client.load_table_from_json(json_object, table, job_config = job_config)
+		print(job.result())
+		delete_duplicate()
+	except:
+		print("No se puede ingestar")
+    
+run()
