7d4145575c14a0464fa975ce042adbe8617660f4
bredg <bred_valenzuela@gmail.com>
2021-11-22 21:09:22 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/Order.py
+++ vtex/orders_api/ORDERS/Order.py
@@ -50,9 +50,9 @@ class init:
     invoicedDate : None
     headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 
-def get_sku(id,reg):
+def get_order(id,reg):
     try:
-        url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str()+""
+        url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
         response = requests.request("GET", url, headers=init.headers)
         Fjson = json.loads(response.text)
         df1 = pd.DataFrame({
@@ -107,37 +107,44 @@ def get_params():
     rows = query_job.result()
     registro = 1
     for row in rows:
-        get_sku(row.orderId,registro)
+        get_order(row.orderId,registro)
         registro += 1
 
 def delete_duplicate():
-    client = bigquery.Client()
-    QUERY = (
-        'CREATE OR REPLACE TABLE `shopstar-datalake.landing_zone.shopstar_vtex_sku` AS SELECT DISTINCT * FROM `shopstar-datalake.landing_zone.shopstar_vtex_sku`')
-    query_job = client.query(QUERY)  
-    rows = query_job.result()
-    print(rows)
+	try:
+		print("Eliminando duplicados")
+		client = bigquery.Client()
+		QUERY = (
+			'CREATE OR REPLACE TABLE `shopstar-datalake.landing_zone.shopstar_vtex_order` AS SELECT DISTINCT * FROM `shopstar-datalake.landing_zone.shopstar_vtex_order`')
+		query_job = client.query(QUERY)
+		rows = query_job.result()
+		print(rows)
+	except:
+		print("Consulta SQL no ejecutada")
 
 def run():
-    get_params()
-    df = init.df
-    df.reset_index(drop=True, inplace=True)
-    json_data = df.to_json(orient = 'records')
-    json_object = json.loads(json_data)
-    
-    project_id = '999847639598'
-    dataset_id = 'landing_zone'
-    table_id = 'shopstar_vtex_sku'
-
-    client  = bigquery.Client(project = project_id)
-    dataset  = client.dataset(dataset_id)
-    table = dataset.table(table_id)
-    job_config = bigquery.LoadJobConfig()
-    job_config.write_disposition = "WRITE_TRUNCATE"
-    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job_config.autodetect = True
-    job = client.load_table_from_json(json_object, table, job_config = job_config)
-    print(job.result())
-    delete_duplicate()
+	try:
+	    get_params()
+	    df = init.df
+	    df.reset_index(drop=True, inplace=True)
+	    json_data = df.to_json(orient = 'records')
+	    json_object = json.loads(json_data)
+	    
+	    project_id = '999847639598'
+	    dataset_id = 'landing_zone'
+	    table_id = 'shopstar_vtex_order'
+	
+	    client  = bigquery.Client(project = project_id)
+	    dataset  = client.dataset(dataset_id)
+	    table = dataset.table(table_id)
+	    job_config = bigquery.LoadJobConfig()
+	    job_config.write_disposition = "WRITE_TRUNCATE"
+	    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+	    job_config.autodetect = True
+	    job = client.load_table_from_json(json_object, table, job_config = job_config)
+	    print(job.result())
+	    delete_duplicate()
+    except:
+    	print("Error al ingesta")
     
 run()
