5f635ac8d137c2eea5aa77ae9fa9384ba2cd6245
bredg <bred_valenzuela@gmail.com>
2021-11-24 13:45:25 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/Order.py
+++ vtex/orders_api/ORDERS/Order.py
@@ -179,7 +179,7 @@ def get_order(id,reg):
                 init.total_id_tax = tax["id"]
                 init.total_name_tax = tax["name"]
                 init.total_value_tax = tax["value"]
-            if  Total[4] in Total.keys():
+            if  Total.keys in Total[4]:
                 change = Total[4]
                 init.total_id_change = change["id"]
                 init.total_name_change = change["name"]
