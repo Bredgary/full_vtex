e8f8d4f33039e4d1f416e35a3ee4db54a0c6cbbf
bredg <bred_valenzuela@gmail.com>
2021-11-24 18:45:35 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/Order.py
+++ vtex/orders_api/ORDERS/Order.py
@@ -229,7 +229,7 @@ def get_order(id,reg):
         itemAttachment = Items["itemAttachment"]
         additionalInfo = Items["additionalInfo"]
         priceDefinition = Items["priceDefinition"]
-        sellingPrice = priceDefinition["sellingPrice"]
+        sellingPrice = Items["sellingPrice"]
         quantity = sellingPrice[0]
         dimension = additionalInfo["dimension"]
         '''
