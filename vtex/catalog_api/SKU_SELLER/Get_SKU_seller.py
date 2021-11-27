4fffe1c01a30b3ec8681d0f2184b09ed869e15ae
bredg <bred_valenzuela@gmail.com>
2021-11-27 19:59:40 -0300

    MOD: prueba

--- vtex/catalog_api/SKU_SELLER/Get_SKU_seller.py
+++ vtex/catalog_api/SKU_SELLER/Get_SKU_seller.py
@@ -67,7 +67,7 @@ def run():
         df.reset_index(drop=True, inplace=True)
         json_data = df.to_json(orient = 'records')
         json_object = json.loads(json_data)
-        
+        print(df)
         project_id = '999847639598'
         dataset_id = 'landing_zone'
         table_id = 'shopstar_vtex_sku_seller'
