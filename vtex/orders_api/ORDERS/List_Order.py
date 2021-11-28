e3ba46412608f19d69838e14e2705f21862a309b
bredg <bred_valenzuela@gmail.com>
2021-11-28 20:03:10 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -10,21 +10,6 @@ from datetime import timedelta
 from os import system
 from google.cloud import bigquery
 import logging
-from datetime import datetime
-from datetime import datetime, timezone
-
-naive_dt = datetime.now()
-aware_dt = naive_dt.astimezone()
-# correct, ISO-8601 (but not UTC)
-aware_dt.isoformat(timespec="seconds")
-# lets get the time in UTC
-utc_dt = aware_dt.astimezone(timezone.utc)
-# correct, ISO-8601 and UTC (but not in UTC format)
-date_str = utc_dt.isoformat(timespec='milliseconds')
-date = date_str.replace("+00:00", "Z")
-
-now = datetime.now()
-format = now.strftime('%Y-%m-%d')
 
 class Init:
 	today = datetime.date.today()
