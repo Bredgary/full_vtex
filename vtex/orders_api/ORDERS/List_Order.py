9f825b1859ada9e0152fd3fefd1fdb199486ed0d
bredg <bred_valenzuela@gmail.com>
2021-11-28 18:35:04 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -10,6 +10,7 @@ from datetime import timedelta
 from os import system
 from google.cloud import bigquery
 import logging
+from datetime import datetime
 from datetime import datetime, timezone
 
 naive_dt = datetime.now()
