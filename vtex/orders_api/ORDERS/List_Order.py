c0692be43acbd1760fe1a025f71602dddcb26850
bredg <bred_valenzuela@gmail.com>
2021-11-28 20:19:05 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -12,6 +12,14 @@ from datetime import timedelta
 from os import system
 from google.cloud import bigquery
 import logging
+
+class Init:
+	today = datetime.date.today()
+	yesterday = today - datetime.timedelta(days=1)
+	before_yesterday = today - datetime.timedelta(days=2)
+	ordenes = {}
+	df = pd.DataFrame()
+	
 import pandas as pd
 import numpy as np
 from google.cloud import bigquery
@@ -20,18 +28,19 @@ from datetime import datetime
 import requests
 from datetime import datetime, timezone
 
-naive_dt = datetime.now()
-aware_dt = naive_dt.astimezone()
-# correct, ISO-8601 (but not UTC)
-aware_dt.isoformat(timespec="seconds")
-# lets get the time in UTC
-utc_dt = aware_dt.astimezone(timezone.utc)
-# correct, ISO-8601 and UTC (but not in UTC format)
-date_str = utc_dt.isoformat(timespec='milliseconds')
-date = date_str.replace("+00:00", "Z")
-
-now = datetime.now()
-format = now.strftime('%Y-%m-%d')
+class date:
+	naive_dt = datetime.now()
+	aware_dt = naive_dt.astimezone()
+	# correct, ISO-8601 (but not UTC)
+	aware_dt.isoformat(timespec="seconds")
+	# lets get the time in UTC
+	utc_dt = aware_dt.astimezone(timezone.utc)
+	# correct, ISO-8601 and UTC (but not in UTC format)
+	date_str = utc_dt.isoformat(timespec='milliseconds')
+	date = date_str.replace("+00:00", "Z")
+	
+	now = datetime.now()
+	format = now.strftime('%Y-%m-%d')
 
 
 class Init:
