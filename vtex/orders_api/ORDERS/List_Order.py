b147bfa08d83f0b8b130329aa0c2716772282f1d
bredg <bred_valenzuela@gmail.com>
2021-12-04 13:03:17 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_Order.py
+++ vtex/orders_api/ORDERS/List_Order.py
@@ -31,6 +31,8 @@ class date:
     import requests
     from datetime import datetime, timezone
     
+    
+    
     naive_dt = datetime.now()
     aware_dt = naive_dt.astimezone()
     # correct, ISO-8601 (but not UTC)
@@ -43,7 +45,11 @@ class date:
     now = datetime.now()
     format = now.strftime('%Y-%m-%d')
     
-
+def format_schema(schema):
+    formatted_schema = []
+    for row in schema:
+        formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
+    return formatted_schema
 
 
 
@@ -52,37 +58,37 @@ def get_order_list(page):
     querystring = {"f_creationDate":"creationDate:["+str(init.yesterday)+"T02:00:00.000Z TO "+str(date.date)+"]","f_hasInputInvoice":"false"}
     headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
     response = requests.request("GET", url, headers=headers, params=querystring)
-    FJ = json.loads(response.text)
-    FJsonn = FJ["list"]
-    if FJsonn:
-        for FJson in FJsonn:
+    FJTemp = json.loads(response.text)
+    FJson = FJTemp["list"]
+    if FJson:
+        for x in FJson:
             init.reg +=1
             df1 = pd.DataFrame({
-                'orderId': FJson["orderId"],
-                'creationDate': FJson["creationDate"],
-                'clientName': FJson["clientName"],
-                'totalValue': FJson["totalValue"],
-                'paymentNames': FJson["paymentNames"],
-                'status': FJson["status"],
-                'statusDescription': FJson["statusDescription"],
-                'marketPlaceOrderId': FJson["marketPlaceOrderId"],
-                'sequence': FJson["sequence"],
-                'salesChannel': FJson["salesChannel"],
-                'affiliateId': FJson["affiliateId"],
-                'origin': FJson["origin"],
-                'workflowInErrorSta1te': FJson["workflowInErrorState"],
-                'workflowInRetry': FJson["workflowInRetry"],
-                'lastMessageUnread': FJson["lastMessageUnread"],
-                'ShippingEstimatedDate': FJson["ShippingEstimatedDate"],
-                'ShippingEstimatedDateMax': FJson["ShippingEstimatedDateMax"],
-                'ShippingEstimatedDateMin': FJson["ShippingEstimatedDateMin"],
-                'orderIsComplete': FJson["orderIsComplete"],
-                'listId': FJson["listId"],
-                'listType': FJson["listType"],
-                'authorizedDate': FJson["authorizedDate"],
-                'callCenterOperatorName': FJson["callCenterOperatorName"],
-                'totalItems': FJson["totalItems"],
-                'currencyCode': FJson["currencyCode"]}, index=[0])
+                'orderId': str(x["orderId"]),
+                'creationDate': str(x["creationDate"]),
+                'clientName': str(x["clientName"]),
+                'totalValue': str(x["totalValue"]),
+                'paymentNames': str(x["paymentNames"]),
+                'status': str(x["status"]),
+                'statusDescription': str(x["statusDescription"]),
+                'marketPlaceOrderId': str(x["marketPlaceOrderId"]),
+                'sequence': str(x["sequence"]),
+                'salesChannel': str(x["salesChannel"]),
+                'affiliateId': str(x["affiliateId"]),
+                'origin': str(x["origin"]),
+                'workflowInErrorSta1te': str(x["workflowInErrorState"]),
+                'workflowInRetry': str(x["workflowInRetry"]),
+                'lastMessageUnread': str(x["lastMessageUnread"]),
+                'ShippingEstimatedDate': str(x["ShippingEstimatedDate"]),
+                'ShippingEstimatedDateMax': str(x["ShippingEstimatedDateMax"]),
+                'ShippingEstimatedDateMin': str(x["ShippingEstimatedDateMin"]),
+                'orderIsComplete': str(x["orderIsComplete"]),
+                'listId': str(x["listId"]),
+                'listType': str(x["listType"]),
+                'authorizedDate': str(x["authorizedDate"]),
+                'callCenterOperatorName': str(x["callCenterOperatorName"]),
+                'totalItems': str(x["totalItems"]),
+                'currencyCode': str(x["currencyCode"])}, index=[0])
             print("Registro: "+str(init.reg))
             init.df = init.df.append(df1)
 
@@ -111,6 +117,108 @@ def run():
     json_data = df.to_json(orient = 'records')
     json_object = json.loads(json_data)
     
+    table_schema = {
+        "name": "orderId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "creationDate",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "clientName",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "totalValue",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "paymentNames",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "status",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "statusDescription",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "marketPlaceOrderId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "sequence",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "salesChannel",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "affiliateId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "origin",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "workflowInErrorSta1te",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "workflowInRetry",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "lastMessageUnread",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "ShippingEstimatedDate",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "ShippingEstimatedDateMax",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "ShippingEstimatedDateMin",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "orderIsComplete",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "listId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "listType",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "authorizedDate",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "callCenterOperatorName",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "totalItems",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "currencyCode",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      }
+    
     project_id = '999847639598'
     dataset_id = 'staging_zone'
     table_id = 'shopstar_vtex_list_order'
@@ -121,7 +229,7 @@ def run():
     tableO = dataset.table(table_id)
     job_config = bigquery.LoadJobConfig()
     job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job_config.autodetect = True
+    job_config.schema = format_schema(table_schema)
     job = client.load_table_from_json(json_object, tableO, job_config = job_config)
     print(job.result())
     
@@ -129,7 +237,7 @@ def run():
     job_config_temp = bigquery.LoadJobConfig()
     job_config_temp.write_disposition = "WRITE_TRUNCATE"
     job_config_temp.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job_config.autodetect = True
+    job_config.schema = format_schema(table_schema)
     job = client.load_table_from_json(json_object, tableT, job_config = job_config_temp)
     print(job.result())
     delete_duplicate()
--- vtex/orders_api/ORDERS/List_order_History.py
+++ vtex/orders_api/ORDERS/List_order_History.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
 import pandas as pd
 import numpy as np
 import requests
@@ -29,6 +31,8 @@ class date:
     import requests
     from datetime import datetime, timezone
     
+    
+    
     naive_dt = datetime.now()
     aware_dt = naive_dt.astimezone()
     # correct, ISO-8601 (but not UTC)
@@ -41,43 +45,50 @@ class date:
     now = datetime.now()
     format = now.strftime('%Y-%m-%d')
     
+def format_schema(schema):
+    formatted_schema = []
+    for row in schema:
+        formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
+    return formatted_schema
+
+
 
 def get_order_list(page):
     url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders?page="+str(page)+""
     querystring = {"f_creationDate":"creationDate:[2020-01-01T02:00:00.000Z TO 2020-01-02T01:59:59.999Z]","f_hasInputInvoice":"false"}
     headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
     response = requests.request("GET", url, headers=headers, params=querystring)
-    FJ = json.loads(response.text)
-    FJsonn = FJ["list"]
-    if FJsonn:
-        for FJson in FJsonn:
+    FJTemp = json.loads(response.text)
+    FJson = FJTemp["list"]
+    if FJson:
+        for x in FJson:
             init.reg +=1
             df1 = pd.DataFrame({
-                'orderId': str(FJson["orderId"]),
-                'creationDate': str(FJson["creationDate"]),
-                'clientName': str(FJson["clientName"]),
-                'totalValue': str(FJson["totalValue"]),
-                'paymentNames': str(FJson["paymentNames"]),
-                'status': str(FJson["status"]),
-                'statusDescription': str(FJson["statusDescription"]),
-                'marketPlaceOrderId': str(FJson["marketPlaceOrderId"]),
-                'sequence': str(FJson["sequence"]),
-                'salesChannel': str(FJson["salesChannel"]),
-                'affiliateId': str(FJson["affiliateId"]),
-                'origin': str(FJson["origin"]),
-                'workflowInErrorSta1te': str(FJson["workflowInErrorState"]),
-                'workflowInRetry': str(FJson["workflowInRetry"]),
-                'lastMessageUnread': str(FJson["lastMessageUnread"]),
-                'ShippingEstimatedDate': str(FJson["ShippingEstimatedDate"]),
-                'ShippingEstimatedDateMax': str(FJson["ShippingEstimatedDateMax"]),
-                'ShippingEstimatedDateMin': str(FJson["ShippingEstimatedDateMin"]),
-                'orderIsComplete': str(FJson["orderIsComplete"]),
-                'listId': str(FJson["listId"]),
-                'listType': str(FJson["listType"]),
-                'authorizedDate': str(FJson["authorizedDate"]),
-                'callCenterOperatorName': str(FJson["callCenterOperatorName"]),
-                'totalItems': str(FJson["totalItems"]),
-                'currencyCode': str(FJson["currencyCode"])}, index=[0])
+                'orderId': str(x["orderId"]),
+                'creationDate': str(x["creationDate"]),
+                'clientName': str(x["clientName"]),
+                'totalValue': str(x["totalValue"]),
+                'paymentNames': str(x["paymentNames"]),
+                'status': str(x["status"]),
+                'statusDescription': str(x["statusDescription"]),
+                'marketPlaceOrderId': str(x["marketPlaceOrderId"]),
+                'sequence': str(x["sequence"]),
+                'salesChannel': str(x["salesChannel"]),
+                'affiliateId': str(x["affiliateId"]),
+                'origin': str(x["origin"]),
+                'workflowInErrorSta1te': str(x["workflowInErrorState"]),
+                'workflowInRetry': str(x["workflowInRetry"]),
+                'lastMessageUnread': str(x["lastMessageUnread"]),
+                'ShippingEstimatedDate': str(x["ShippingEstimatedDate"]),
+                'ShippingEstimatedDateMax': str(x["ShippingEstimatedDateMax"]),
+                'ShippingEstimatedDateMin': str(x["ShippingEstimatedDateMin"]),
+                'orderIsComplete': str(x["orderIsComplete"]),
+                'listId': str(x["listId"]),
+                'listType': str(x["listType"]),
+                'authorizedDate': str(x["authorizedDate"]),
+                'callCenterOperatorName': str(x["callCenterOperatorName"]),
+                'totalItems': str(x["totalItems"]),
+                'currencyCode': str(x["currencyCode"])}, index=[0])
             print("Registro: "+str(init.reg))
             init.df = init.df.append(df1)
 
@@ -106,6 +117,108 @@ def run():
     json_data = df.to_json(orient = 'records')
     json_object = json.loads(json_data)
     
+    table_schema = {
+        "name": "orderId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "creationDate",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "clientName",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "totalValue",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "paymentNames",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "status",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "statusDescription",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "marketPlaceOrderId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "sequence",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "salesChannel",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "affiliateId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "origin",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "workflowInErrorSta1te",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "workflowInRetry",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "lastMessageUnread",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "ShippingEstimatedDate",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "ShippingEstimatedDateMax",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "ShippingEstimatedDateMin",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "orderIsComplete",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "listId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "listType",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "authorizedDate",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "callCenterOperatorName",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "totalItems",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      },{
+        "name": "currencyCode",
+        "type": "STRING",
+        "mode": "NULLABLE"
+      }
+    
     project_id = '999847639598'
     dataset_id = 'staging_zone'
     table_id = 'shopstar_vtex_list_order'
@@ -116,7 +229,7 @@ def run():
     tableO = dataset.table(table_id)
     job_config = bigquery.LoadJobConfig()
     job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job_config.autodetect = True
+    job_config.schema = format_schema(table_schema)
     job = client.load_table_from_json(json_object, tableO, job_config = job_config)
     print(job.result())
     
@@ -124,13 +237,11 @@ def run():
     job_config_temp = bigquery.LoadJobConfig()
     job_config_temp.write_disposition = "WRITE_TRUNCATE"
     job_config_temp.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job_config.autodetect = True
+    job_config.schema = format_schema(table_schema)
     job = client.load_table_from_json(json_object, tableT, job_config = job_config_temp)
     print(job.result())
     delete_duplicate()
 
-
 run()
-
 
 
