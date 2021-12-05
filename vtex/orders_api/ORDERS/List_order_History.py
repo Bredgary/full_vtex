90e1a6bdad0a87160ee71a887fe5b10cd2daf859
bredg <bred_valenzuela@gmail.com>
2021-12-04 23:59:50 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_order_History.py
+++ vtex/orders_api/ORDERS/List_order_History.py
@@ -53,9 +53,9 @@ def format_schema(schema):
 
 def get_order_list(page):
     url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders?f_creationDate=creationDate:[2021-03-02T02:00:00.000Z TO 2021-03-03T01:59:59.999Z]"
-    #querystring = {"f_creationDate":"creationDate:[2021-03-02T02:00:00.000Z TO 2021-03-03T01:59:59.999Z]","f_hasInputInvoice":"false"}
+    querystring = {"f_hasInputInvoice":"false"}
     headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-    response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", url, headers=headers, params=querystring)
     FJTemp = json.loads(response.text)
     FJson = FJTemp["list"]
     if FJson:
