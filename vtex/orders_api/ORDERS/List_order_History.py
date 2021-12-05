d7738de5925eaec48a739582644fbda37452e217
bredg <bred_valenzuela@gmail.com>
2021-12-05 15:33:41 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/List_order_History.py
+++ vtex/orders_api/ORDERS/List_order_History.py
@@ -22,8 +22,8 @@ class init:
     df = pd.DataFrame()
     registro = 0
     reg = 0 
-    num_from = "05"
-    num_to ="05"
+    num_from = "06"
+    num_to ="06"
     mount = "06"
     '''
     30
