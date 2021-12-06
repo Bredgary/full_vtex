36947d1e587de53784437ea289de00f47bfb80ba
bredg <bred_valenzuela@gmail.com>
2021-12-06 00:38:45 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/Order.py
+++ vtex/orders_api/ORDERS/Order.py
@@ -1019,18 +1019,21 @@ def get_order(id,reg):
         emailTracked = None
         print("nulo")
     
-    try:
-        init.itemMetadata_Id = ItemMetadata["Id"]
-        init.itemMetadata_Seller = ItemMetadata["Seller"]
-        init.itemMetadata_Name = ItemMetadata["Name"]
-        init.itemMetadata_SkuName = ItemMetadata["SkuName"]
-        init.itemMetadata_ProductId = ItemMetadata["ProductId"]
-        init.itemMetadata_RefId = ItemMetadata["RefId"]
-        init.itemMetadata_Ean = ItemMetadata["Ean"]
-        init.itemMetadata_ImageUrl = ItemMetadata["ImageUrl"]
-        init.itemMetadata_DetailUrl = ItemMetadata["DetailUrl"]
-    except:
-        print("vacio")
+    #try:
+    init.itemMetadata_Id = ItemMetadata["Id"]
+    init.itemMetadata_Seller = ItemMetadata["Seller"]
+    init.itemMetadata_Name = ItemMetadata["Name"]
+    init.itemMetadata_SkuName = ItemMetadata["SkuName"]
+    init.itemMetadata_ProductId = ItemMetadata["ProductId"]
+    init.itemMetadata_RefId = ItemMetadata["RefId"]
+    init.itemMetadata_Ean = ItemMetadata["Ean"]
+    init.itemMetadata_ImageUrl = ItemMetadata["ImageUrl"]
+    init.itemMetadata_DetailUrl = ItemMetadata["DetailUrl"]
+    
+    print(init.itemMetadata_Id)
+    print(init.itemMetadata_Seller)
+    #except:
+    #    print("vacio")
     
     df1 = pd.DataFrame({
         'orderId': init.orderId,
