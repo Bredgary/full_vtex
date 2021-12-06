ced09a22f71fa3ce9724ffbaecab8a91941184fb
bredg <bred_valenzuela@gmail.com>
2021-12-06 00:09:05 -0300

    MOD: prueba

--- vtex/license_manager_api/STORE/Get_by_Account.py
+++ vtex/license_manager_api/STORE/Get_by_Account.py
@@ -14,7 +14,7 @@ class init:
     hosts = None
         
 def get_account():
-    try:
+    #try:
     	url = "https://mercury.vtexcommercestable.com.br/api/vlm/account/stores"
     	response = requests.request("GET", url, headers=headers)
     	Fjson = json.loads(response.text)
@@ -30,8 +30,8 @@ def get_account():
 			'hosts' : init.hosts}, index=[0])
     	init.df = init.df.append(df1)
     	print("Registro: "+str(reg))
-    except:
-    	print("Vacio")
+    #except:
+    #	print("Vacio")
 
     
 def delete_duplicate():
