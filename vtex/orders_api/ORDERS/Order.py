dc93acd16989af44e979312e9007fd22520e9e2f
bredg <bred_valenzuela@gmail.com>
2021-12-06 14:12:42 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/Order.py
+++ vtex/orders_api/ORDERS/Order.py
@@ -1395,7 +1395,7 @@ def get_order(id,reg):
         'EnableInferItems': init.EnableInferItems,
         'invoice_address': init.invoice_address,
         'userPaymentInfo': init.userPaymentInfo,
-        'serialNumbers':init.serialNumbers,
+        'serialNumbers':init.item_serialNumbers,
         'invoicedDate': init.invoicedDate}, index=[0])
     init.df = init.df.append(df1)
     print("Registro: "+str(reg))
