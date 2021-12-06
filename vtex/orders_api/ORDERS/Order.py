5ed19fd0f8153ded328a23bf9e7d6d524fc45b23
bredg <bred_valenzuela@gmail.com>
2021-12-06 18:35:07 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/Order.py
+++ vtex/orders_api/ORDERS/Order.py
@@ -1466,7 +1466,7 @@ def get_params():
     for row in rows:
         get_order(row.orderId,registro)
         registro += 1
-        if registro == 101:
+        if registro == 2000:
             break
         
 def delete_duplicate():
