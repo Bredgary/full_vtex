8d2c465297fe54861063d47d090a01f37f0289cc
bredg <bred_valenzuela@gmail.com>
2021-12-08 16:21:39 -0300

    MOD: prueba

--- vtex/catalog_api/SUB_COLLECTION/Get_Subcollection_by_Collection_ID.py
+++ vtex/catalog_api/SUB_COLLECTION/Get_Subcollection_by_Collection_ID.py
@@ -1,74 +1,81 @@
-import requests
-import json
-import os
-import re
-from datetime import datetime
-from os import system
+import pandas as pd
+import numpy as np
 from google.cloud import bigquery
+import os, json
+from datetime import datetime
+import requests
+from datetime import datetime, timezone
 
-client = bigquery.Client()
-productList = []
-count = 0
+class init:
+    df = pd.DataFrame()
+    headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+    
+
+def get_subCollection(id,reg):
+    try:
+        url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/collection/"+str(id)+"/subcollection"
+        response = requests.request("GET", url, headers=init.headers)
+        print(Fjson)
+        df1 = pd.DataFrame({
+            'Id' : Fjson["Id"],
+			'CollectionId' : Fjson["CollectionId"],
+			'Name' : Fjson["Name"],
+			'Type' : Fjson["Type"],
+			'PreSale' : Fjson["PreSale"],
+			'Release': Fjson["Release"]}, index=[0])
+        init.df = init.df.append(df1)
+        print("Registro: "+str(reg))
+    except:
+        print("Vacio")
 
 
-def get_subcollection(id):
-	try:
-		url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/collection/"+str(id)+"/subcollection"
-		headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-		response = requests.request("GET", url, headers=headers)
-		FJson = json.loads(response.text)
-		result = json.dumps(FJson)
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SUB_COLLECTION/temp.json", "w")
-		text_file.write(result)
-		text_file.close()
-		cargando_bigquery()
-	except:
-		print("Vacio")
+def get_params():
+    print("Cargando consulta")
+    client = bigquery.Client()
+    QUERY = (
+        'SELECT id FROM `shopstar-datalake.staging_zone.shopstar_vtex_collection`')
+    query_job = client.query(QUERY)  
+    rows = query_job.result()
+    registro = 1
+    for row in rows:
+        get_subCollection(row.id,registro)
+        registro += 1
+    
+def delete_duplicate():
+    try:
+        print("Borrando duplicados")
+        client = bigquery.Client()
+        QUERY = (
+            'CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_vtex_sub_collection` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_vtex_sub_collection`')
+        query_job = client.query(QUERY)  
+        rows = query_job.result()
+        print(rows)
+    except:
+        print("Query no ejecutada")
 
-def cargando_bigquery():
-	system("cat temp.json | jq -c '.[]' > sub_collection.json")
-	print("Cargando a BigQuery")
-	client = bigquery.Client()
-	filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SUB_COLLECTION/sub_collection.json'
-	dataset_id = 'landing_zone'
-	table_id = 'shopstar_vtex_sub_collection'
-	dataset_ref = client.dataset(dataset_id)
-	table_ref = dataset_ref.table(table_id)
-	job_config = bigquery.LoadJobConfig()
-	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-	job_config.autodetect = True
-	with open(filename, "rb") as source_file:
-		job = client.load_table_from_file(
-			source_file,
-			table_ref,
-			location="southamerica-east1",  # Must match the destination dataset location.
-		job_config=job_config,)  # API request
-	job.result()  # Waits for table load to complete.
-	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-	print("finalizado")
-
-def operacion_fenix(count):
-	f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/SUB_COLLECTION/id_collecion.json','r')
-	data_from_string = f_01.read()
-	listaIDS = json.loads(data_from_string)
-	for i in listaIDS:
-		get_subcollection(i)
-		count += 1
-		print(str(count)+" registro almacenado.")
-
-operacion_fenix(count)
-
-'''
-QUERY = (
-    'SELECT id FROM `shopstar-datalake.landing_zone.shopstar_vtex_collection_beta`')
-query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
-
-for row in rows:
-    productList.append(row.id)
-
-string = json.dumps(productList)
-text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SUB_COLLECTION/id_collecion.json", "w")
-text_file.write(string)
-text_file.close()
-'''
+def run():
+    try:
+        get_params()
+        df = init.df
+        df.reset_index(drop=True, inplace=True)
+        json_data = df.to_json(orient = 'records')
+        json_object = json.loads(json_data)
+        print(df)
+        project_id = '999847639598'
+        dataset_id = 'staging_zone'
+        table_id = 'shopstar_vtex_sub_collection'
+    
+        client  = bigquery.Client(project = project_id)
+        dataset  = client.dataset(dataset_id)
+        table = dataset.table(table_id)
+        job_config = bigquery.LoadJobConfig()
+        job_config.write_disposition = "WRITE_TRUNCATE"
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        job_config.autodetect = True
+        job = client.load_table_from_json(json_object, table, job_config = job_config)
+        print(job.result())
+        delete_duplicate()
+    except:
+        print("vacio")
+    
+run()
