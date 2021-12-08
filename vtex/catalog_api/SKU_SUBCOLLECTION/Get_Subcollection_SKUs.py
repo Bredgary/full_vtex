aa38f4d7c5ff4de31df8e701d6ba7ff29d5846b6
bredg <bred_valenzuela@gmail.com>
2021-12-08 17:00:53 -0300

    MOD: prueba

--- vtex/catalog_api/SKU_SUBCOLLECTION/Get_Subcollection_SKUs.py
+++ vtex/catalog_api/SKU_SUBCOLLECTION/Get_Subcollection_SKUs.py
@@ -1,75 +1,86 @@
-import requests
-import json
-import os
-import re
-from datetime import datetime
-from os import system
+import pandas as pd
+import numpy as np
 from google.cloud import bigquery
+import os, json
+from datetime import datetime
+import requests
+from datetime import datetime, timezone
 
-client = bigquery.Client()
-productList = []
-count = 0
+class init:
+    df = pd.DataFrame()
+    
+    url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/subcollection/1103/stockkeepingunit"
+    headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+    response = requests.request("GET", url, headers=headers)
+    FormatoJson = json.loads(response.text)
+    page = FormatoJson["page"]
+    size = FormatoJson["size"]
 
-def get_aen(id):
-	try:
-		url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/subcollection/"+str(id)+"/stockkeepingunit"
-		querystring = {"page":"1","size":"10000"}
-		headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-		response = requests.request("GET", url, headers=headers, params=querystring)
-		FJson = json.loads(response.text)
-		result = json.dumps(FJson)
-		text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_SUBCOLLECTION/temp.json", "w")
-		text_file.write(result)
-		text_file.close()
-		cargando_bigquery()
-	except:
-		print("Vacio")
+def get_subCollectionSKU(id,reg):
+    try:
+    	querystring = {"page":""+str(init.page)+"","size":""+str(init.size)+""}
+    	url = "https://mercury.vtexcommercestable.com.br/api/catalog/pvt/collection/"+str(id)+"/subcollection"
+    	response = requests.request("GET", url, headers=init.headers, params=querystring)
+    	data = json.loads(response.text)
+    	Fjson = data["Data"]
+    	if Fjson:
+    		for x in Fjson:
+    			df1 = pd.DataFrame({
+					'SubCollectionId' : x["SubCollectionId"],
+					'SkuId': x["SkuId"]}, index=[0])
+    			init.df = init.df.append(df1)
+    			print("Registro: "+str(reg))
+    except:
+        print("Vacio")
+        
 
-def cargando_bigquery():
-	#system("cat temp.json | jq -c '.[]' > eat_table.json")
-	print("Cargando a BigQuery")
-	client = bigquery.Client()
-	filename = '/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_SUBCOLLECTION/temp.json'
-	dataset_id = 'landing_zone'
-	table_id = 'shopstar_vtex_sku_sub_collection'
-	dataset_ref = client.dataset(dataset_id)
-	table_ref = dataset_ref.table(table_id)
-	job_config = bigquery.LoadJobConfig()
-	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-	job_config.autodetect = True
-	with open(filename, "rb") as source_file:
-		job = client.load_table_from_file(
-			source_file,
-			table_ref,
-			location="southamerica-east1",  # Must match the destination dataset location.
-		job_config=job_config,)  # API request
-	job.result()  # Waits for table load to complete.
-	print("Loaded {} rows into {}:{}.".format(job.output_rows, dataset_id, table_id))
-	print("finalizado")
+def get_params():
+    print("Cargando consulta")
+    client = bigquery.Client()
+    QUERY = (
+        'SELECT Id FROM `shopstar-datalake.staging_zone.shopstar_vtex_sub_collection`')
+    query_job = client.query(QUERY)  
+    rows = query_job.result()
+    registro = 1
+    for row in rows:
+        get_subCollectionSKU(row.Id,registro)
+        registro += 1
+    
+def delete_duplicate():
+    try:
+        print("Borrando duplicados")
+        client = bigquery.Client()
+        QUERY = (
+            'CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_vtex_sku_sub_collection` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_vtex_sku_sub_collection`')
+        query_job = client.query(QUERY)  
+        rows = query_job.result()
+        print(rows)
+    except:
+        print("Query no ejecutada")
 
-def operacion_fenix(count):
-	f_01 = open ('/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_SUBCOLLECTION/id_sub_collection.json','r')
-	data_from_string = f_01.read()
-	listaIDS = json.loads(data_from_string)
-	for i in listaIDS:
-		get_aen(i)
-		count += 1
-		#print(str(count)+" registro almacenado.")
-
-operacion_fenix(count)
-
-
-'''
-QUERY = (
-    'SELECT id FROM `shopstar-datalake.landing_zone.shopstar_vtex_sub_collection`')
-query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
-
-for row in rows:
-    productList.append(row.id)
-
-string = json.dumps(productList)
-text_file = open("/home/bred_valenzuela/full_vtex/vtex/catalog_api/SKU_SUBCOLLECTION/id_sub_collection.json", "w")
-text_file.write(string)
-text_file.close()
-'''
+def run():
+    try:
+        get_params()
+        df = init.df
+        df.reset_index(drop=True, inplace=True)
+        json_data = df.to_json(orient = 'records')
+        json_object = json.loads(json_data)
+        print(df)
+        project_id = '999847639598'
+        dataset_id = 'staging_zone'
+        table_id = 'shopstar_vtex_sku_sub_collection'
+    
+        client  = bigquery.Client(project = project_id)
+        dataset  = client.dataset(dataset_id)
+        table = dataset.table(table_id)
+        job_config = bigquery.LoadJobConfig()
+        job_config.write_disposition = "WRITE_TRUNCATE"
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        job_config.autodetect = True
+        job = client.load_table_from_json(json_object, table, job_config = job_config)
+        print(job.result())
+        delete_duplicate()
+    except:
+        print("vacio")
+    
+run()
