745d81cfa8cc6d73e760a310fe72af4adc3a260f
bredg <bred_valenzuela@gmail.com>
2021-12-11 18:04:24 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/client.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/client.py
@@ -96,8 +96,6 @@ def get_params():
     for row in rows:
         get_order(row.orderId,registro)
         registro += 1
-        if registro == 15:
-            break
         
 def delete_duplicate():
     try:
