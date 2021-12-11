5b5329be0c50d52fd8db5f991f2751d404487f41
bredg <bred_valenzuela@gmail.com>
2021-12-11 16:36:59 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/client.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/client.py
@@ -64,7 +64,6 @@ def get_params():
         if registro == 15:
             break
         
-        
 def run(orderId,registro):
     df = pd.DataFrame(cl_client(orderId,registro),
                     columns=['orderId','beneficio','beneficio2','crearGiftcard','profilePicture','proteccionDatos','terminosCondiciones','terminosPago','tradeName','rclastcart','rclastsession','rclastsessiondate','homePhone','phone','stateRegistration','email','userId','firstName','lastName','document','localeDefault','attach','approved','birthDate','businessPhone','corporateDocument','corporateName','documentType','gender','customerClass','priceTables','id','accountId','accountName','dataEntityId','createdBy','createdIn','updatedBy','updatedIn','lastInteractionBy','lastInteractionIn','followers','auto_filter'])
