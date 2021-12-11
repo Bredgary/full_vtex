d22fdded97cd5d76c58913a47fd41429508d6e34
bredg <bred_valenzuela@gmail.com>
2021-12-11 17:36:39 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/client.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/client.py
@@ -5,55 +5,88 @@ import os, json
 from datetime import datetime
 import requests
 from datetime import datetime, timezone
+from os.path import join
 
 class init:
     productList = []
     df = pd.DataFrame()
-    headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-
-def cl_client(orderId,reg):
-    url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(orderId)+""
-    response = requests.request("GET", url, headers=init.headers)
-    Fjson2 = json.loads(response.text)
-    clientProfileData = Fjson2["clientProfileData"]
-    document = clientProfileData["document"]
     
-    url = "https://mercury.vtexcommercestable.com.br/api/dataentities/CL/search"
-    querystring = {"_fields":"beneficio,beneficio2,crearGiftcard,profilePicture,proteccionDatos,terminosCondiciones,terminosPago,tradeName,rclastcart,rclastsession,rclastsessiondate,homePhone,phone,stateRegistration,email,userId,firstName,lastName,document,localeDefault,attach,approved,birthDate,businessPhone,corporateDocument,corporateName,documentType,gender,customerClass,priceTables,id,accountId,accountName,dataEntityId,createdBy,createdIn,updatedBy,updatedIn,lastInteractionBy,lastInteractionIn","_where":"document="+document+""}
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/vnd.vtex.ds.v10+json",
-        "REST-Range": "resources=0-1",
-        "X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA",
-        "X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    Fjson = json.loads(response.text)
-    id = Fjson["id"]
-    df1 = pd.DataFrame({
-            'id': str(id),
-            'orderId': str(orderId)}, index=[0])
-    init.df = init.df.append(df1)
-    print("registro = "+str(reg))
-
-def format_schema(schema):
-    formatted_schema = []
-    for row in schema:
-        formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
-    return formatted_schema
-
-def delete_duplicate():
+    id = None
+    email = None
+    firstName = None
+    lastName = None
+    documentType = None
+    document = None
+    phone = None
+    corporateName = None
+    tradeName = None
+    corporateDocument = None
+    stateInscription = None
+    corporatePhone = None
+    isCorporate = None
+    userProfileId = None
+    
+    headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+    
+def decrypt_email(email):
     try:
-        print("Eliminando duplicados")
-        client = bigquery.Client()
-        QUERY = (
-            'CREATE OR REPLACE TABLE `shopstar-datalake.test.shopstar_vtex_order_client` AS SELECT DISTINCT * FROM `shopstar-datalake.test.shopstar_vtex_order_client`')
-        query_job = client.query(QUERY)
-        rows = query_job.result()
-        print(rows)
+        url = "https://conversationtracker.vtex.com.br/api/pvt/emailMapping?an=mercury&alias="+email+""
+        headers = {"Accept": "application/json","Content-Type": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+        response = requests.request("GET", url, headers=headers)
+        formatoJ = json.loads(response.text)
+        return formatoJ["email"]
     except:
-        print("Consulta SQL no ejecutada")
-
+        print("No se pudo desencriptar Email: "+str(email))
+        
+def get_order(id,reg):
+    try:
+        reg +=1
+        url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
+        response = requests.request("GET", url, headers=init.headers)
+        Fjson = json.loads(response.text)
+        try:
+            clientProfileData = Fjson["clientProfileData"]
+            init.id = clientProfileData["id"]
+            init.email = clientProfileData["email"]
+            init.firstName = clientProfileData["firstName"]
+            init.lastName = clientProfileData["lastName"]
+            init.documentType = clientProfileData["documentType"]
+            init.document = clientProfileData["document"]
+            init.phone = clientProfileData["phone"]
+            init.corporateName = clientProfileData["corporateName"]
+            init.tradeName = clientProfileData["tradeName"]
+            init.corporateDocument = clientProfileData["corporateDocument"]
+            init.stateInscription = clientProfileData["stateInscription"]
+            init.corporatePhone = clientProfileData["corporatePhone"]
+            init.isCorporate = clientProfileData["isCorporate"]
+            init.suserProfileId = clientProfileData["suserProfileId"]
+            decrypt_email(init.email)
+            print("Registro: "+str(reg))
+        except:
+            print("Registro: "+str(reg))
+        df1 = pd.DataFrame({
+            'orderId': id,
+            'dim_client': init.id,
+            'email': init.email,
+            'firstName': init.firstName,
+            'lastName': init.lastName,
+            'documentType': init.documentType,
+            'document': init.document,
+            'phone': init.phone,
+            'corporateName': init.corporateName,
+            'tradeName': init.tradeName,
+            'corporateDocument': init.corporateDocument,
+            'stateInscription': init.stateInscription,
+            'corporatePhone': init.corporatePhone,
+            'isCorporate': init.isCorporate,
+            'suserProfileId': init.suserProfileId}, index=[0])
+        init.df = init.df.append(df1)
+    except:
+        print("Vacio")
+        print("Registro: "+str(reg))
+        
+        
+        
 def get_params():
     print("Cargando consulta")
     client = bigquery.Client()
@@ -61,207 +94,44 @@ def get_params():
         'SELECT orderId FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`')
     query_job = client.query(QUERY)  
     rows = query_job.result()
-    registro = 0
+    registro = 1
     for row in rows:
+        get_order(row.orderId,registro)
         registro += 1
         if registro == 15:
             break
         
+def delete_duplicate():
+    try:
+        print("Eliminando duplicados")
+        client = bigquery.Client()
+        QUERY = (
+            'CREATE OR REPLACE TABLE `shopstar-datalake.test.shopstar_order_client` AS SELECT DISTINCT * FROM `shopstar-datalake.test.shopstar_order_client`')
+        query_job = client.query(QUERY)
+        rows = query_job.result()
+        print(rows)
+    except:
+        print("Consulta SQL no ejecutada")
+
+
 def run():
     get_params()
     df = init.df
     df.reset_index(drop=True, inplace=True)
     json_data = df.to_json(orient = 'records')
     json_object = json.loads(json_data)
-
-
     
-    table_schema = {
-        "name": "orderId",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "id",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "email",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "userId",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "firstName",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "lastName",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "document",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "localeDefault",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "attach",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "accountId",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "accountName",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "dataEntityId",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "createdBy",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "createdIn",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "updatedBy",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "beneficio2",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "crearGiftcard",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "profilePicture",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "proteccionDatos",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "terminosCondiciones",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "terminosPago",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "tradeName",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "rclastcart",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "rclastsession",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "rclastsessiondate",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "homePhone",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "phone",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "stateRegistration",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "approved",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "birthDate",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "businessPhone",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "corporateDocument",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "corporateName",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "documentType",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "gender",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "customerClass",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "priceTables",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "beneficio",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "updatedIn",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "lastInteractionBy",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "lastInteractionIn",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      },{
-        "name": "followers",
-        "type": "STRING",
-        "mode": "REPEATED"
-      },{
-        "name": "auto_filter",
-        "type": "STRING",
-        "mode": "NULLABLE"
-      }
-
     project_id = '999847639598'
     dataset_id = 'test'
-    table_id = 'shopstar_vtex_order_client'
+    table_id = 'shopstar_order_client'
     
     client  = bigquery.Client(project = project_id)
     dataset  = client.dataset(dataset_id)
     table = dataset.table(table_id)
-    
     job_config = bigquery.LoadJobConfig()
+    job_config.write_disposition = "WRITE_TRUNCATE"
     job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job_config.schema = format_schema(table_schema)
-    #job_config.autodetect = True
+    job_config.autodetect = True
     job = client.load_table_from_json(json_object, table, job_config = job_config)
     print(job.result())
     delete_duplicate()
