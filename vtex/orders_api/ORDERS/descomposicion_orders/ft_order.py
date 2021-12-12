2d4649e076134cb4af13ab78086ce97efc909469
bredg <bred_valenzuela@gmail.com>
2021-12-12 11:53:11 -0300

    MOD: pueba

--- vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
@@ -164,6 +164,8 @@ def get_params():
     for row in rows:
         get_order(row.orderId,registro)
         registro += 1
+        if registro == 15:
+            break
         
 def delete_duplicate():
     try:
