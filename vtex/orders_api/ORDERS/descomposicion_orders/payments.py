1fb07e306d17db45aaf25e04f906f10bf7627e05
bredg <bred_valenzuela@gmail.com>
2021-12-12 14:55:53 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/payments.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/payments.py
@@ -125,7 +125,7 @@ def get_order(id,reg):
 def get_params():
     print("Cargando consulta")
     client = bigquery.Client()
-    QUERY = ('SELECT orderId FROM `shopstar-datalake.staging_zone.order_write`')
+    QUERY = ('SELECT orderId FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`')
     query_job = client.query(QUERY)
     rows = query_job.result()
     registro = 1
