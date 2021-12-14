978f217ff0bffff8d502a5d3afa67e1cffd058c0
bredg <bred_valenzuela@gmail.com>
2021-12-14 13:20:54 -0300

    MOD: prueba

--- vtex/entity/CL/test.py
+++ vtex/entity/CL/test.py
@@ -4,7 +4,7 @@ def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-start_date = date(2013, 1, 1)
-end_date = date(2015, 6, 2)
+start_date = date(2010, 1, 1)
+end_date = date(2021, 12, 14)
 for single_date in daterange(start_date, end_date):
     print(single_date.strftime("%Y-%m-%d"))
