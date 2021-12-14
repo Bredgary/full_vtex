470ba9d11ef593c9e4fbbc9828aa2bf0127ac244
bredg <bred_valenzuela@gmail.com>
2021-12-14 13:31:03 -0300

    MOD: prueba

--- vtex/entity/CL/HistoryClient.py
+++ vtex/entity/CL/HistoryClient.py
@@ -7,22 +7,6 @@ import requests
 from datetime import datetime, timezone
 from datetime import date, timedelta
 
-
-
-
-
-def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
-        yield start_date + timedelta(n)
-
-start_date = date(2016, 1, 1)
-end_date = date(2021, 12, 15)
-for single_date in daterange(start_date, end_date):
-    variFecha = single_date.strftime("%Y-%m-%d")
-    print("Dia: "+str(variFecha))
-    run(variFecha)
-
-
 def cl_client(fecha):
 	print("Cargando")
 	url = "https://mercury.vtexcommercestable.com.br/api/dataentities/CL/search"
@@ -241,4 +225,21 @@ def run(variFecha):
 	job = client.load_table_from_json(json_object, table, job_config = job_config)
 	print(job.result())
 	delete_duplicate()
+
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+start_date = date(2016, 1, 1)
+end_date = date(2021, 12, 15)
+for single_date in daterange(start_date, end_date):
+    variFecha = single_date.strftime("%Y-%m-%d")
+    print("Dia: "+str(variFecha))
+    run(variFecha)
+
+
+
+
+
 	
