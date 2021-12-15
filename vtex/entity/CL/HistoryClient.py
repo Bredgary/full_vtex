e10eaad8fef58778b14268cf9f0a30f4abfd4d76
bredg <bred_valenzuela@gmail.com>
2021-12-14 23:20:39 -0300

    MOD: prueba

--- vtex/entity/CL/HistoryClient.py
+++ vtex/entity/CL/HistoryClient.py
@@ -33,7 +33,7 @@ def delete_duplicate():
 		print("Eliminando duplicados")
 		client = bigquery.Client()
 		QUERY = (
-			'CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_vtex_client_1` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_vtex_client_1`')
+			'CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_vtex_client_` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_vtex_client_`')
 		query_job = client.query(QUERY)
 		rows = query_job.result()
 		print(rows)
@@ -212,7 +212,7 @@ def run(variFecha):
 
 	project_id = '999847639598'
 	dataset_id = 'staging_zone'
-	table_id = 'shopstar_vtex_client'
+	table_id = 'shopstar_vtex_client_'
 	
 	client  = bigquery.Client(project = project_id)
 	dataset  = client.dataset(dataset_id)
@@ -231,7 +231,7 @@ def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-start_date = date(2019, 1, 1)
+start_date = date(2019, 7, 1)
 end_date = date(2021, 12, 16)
 for single_date in daterange(start_date, end_date):
     variFecha = single_date.strftime("%Y-%m-%d")
