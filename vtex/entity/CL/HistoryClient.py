ca78361f5fa00a8c70bec89f07d3565b502d0c2a
bredg <bred_valenzuela@gmail.com>
2021-12-15 00:29:22 -0300

    MOD: prueba

--- vtex/entity/CL/HistoryClient.py
+++ vtex/entity/CL/HistoryClient.py
@@ -33,7 +33,7 @@ def delete_duplicate():
 		print("Eliminando duplicados")
 		client = bigquery.Client()
 		QUERY = (
-			'CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_vtex_client_` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_vtex_client_`')
+			'CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_vtex_client_01` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_vtex_client_01`')
 		query_job = client.query(QUERY)
 		rows = query_job.result()
 		print(rows)
@@ -212,7 +212,7 @@ def run(variFecha):
 
 	project_id = '999847639598'
 	dataset_id = 'staging_zone'
-	table_id = 'shopstar_vtex_client'
+	table_id = 'shopstar_vtex_client_01'
 	
 	client  = bigquery.Client(project = project_id)
 	dataset  = client.dataset(dataset_id)
@@ -225,13 +225,13 @@ def run(variFecha):
 	job = client.load_table_from_json(json_object, table, job_config = job_config)
 	print(job.result())
 	delete_duplicate()
-
+#2019-03-04
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-start_date = date(2019, 1, 1)
+start_date = date(2019, 3, 4)
 end_date = date(2021, 12, 16)
 for single_date in daterange(start_date, end_date):
     variFecha = single_date.strftime("%Y-%m-%d")
