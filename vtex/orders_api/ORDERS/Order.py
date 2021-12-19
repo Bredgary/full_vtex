ec830b1bf162c48e797c563a9cc6f32a891eba93
bredg <bred_valenzuela@gmail.com>
2021-12-19 20:02:18 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/Order.py
+++ vtex/orders_api/ORDERS/Order.py
@@ -1216,7 +1216,7 @@ def get_order(id,reg):
             'DIM_ITEMS_uniqueId': str(init.items_uniqueId),
             'DIM_ITEMS_items_id': init.items_id,
             'DIM_ITEMS_productId': init.items_productId,
-            'DIM_ITEMS_ean': str(init.items_ean),
+            'DIM_ITEMS_ean': int(init.items_ean),
             'DIM_ITEMS_lockId': str(init.items_lockId),
             'DIM_ITEMS_quantity': init.item_quantity,
             'DIM_ITEMS_seller': str(init.item_seller),
