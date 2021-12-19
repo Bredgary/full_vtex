119cdf455fa6698ec30be6e9b751835bbe3af09c
bredg <bred_valenzuela@gmail.com>
2021-12-19 11:40:12 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/marketplace.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/marketplace.py
@@ -73,7 +73,7 @@ def run():
     
     project_id = '999847639598'
     dataset_id = 'test'
-    table_id = 'shopstar_vtex_order'
+    table_id = 'shopstar_order_marketplace'
     
     client  = bigquery.Client(project = project_id)
     dataset  = client.dataset(dataset_id)
@@ -88,7 +88,7 @@ def run():
 def get_params():
     print("Cargando consulta")
     client = bigquery.Client()
-    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.test.shopstar_vtex_order`))')
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.test.shopstar_order_marketplace`))')
     query_job = client.query(QUERY)  
     rows = query_job.result()
     registro = 0
@@ -107,5 +107,4 @@ def get_params():
             run()
     run()
         
-    
 get_params()
