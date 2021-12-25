f447c5fa289efd9e340b67b5f3ac2d6238cfe71c
bredg <bred_valenzuela@gmail.com>
2021-12-25 16:58:05 -0300

    MOD: prueba

--- vtex/entity/CL/availableAddresses.py
+++ vtex/entity/CL/availableAddresses.py
@@ -12,38 +12,26 @@ class init:
   df = pd.DataFrame()
   
   headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-    
-def get_order(id,email,reg):
-  try:
-    print(email)
-    url = "https://mercury.vtexcommercestable.com.br/api/checkout/pub/profiles"
-    querystring = {"email":""+str(email)+""}
-    response = requests.request("GET", url, headers=init.headers, params=querystring)
-    Fjson = json.loads(response.text)
+
+def format_schema(schema):
+    formatted_schema = []
+    for row in schema:
+        formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
+    return formatted_schema
+
+
+def get_order(email,reg):
     try:
-      userProfileId = Fjson["userProfileId"]
-      profileProvider = Fjson["profileProvider"]
-      isComplete = Fjson["isComplete"]
-    except:
-      userProfileId = "Vacio"
-    try:
-      availableAccounts = Fjson["availableAccounts"]
-      for x in availableAccounts:
-        init.accountId = x["accountId"]
-        init.paymentSystem = x["paymentSystem"]
-        init.paymentSystemName = x["paymentSystemName"]
-        init.cardNumber = x["cardNumber"]
-        init.bin = x["bin"]
-        try:
-          availableAddresses = x["availableAddresses"]
-          init.availableAddresses_0 = availableAddresses[0]
-          init.availableAddresses_1 = availableAddresses[1]
-        except:
-          print("Ramificacion no existe")
-        init.expirationDate = x["expirationDate"]
-        init.isExpired = x["isExpired"]
-        init.availableAddresses = Fjson["availableAddresses"]
-        for x in init.availableAddresses:
+        url = "https://mercury.vtexcommercestable.com.br/api/checkout/pub/profiles"
+        querystring = {"email":""+str(email)+""}
+        response = requests.request("GET", url, headers=init.headers, params=querystring)
+        Fjson = json.loads(response.text)
+        
+        userProfileId = Fjson["userProfileId"]
+        profileProvider = Fjson["profileProvider"]
+        isComplete = Fjson["isComplete"]
+        availableAddresses = Fjson["availableAddresses"]
+        for x in availableAddresses:
             addressType = x["addressType"]
             receiverName = x["receiverName"]
             addressId = x["addressId"]
@@ -57,124 +45,190 @@ def get_order(id,email,reg):
             neighborhood = x["neighborhood"]
             complement = x["complement"]
             reference = x["reference"]
-            geoCoordinates = x["geoCoordinates"]
-            lon = geoCoordinates[0]
-            lat = geoCoordinates[1]
+            df1 = pd.DataFrame({
+                'email': email,
+                'userProfileId': userProfileId,
+                'profileProvider': profileProvider,
+                'isComplete': isComplete,
+                'addressType': addressType,
+                'receiverName': receiverName,
+                'addressId': addressId,
+                'isDisposable': isDisposable,
+                'postalCode': postalCode,
+                'city': city,
+                'state': state,
+                'country': country,
+                'street': street,
+                'number': number,
+                'neighborhood': neighborhood,
+                'complement': complement,
+                'reference': reference}, index=[0])
+            init.df = init.df.append(df1)
+            print("Registro: "+str(reg))
     except:
-      print("availableAddresses Vacio")
-    try:
-      userProfile = Fjson["userProfile"]
-      init.email = userProfile["email"]
-      init.firstName = userProfile["firstName"]
-      init.lastName = userProfile["lastName"]
-      init.document = userProfile["document"]
-      init.documentType = userProfile["documentType"]
-      init.phone = userProfile["phone"]
-      init.corporateName = userProfile["corporateName"]
-      init.tradeName = userProfile["tradeName"]
-      init.corporateDocument = userProfile["corporateDocument"]
-      init.stateInscription = userProfile["stateInscription"]
-      init.corporatePhone = userProfile["corporatePhone"]
-      init.isCorporate = userProfile["isCorporate"]
-      init.profileCompleteOnLoading = userProfile["profileCompleteOnLoading"]
-      init.profileErrorOnLoading = userProfile["profileErrorOnLoading"]
-    except:
-      print("client profile")
-    df1 = pd.DataFrame({
-      'customer_id': str(id),
-      'userProfileId': str(init.userProfileId),
-      'profileProvider': str(init.profileProvider),
-      'addressType': str(init.addressType),
-      'receiverName': str(init.receiverName),
-      'addressId': str(init.addressId),
-      'isDisposable': str(init.isDisposable),
-      'postalCode': str(init.postalCode),
-      'city': str(init.city),
-      'state': str(init.state),
-      'country': str(init.country),
-      'street': str(init.street),
-      'number': str(init.number),
-      'neighborhood': str(init.neighborhood),
-      'complement': str(init.complement),
-      'reference': str(init.reference),
-      'lon': str(init.lon),
-      'lat': str(init.lat),
-      'userProfile': str(init.userProfile),
-      'email': str(init.email),
-      'firstName': str(init.firstName),
-      'lastName': str(init.lastName),
-      'document': str(init.document),
-      'documentType': str(init.documentType),
-      'phone': str(init.phone),
-      'corporateName': str(init.corporateName),
-      'tradeName': str(init.tradeName),
-      'corporateDocument': str(init.corporateDocument),
-      'stateInscription': str(init.stateInscription),
-      'corporatePhone': str(init.corporatePhone),
-      'isCorporate': str(init.isCorporate),
-      'profileCompleteOnLoading': str(init.profileCompleteOnLoading),
-      'profileErrorOnLoading': str(init.profileErrorOnLoading),
-      'accountId': str(init.accountId),
-      'paymentSystem': str(init.paymentSystem),
-      'paymentSystemName': str(init.paymentSystemName),
-      'cardNumber': str(init.cardNumber),
-      'bin': str(init.bin),
-      'availableAddresses_0': str(init.availableAddresses_0),
-      'availableAddresses_1': str(init.availableAddresses_1),
-      'expirationDate': str(init.expirationDate),
-      'isExpired': str(init.isExpired),
-      'isComplete': str(init.isComplete)}, index=[0])
-    init.df = init.df.append(df1)
-    print("Registro: "+str(reg))
-  except:
-    print("No data profile "+str(reg))
+        print("No data profile "+str(reg))
         
         
 def get_params():
   print("Cargando consulta")
   client = bigquery.Client()
- 
+  QUERY = ('SELECT DISTINCT email FROM `shopstar-datalake.staging_zone.shopstar_vtex_client`')
   query_job = client.query(QUERY)
   rows = query_job.result()
   registro = 0
   for row in rows:
     registro += 1
-    get_order(row.id,row.email,registro)
+    get_order('tg.arquitectos@gmail.com',registro)
+    if registro == 1:
+        run()
+    if registro == 100:
+        run()
+    if registro == 200:
+        run()
+    if registro == 300:
+        run()
+    if registro == 400:
+        run()
+    if registro == 500:
+        run()
+    if registro == 600:
+        run()
+    if registro == 700:
+        run()
+    if registro == 800:
+        run()
+    if registro == 900:
+        run()
+    if registro == 1000:
+        run()
+    if registro == 2000:
+        run()
+    if registro == 3000:
+        run()
+    if registro == 4000:
+        run()
+    if registro == 5000:
+        run()
+    if registro == 6000:
+        run()
+    if registro == 7000:
+        run()
+    if registro == 8000:
+        run()
+    if registro == 9000:
+        run()
+    if registro == 10000:
+        run()
+    if registro == 20000:
+        run()
+    if registro == 30000:
+        run()
+    if registro == 40000:
+        run()
+    if registro == 50000:
+        run()
+    if registro == 60000:
+        run()
+    if registro == 70000:
+        run()
+    if registro == 80000:
+        run()
+    if registro == 90000:
+        run()
+    if registro == 100000:
+        run()
   run()
-        
-def delete_duplicate():
-  try:
-    print("Eliminando duplicados")
-    client = bigquery.Client()
-    QUERY = ('CREATE OR REPLACE TABLE `shopstar-datalake.test.shopstar_vtex_client_profile`')
-    query_job = client.query(QUERY)
-    rows = query_job.result()
-    print(rows)
-  except:
-    print("Consulta SQL no ejecutada")
 
 
 def run():
- # try:
     df = init.df
     df.reset_index(drop=True, inplace=True)
     json_data = df.to_json(orient = 'records')
     json_object = json.loads(json_data)
     
+    table_schema = [
+    {
+        "name": "email",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "userProfileId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "profileProvider",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "addressType",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "receiverName",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "addressId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "isDisposable",
+        "type": "BOOLEAN",
+        "mode": "NULLABLE"
+    },{
+        "name": "postalCode",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "city",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "state",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "country",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "street",
+        "type": "TIMESTAMP",
+        "mode": "NULLABLE"
+    },{
+        "name": "number",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "neighborhood",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "complement",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "reference",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "isComplete",
+        "type": "BOOLEAN",
+        "mode": "NULLABLE"
+    }]
+    
     project_id = '999847639598'
     dataset_id = 'test'
-    table_id = 'shopstar_vtex_client_profile'
+    table_id = 'shopstar_vtex_client_availableAddresses'
     
     client  = bigquery.Client(project = project_id)
     dataset  = client.dataset(dataset_id)
     table = dataset.table(table_id)
     job_config = bigquery.LoadJobConfig()
-    job_config.autodetect = True
+    #job_config.autodetect = True
+    job_config.schema = format_schema(table_schema)
     job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
     job = client.load_table_from_json(json_object, table, job_config = job_config)
     print(job.result())
-    delete_duplicate()
- # except:
-  #  print("vacio")
   
 get_params()
