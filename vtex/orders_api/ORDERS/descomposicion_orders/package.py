d6e54ae4b81a18d855267e3f9c6c91cdfe5d55f6
bredg <bred_valenzuela@gmail.com>
2021-12-25 12:46:25 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/package.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/package.py
@@ -9,25 +9,6 @@ from os.path import join
 
 class init:
     df = pd.DataFrame()
-    
-    '''
-    packageAttachment
-    '''
-    
-    courier = None
-    invoiceNumber = None
-    invoiceValue = None
-    invoiceUrl = None
-    issuanceDate = None
-    trackingNumber = None
-    invoiceKey = None
-    trackingUrl = None
-    embeddedInvoice = None
-    type = None
-    cfop = None
-    volumes = None
-    EnableInferItems = None
-    
     headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 
 
@@ -36,56 +17,67 @@ def get_order(id,reg):
         url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
         response = requests.request("GET", url, headers=init.headers)
         Fjson = json.loads(response.text)
-        
-        '''
-        INIT DIMENSION  packageAttachment
-        '''    
-        try:
-            packageAttachment = Fjson["packageAttachment"]
-            packages = packageAttachment["packages"]
-        except:
-            print("packageAttachment. No tiene datos")    
-        
-        
-        try:
-            for x in packages:
-                init.courier = x["courier"]
-                init.invoiceNumber = x["invoiceNumber"]
-                init.invoiceValue = x["invoiceValue"]
-                init.invoiceUrl = x["invoiceUrl"]
-                init.issuanceDate = x["issuanceDate"]
-                init.trackingNumber = x["trackingNumber"]
-                init.invoiceKey = x["invoiceKey"]
-                init.trackingUrl = x["trackingUrl"]
-                init.embeddedInvoice = x["embeddedInvoice"]
-                init.type = x["type"]
-                init.cfop = x["cfop"]
-                init.volumes = x["volumes"]
-                init.EnableInferItems = x["EnableInferItems"]
-        except:
-            print("vacio")
-    
-        
-        df1 = pd.DataFrame({
-            'orderId': id,
-            'courier': init.courier,
-            'invoiceNumber': init.invoiceNumber,
-            'invoiceValue': init.invoiceValue,
-            'invoiceUrl': init.invoiceUrl,
-            'issuanceDate': init.issuanceDate,
-            'trackingNumber': init.trackingNumber,
-            'invoiceKey': init.invoiceKey,
-            'trackingUrl': init.trackingUrl,
-            'embeddedInvoice': init.embeddedInvoice,
-            'type': init.type,
-            'cfop': init.cfop,
-            'volumes': init.volumes,
-            'EnableInferItems': init.EnableInferItems}, index=[0])
-        init.df = init.df.append(df1)
-        print("Registro: "+str(reg))
+        packageAttachment = Fjson["packageAttachment"]
+        packages = packageAttachment["packages"]
+        for x in packages:
+            items = x["items"]
+            courier = x["courier"]
+            invoiceNumber = x["invoiceNumber"]
+            invoiceValue = x["invoiceValue"]
+            invoiceUrl = x["invoiceUrl"]
+            issuanceDate = x["issuanceDate"]
+            trackingNumber = x["trackingNumber"]
+            invoiceKey = x["invoiceKey"]
+            trackingUrl = x["trackingUrl"]
+            embeddedInvoice = x["embeddedInvoice"]
+            package_type = x["type"]
+            cfop = x["cfop"]
+            volumes = x["volumes"]
+            EnableInferItems = x["EnableInferItems"]
+            try:
+                courierStatus = x["courierStatus"]
+                for j in courierStatus:
+                    status = j[0]
+                    finished = j[1]
+                    deliveredDate = j[2]
+            except:
+                courierStatus =""
+                status =""
+                finished =""
+                deliveredDate =""
+            for y in items:
+                itemIndex = y["itemIndex"]
+                quantity = y["quantity"]
+                price = y["price"]
+                description = y["description"]
+                unitMultiplier = y["unitMultiplier"]
+                df1 = pd.DataFrame({
+                    'orderId': id,
+                    'courier': courier,
+                    'invoiceNumber': invoiceNumber,
+                    'invoiceValue': invoiceValue,
+                    'invoiceUrl': invoiceUrl,
+                    'issuanceDate': issuanceDate,
+                    'trackingNumber': trackingNumber,
+                    'invoiceKey': invoiceKey,
+                    'trackingUrl': trackingUrl,
+                    'embeddedInvoice': embeddedInvoice,
+                    'package_type': package_type,
+                    "cfop":cfop,
+                    "volumes":volumes,
+                    "EnableInferItems":EnableInferItems,
+                    'status': status,
+                    'finished': finished,
+                    'deliveredDate': deliveredDate,
+                    'itemIndex': itemIndex,
+                    'quantity': quantity,
+                    'price': price,
+                    'description': description,
+                    'unitMultiplier': init.unitMultiplier}, index=[0])
+                init.df = init.df.append(df1)
+                print("Registro: "+str(reg))
     except:
-        print("vacio")
-              
+        print("No packages "+str(reg))    
 
 def get_params():
     print("Cargando consulta")
@@ -96,7 +88,10 @@ def get_params():
     registro = 0
     for row in rows:
         registro += 1
-        get_order(row.orderId,registro)
+        get_order("1040711467154-01",registro)
+        if registro == 1:
+            run()
+    run()
         
 def delete_duplicate():
     try:
@@ -111,11 +106,101 @@ def delete_duplicate():
 
 
 def run():
-    get_params()
     df = init.df
     df.reset_index(drop=True, inplace=True)
     json_data = df.to_json(orient = 'records')
     json_object = json.loads(json_data)
+    
+    table_schema = [
+        {
+            "name": "orderId",
+            "type": "STRING",
+            "mode": "NULLABLE"
+    },{
+        "name": "courier",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "invoiceNumber",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "invoiceValue",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "invoiceUrl",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "issuanceDate",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },{
+        "name": "trackingNumber",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "invoiceKey",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "trackingUrl",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "embeddedInvoice",
+        "type": "FLOAT",
+        "mode": "NULLABLE"
+    },{
+        "name": "type",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "status",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "finished",
+        "type": "BOOLEAN",
+        "mode": "NULLABLE"
+    },{
+        "name": "deliveredDate",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "cfop",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "volumes",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "EnableInferItems",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "itemIndex",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "quantity",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "price",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "description",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "unitMultiplier",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    }]
     
     project_id = '999847639598'
     dataset_id = 'test'
@@ -124,10 +209,10 @@ def run():
     client  = bigquery.Client(project = project_id)
     dataset  = client.dataset(dataset_id)
     table = dataset.table(table_id)
+    
     job_config = bigquery.LoadJobConfig()
+    job_config.schema = format_schema(table_schema)
     job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
     job = client.load_table_from_json(json_object, table, job_config = job_config)
-    print(job.result())
-    delete_duplicate()
     
-run()
+get_params()
