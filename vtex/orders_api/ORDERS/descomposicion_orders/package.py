6c621e67ac100df79768a4d894449642c71e8b2d
bredg <bred_valenzuela@gmail.com>
2021-12-25 12:48:33 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/package.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/package.py
@@ -82,7 +82,7 @@ def get_order(id,reg):
 def get_params():
     print("Cargando consulta")
     client = bigquery.Client()
-    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.test.shopstar_order_package`))')
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`')
     query_job = client.query(QUERY)
     rows = query_job.result()
     registro = 0
