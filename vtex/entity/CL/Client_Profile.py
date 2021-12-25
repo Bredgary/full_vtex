6734b00dae55f1d2571f094216d169ba053d3b74
bredg <bred_valenzuela@gmail.com>
2021-12-24 21:41:46 -0300

    MOD: prueba

--- vtex/entity/CL/Client_Profile.py
+++ vtex/entity/CL/Client_Profile.py
@@ -49,7 +49,7 @@ def get_order(email,reg):
  
     
     df1 = pd.DataFrame({
-      'userProfileId': userProfileId,
+      #'userProfileId': userProfileId,
       #'profileProvider': profileProvider,
       'email': email,
       'firstName': firstName,
