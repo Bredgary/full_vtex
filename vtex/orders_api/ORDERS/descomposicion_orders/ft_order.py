aef85f42624ea9c98af9b7e14964f8e24b349089
bredg <bred_valenzuela@gmail.com>
2021-12-26 00:22:07 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
 import pandas as pd
 import numpy as np
 from google.cloud import bigquery
@@ -10,178 +12,178 @@ from os.path import join
 class init:
     productList = []
     df = pd.DataFrame()
-    orderId = None
-    sequence = None
-    marketplaceOrderId = None
-    marketplaceServicesEndpoint = None
-    sellerOrderId = None
-    origin = None
-    affiliateId = None
-    salesChannel = None
-    merchantName = None
-    status = None
-    statusDescription = None
-    value = None
-    creationDate = None
-    lastChange = None
-    orderGroup = None
-    giftRegistryData = None
-    marketingData = None
-    callCenterOperatorData = None
-    followUpEmail = None
-    lastMessage = None
-    hostname = None
-    changesAttachment = None
-    openTextField = None
-    roundingError = None
-    orderFormId = None
-    commercialConditionData = None
-    isCompleted = None
-    customData = None
-    allowCancellation = None
-    allowEdition = None
-    isCheckedIn = None
-    marketplace = None
-    authorizedDate = None
-    invoicedDate = None
-    cancelReason = None
-    subscriptionData = None
-    taxData = None
-    checkedInPickupPointId = None
-    cancellationData = None
-    seller_id  = None
-    seller_name = None
-    seller_logo = None
-    
     headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
-    
 
-        
-def get_order(id,reg):
+def format_schema(schema):
+    formatted_schema = []
+    for row in schema:
+        formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
+    return formatted_schema
+
+def decrypt_email(email):
     try:
-        reg +=1
+        url = "https://conversationtracker.vtex.com.br/api/pvt/emailMapping?an=mercury&alias="+email+""
+        response = requests.request("GET", url, headers=init.headers)
+        formatoJ = json.loads(response.text)
+        return formatoJ["email"]
+    except:
+        print(str(email))
+
+def get_order(id):
+    try:
         url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
         response = requests.request("GET", url, headers=init.headers)
         Fjson = json.loads(response.text)
-        #try:
-        init.orderId = Fjson["orderId"]
-        init.sequence = Fjson["sequence"]
-        init.marketplaceOrderId = Fjson["marketplaceOrderId"]
-        init.marketplaceServicesEndpoint = Fjson["marketplaceServicesEndpoint"]
-        init.sellerOrderId = Fjson["sellerOrderId"]
-        init.origin = Fjson["origin"]
-        init.affiliateId = Fjson["affiliateId"]
-        init.salesChannel = Fjson["salesChannel"]
-        init.merchantName = Fjson["merchantName"]
-        init.status = Fjson["status"]
-        init.statusDescription = Fjson["statusDescription"]
-        init.value = Fjson["value"]
-        init.creationDate = Fjson["creationDate"]
-        init.lastChange = Fjson["lastChange"]
-        init.orderGroup = Fjson["orderGroup"]
-        init.giftRegistryData = Fjson["giftRegistryData"]
-        init.marketingData = Fjson["marketingData"]
-        init.callCenterOperatorData = Fjson["callCenterOperatorData"]
-        init.followUpEmail = Fjson["followUpEmail"]
-        init.lastMessage = Fjson["lastMessage"]
-        init.hostname = Fjson["hostname"]
-        init.changesAttachment = Fjson["changesAttachment"]
-        init.openTextField = Fjson["openTextField"]
-        init.roundingError = Fjson["roundingError"]
-        init.orderFormId = Fjson["orderFormId"]
-        init.commercialConditionData = Fjson["commercialConditionData"]
-        init.isCompleted = Fjson["isCompleted"]
-        init.customData = Fjson["customData"]
-        init.allowCancellation = Fjson["allowCancellation"]
-        init.allowEdition = Fjson["allowEdition"]
-        init.isCheckedIn = Fjson["isCheckedIn"]
-        init.marketplace = Fjson["marketplace"]
-        init.authorizedDate = Fjson["authorizedDate"]
-        init.invoicedDate = Fjson["invoicedDate"]
-        init.cancelReason = str(Fjson["cancelReason"])
-        init.taxData = Fjson["taxData"]
-        init.checkedInPickupPointId = Fjson["checkedInPickupPointId"]
-        init.cancellationData = Fjson["cancellationData"]
+        
+        orderId = Fjson["orderId"]
+        client_email = clientProfileData["email"]
+        userProfileId = clientProfileData["userProfileId"]
+        sequence = Fjson["sequence"]
+        marketplaceOrderId = Fjson["marketplaceOrderId"]
+        marketplaceServicesEndpoint = Fjson["marketplaceServicesEndpoint"]
+        sellerOrderId = Fjson["sellerOrderId"]
+        origin = Fjson["origin"]
+        affiliateId = Fjson["affiliateId"]
+        checkedInPickupPointId = Fjson["checkedInPickupPointId"]
+        salesChannel = Fjson["salesChannel"]
+        merchantName = Fjson["merchantName"]
+        status = Fjson["status"]
+        statusDescription = Fjson["statusDescription"]
+        value = Fjson["value"]
+        creationDate = Fjson["creationDate"]
+        lastChange = Fjson["lastChange"]
+        orderGroup  = Fjson["orderGroup"]
+        callCenterOperatorData = Fjson["callCenterOperatorData"]
+        followUpEmail = Fjson["followUpEmail"]
+        lastMessage = Fjson["lastMessage"]
+        hostname = Fjson["hostname"]
+        changesAttachment = Fjson["changesAttachment"]
+        openTextField = Fjson["openTextField"]
+        roundingError = Fjson["roundingError"]
+        orderFormId = Fjson["orderFormId"]
+        commercialConditionData = Fjson["commercialConditionData"]
+        isCompleted = Fjson["isCompleted"]
+        customData = Fjson["customData"]
+        allowCancellation = Fjson["allowCancellation"]
+        allowEdition = Fjson["allowEdition"]
+        isCheckedIn = Fjson["isCheckedIn"]
+        marketplace = Fjson["marketplace"]
+        authorizedDate = Fjson["authorizedDate"]
+        invoicedDate = Fjson["invoicedDate"]
+        cancelReason = Fjson["cancelReason"]
+        subscriptionData = Fjson["subscriptionData"]
+        taxData = Fjson["taxData"]
+        giftRegistryData = Fjson["giftRegistryData"]
+        
+        clientProfileData = Fjson["clientProfileData"]
+        client_email = clientProfileData["email"]
+        client_email = decrypt_email(str(client_email))
+        
+        try:
+            marketplaceOrderId = Fjson["marketplaceOrderId"]
+            baseURL = marketplace["baseURL"]
+            isCertified = marketplace["isCertified"]
+            name = marketplace["name"]
+        except:
+            baseURL = ''
+            isCertified = ''
+            name = ''
         try:
             sellers_ = Fjson["sellers"]
-        except:
-            print("sellers_ No tiene datos")
-        try:
             sellers = sellers_[0]
-            init.seller_id = sellers["id"]
-            init.seller_name = sellers["name"]
-            init.seller_logo = sellers["logo"]
+            seller_id = sellers["id"]
+            seller_name = sellers["name"]
+            seller_logo = sellers["logo"]
         except:
-            print("sellers No tiene datos")
-        
-        
+            eller_id = ''
+            seller_name = ''
+            seller_logo = ''
+        try:
+            cancellationData = Fjson["cancellationData"]
+            CancellationDate = cancellationData["CancellationDate"]
+            RequestedByUser = cancellationData["RequestedByUser"]
+            RequestedBySystem = cancellationData["RequestedBySystem"]
+            RequestedBySellerNotification = cancellationData["RequestedBySellerNotification"]
+            RequestedByPaymentNotification = cancellationData["RequestedByPaymentNotification"]
+            Reason = cancellationData["Reason"]
+        except:
+            RequestedByUser = ""
+            RequestedBySystem = ""
+            RequestedBySellerNotification = ""
+            RequestedByPaymentNotification = ""
+            Reason = ""
+            CancellationDate = ""
+            
+        try:
+            dim_invoiceData = Fjson["invoiceData"]
+            invoice_address = dim_invoiceData["address"]
+            userPaymentInfo = dim_invoiceData["userPaymentInfo"]
+        except:
+            invoice_address = ''
+            userPaymentInfo = ''
+            
         df1 = pd.DataFrame({
-            'orderId': str(init.orderId),
-            'sequence': str(init.sequence),
-            'marketplaceOrderId': str(init.marketplaceOrderId),
-            'marketplaceServicesEndpoint': str(init.marketplaceServicesEndpoint),
-            'sellerOrderId': str(init.sellerOrderId),
-            'origin': str(init.origin),
-            'affiliateId': str(init.affiliateId),
-            'salesChannel': str(init.salesChannel),
-            'merchantName': str(init.merchantName),
-            'status': str(init.status),
-            'statusDescription': str(init.statusDescription),
-            'value': str(init.value),
-            'creationDate': str(init.creationDate),
-            'lastChange': str(init.lastChange),
-            'orderGroup': str(init.orderGroup),
-            'giftRegistryData': str(init.giftRegistryData),
-            'marketingData': str(init.marketingData),
-            'callCenterOperatorData': str(init.callCenterOperatorData),
-            'lastMessage': str(init.lastMessage),
-            'hostname': str(init.hostname),
-            'changesAttachment': str(init.changesAttachment),
-            'openTextField': str(init.openTextField),
-            'roundingError': str(init.roundingError),
-            'orderFormId': str(init.orderFormId),
-            'commercialConditionData': str(init.commercialConditionData),
-            'isCompleted': str(init.isCompleted),
-            'customData': str(init.customData),
-            'allowCancellation': str(init.allowCancellation),
-            'allowEdition': str(init.allowEdition),
-            'isCheckedIn': str(init.isCheckedIn),
-            'marketplace': str(init.marketplace),
-            'authorizedDate': str(init.authorizedDate),
-            'invoicedDate': str(init.invoicedDate),
-            'cancelReason': str(init.cancelReason),
-            'taxData': str(init.taxData),
-            'checkedInPickupPointId': str(init.checkedInPickupPointId),
-            'cancellationData': str(init.cancellationData),
-            'seller_id': str(init.seller_id),
-            'seller_name': str(init.seller_name),
-            'seller_logo': str(init.seller_logo),
-            'followUpEmail': str(init.followUpEmail)}, index=[0])
+            'orderId': id,
+            'client_email': client_email,
+            'userProfileId': userProfileId,
+            'sequence': sequence,
+            'marketplaceOrderId': marketplaceOrderId,
+            'marketplaceServicesEndpoint': marketplaceServicesEndpoint,
+            'sellerOrderId':sellerOrderId,
+            'origin': origin,
+            'affiliateId': affiliateId,
+            'checkedInPickupPointId': checkedInPickupPointId,
+            'salesChannel': salesChannel,
+            'merchantName': merchantName,
+            'status': status,
+            'statusDescription': statusDescription,
+            'value': value,
+            'creationDate': creationDate,
+            'lastChange': lastChange,
+            'orderGroup': orderGroup,
+            'callCenterOperatorData': callCenterOperatorData,
+            'followUpEmail': followUpEmail,
+            'lastMessage': lastMessage,
+            'hostname': hostname,
+            'changesAttachment': changesAttachment,
+            'openTextField': openTextField,
+            'roundingError': roundingError,
+            'orderFormId': orderFormId,
+            'commercialConditionData': commercialConditionData,
+            'isCompleted': isCompleted,
+            'customData': customData,
+            'allowCancellation': allowCancellation,
+            'allowEdition': allowEdition,
+            'isCheckedIn': isCheckedIn,
+            'marketplace': marketplace,
+            'authorizedDate': authorizedDate,
+            'invoicedDate': invoicedDate,
+            'cancelReason': cancelReason,
+            'subscriptionData': subscriptionData,
+            'taxData': taxData,
+            'baseURL': baseURL,
+            'isCertified': isCertified,
+            'name': name,
+            'seller_id': seller_id,
+            'seller_name': seller_name,
+            'seller_logo': seller_logo,
+            'CancellationDate': CancellationDate,
+            'RequestedByUser': RequestedByUser,
+            'RequestedBySystem': RequestedBySystem,
+            'RequestedBySellerNotification': RequestedBySellerNotification,
+            'RequestedByPaymentNotification': RequestedByPaymentNotification,
+            'Reason': Reason,
+            'giftRegistryData': giftRegistryData}, index=[0])
         init.df = init.df.append(df1)
-        print("Registro: "+str(reg))
     except:
         print("vacio")
-        
-        
 
-def get_params():
-    print("Cargando consulta")
-    client = bigquery.Client()
-    QUERY = (
-        'SELECT orderId FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`')
-    query_job = client.query(QUERY)  
-    rows = query_job.result()
-    registro = 0
-    for row in rows:
-        get_order(row.orderId,registro)
-        registro += 1
-        
 def delete_duplicate():
     try:
         print("Eliminando duplicados")
         client = bigquery.Client()
         QUERY = (
-            'CREATE OR REPLACE TABLE `shopstar-datalake.test.shopstar_order_ft_order` AS SELECT DISTINCT * FROM `shopstar-datalake.test.shopstar_order_ft_order`')
+            'CREATE OR REPLACE TABLE `shopstar-datalake.test.shopstar_vtex_ft_orders` AS SELECT DISTINCT * FROM `shopstar-datalake.test.shopstar_vtex_ft_orders`')
         query_job = client.query(QUERY)
         rows = query_job.result()
         print(rows)
@@ -190,25 +192,109 @@ def delete_duplicate():
 
 
 def run():
-    get_params()
     df = init.df
     df.reset_index(drop=True, inplace=True)
     json_data = df.to_json(orient = 'records')
     json_object = json.loads(json_data)
     
+    #table_schema = 
+
+    
     project_id = '999847639598'
     dataset_id = 'test'
-    table_id = 'shopstar_order_ft_order'
+    table_id = 'shopstar_vtex_ft_orders'
     
-    client  = bigquery.Client(project = project_id)
-    dataset  = client.dataset(dataset_id)
-    table = dataset.table(table_id)
-    job_config = bigquery.LoadJobConfig()
-    job_config.write_disposition = "WRITE_TRUNCATE"
-    job_config.autodetect = True
-    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job = client.load_table_from_json(json_object, table, job_config = job_config)
-    print(job.result())
-    delete_duplicate()
+    try:
+        client  = bigquery.Client(project = project_id)
+        dataset  = client.dataset(dataset_id)
+        table = dataset.table(table_id)
+        job_config = bigquery.LoadJobConfig()
+        job_config.write_disposition = "WRITE_TRUNCATE"
+        job_config.autodetect = True
+        job_config.schema = format_schema(table_schema)
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        job = client.load_table_from_json(json_object, table, job_config = job_config)
+        print(job.result())
+        delete_duplicate()
+    except:
+        client  = bigquery.Client(project = project_id)
+        dataset  = client.dataset(dataset_id)
+        table = dataset.table(table_id)
+        job_config = bigquery.LoadJobConfig()
+        job_config.write_disposition = "WRITE_TRUNCATE"
+        job_config.autodetect = True
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        job = client.load_table_from_json(json_object, table, job_config = job_config)
+        print(job.result())
+        delete_duplicate()   
+
+
+def get_params():
+    print("Cargando consulta")
+    client = bigquery.Client()
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.test.shopstar_vtex_ft_order`))')
+    query_job = client.query(QUERY)  
+    rows = query_job.result()
+    registro = 0
+    for row in rows:
+        registro += 1
+        get_order("1040711467154-01")
+        print("Registro: "+str(registro))
+        if registro == 1:
+            run()
+        if registro == 100:
+            run()
+        if registro == 200:
+            run()
+        if registro == 300:
+            run()
+        if registro == 400:
+            run()
+        if registro == 500:
+            run()
+        if registro == 600:
+            run()
+        if registro == 700:
+            run()
+        if registro == 800:
+            run()
+        if registro == 900:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 2000:
+            run()
+        if registro == 3000:
+            run()
+        if registro == 4000:
+            run()
+        if registro == 5000:
+            run()
+        if registro == 6000:
+            run()
+        if registro == 7000:
+            run()
+        if registro == 8000:
+            run()
+        if registro == 9000:
+            run()
+        if registro == 10000:
+            run()
+        if registro == 20000:
+            run()
+        if registro == 30000:
+            run()
+        if registro == 40000:
+            run()
+        if registro == 50000:
+            run()
+        if registro == 60000:
+            run()
+        if registro == 70000:
+            run()
+        if registro == 80000:
+            run()
+    run()
+        
     
-run()
+get_params()
