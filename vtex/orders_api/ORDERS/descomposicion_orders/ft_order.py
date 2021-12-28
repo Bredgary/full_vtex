358df23e96de4c61812513efd6309b6cff3b5260
bredg <bred_valenzuela@gmail.com>
2021-12-28 12:00:22 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
@@ -407,26 +407,26 @@ def run():
   dataset_id = 'test'
   table_id = 'shopstar_vtex_ft_orders'
   
-  try:
-      client  = bigquery.Client(project = project_id)
-      dataset  = client.dataset(dataset_id)
-      table = dataset.table(table_id)
-      job_config = bigquery.LoadJobConfig()
-      job_config.schema = format_schema(table_schema)
-      job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-      job = client.load_table_from_json(json_object, table, job_config = job_config)
-      print(job.result())
-      delete_duplicate()
-  except:
-      client  = bigquery.Client(project = project_id)
-      dataset  = client.dataset(dataset_id)
-      table = dataset.table(table_id)
-      job_config = bigquery.LoadJobConfig()
-      job_config.autodetect = True
-      job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-      job = client.load_table_from_json(json_object, table, job_config = job_config)
-      print(job.result())
-      delete_duplicate()   
+  #try:
+  client  = bigquery.Client(project = project_id)
+  dataset  = client.dataset(dataset_id)
+  table = dataset.table(table_id)
+  job_config = bigquery.LoadJobConfig()
+  job_config.schema = format_schema(table_schema)
+  job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+  job = client.load_table_from_json(json_object, table, job_config = job_config)
+  print(job.result())
+  delete_duplicate()
+  #except:
+  #    client  = bigquery.Client(project = project_id)
+   #   dataset  = client.dataset(dataset_id)
+    #  table = dataset.table(table_id)
+    #  job_config = bigquery.LoadJobConfig()
+    #  job_config.autodetect = True
+    #  job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+    #  job = client.load_table_from_json(json_object, table, job_config = job_config)
+     # print(job.result())
+     # delete_duplicate()   
 
 
 def get_params():
