4b8a2667bb18ed39643c15e3ea6193968c4c975c
bredg <bred_valenzuela@gmail.com>
2021-12-30 11:31:21 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/order_test.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/order_test.py
@@ -8,30 +8,56 @@ from datetime import datetime, timezone
 from os.path import join
 
 class init:
-  productList = []
-  df = pd.DataFrame()
-  headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+    df = pd.DataFrame()
+    headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+
 
 def format_schema(schema):
-  formatted_schema = []
-  for row in schema:
-    formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
+    formatted_schema = []
+    for row in schema:
+        formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
     return formatted_schema
 
-def decrypt_email(email):
-  try:
-    url = "https://conversationtracker.vtex.com.br/api/pvt/emailMapping?an=mercury&alias="+email+""
-    response = requests.request("GET", url, headers=init.headers)
-    formatoJ = json.loads(response.text)
-    return formatoJ["email"]
-  except:
-    print(str(email))
 
 def get_order(id):
-   # try:
+    try:
         url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
         response = requests.request("GET", url, headers=init.headers)
         Fjson = json.loads(response.text)
+        packageAttachment = Fjson["packageAttachment"]
+        packages = packageAttachment["packages"]
+        
+        items = ""
+        courier = ""
+        invoiceNumber = ""
+        invoiceValue = ""
+        invoiceUrl = ""
+        issuanceDate = ""
+        trackingNumber = ""
+        invoiceKey = ""
+        trackingUrl = ""
+        embeddedInvoice = ""
+        package_type = ""
+        cfop  = ""
+        volumes  = ""
+        EnableInferItems = ""
+        
+        for x in packages:
+            items = packages["items"]
+            courier = x["courier"]
+            invoiceNumber = x["invoiceNumber"]
+            invoiceValue = x["invoiceValue"]
+            invoiceUrl = x["invoiceUrl"]
+            issuanceDate = x["issuanceDate"]
+            trackingNumber = x["trackingNumber"]
+            invoiceKey = x["invoiceKey"]
+            trackingUrl = x["trackingUrl"]
+            embeddedInvoice = x["embeddedInvoice"]
+            package_type = x["type"]
+            cfop = x["cfop"]
+            volumes = x["volumes"]
+            EnableInferItems = x["EnableInferItems"]
+        
         clientProfileData = Fjson["clientProfileData"]
         client_email = clientProfileData["email"]
         orderId = Fjson["orderId"]
@@ -74,44 +100,6 @@ def get_order(id):
         taxData = Fjson["taxData"]
         giftRegistryData = Fjson["giftRegistryData"]
         client_email = decrypt_email(str(client_email))
-        Total = Fjson["totals"]
-        
-        try:
-            items = Total[0]
-            total_id_items = items["id"]
-            total_name_items = items["name"]
-            total_value_items = items["value"]
-        except:
-            total_id_items = ""
-            total_name_items = ""
-            total_value_items = 0
-        try:
-            discounts = Total[1]
-            total_id_discounts = discounts["id"]
-            total_name_discounts = discounts["name"]
-            total_value_discounts = discounts["value"]
-        except:
-            total_id_discounts = ""
-            total_name_discounts =""
-            total_value_discounts = 0
-        try:
-            shipping = Total[2]
-            total_id_shipping = shipping["id"]
-            total_name_shipping = shipping["name"]
-            total_value_shipping = shipping["value"]
-        except:
-            total_id_shipping = ""
-            total_name_shipping = ""
-            total_value_shipping = 0
-        try:
-            tax = Total[3]
-            total_id_tax = tax["id"]
-            total_name_tax = tax["name"]
-            total_value_tax = tax["value"]
-        except:
-            total_id_tax = ""
-            total_name_tax = ""
-            total_value_tax = 0
         
         try:
             marketplaceOrderId = str(Fjson["marketplaceOrderId"])
@@ -158,84 +146,97 @@ def get_order(id):
             invoice_address = ''
             userPaymentInfo = ''
             
-        df1 = pd.DataFrame({
-            'orderId': id,
-            'client_email': client_email,
-            'userProfileId': userProfileId,
-            'sequence': sequence,
-            'marketplaceOrderId': str(marketplaceOrderId),
-            'marketplaceServicesEndpoint': marketplaceServicesEndpoint,
-            'sellerOrderId':sellerOrderId,
-            'origin': origin,
-            'affiliateId': affiliateId,
-            'checkedInPickupPointId': checkedInPickupPointId,
-            'salesChannel': salesChannel,
-            'merchantName': merchantName,
-            'status': status,
-            'statusDescription': statusDescription,
-            'value': value,
-            'creationDate': creationDate,
-            'lastChange': lastChange,
-            'orderGroup': orderGroup,
-            'callCenterOperatorData': callCenterOperatorData,
-            'followUpEmail': followUpEmail,
-            'lastMessage': lastMessage,
-            'hostname': hostname,
-            'changesAttachment': changesAttachment,
-            'openTextField': openTextField,
-            'roundingError': roundingError,
-            'orderFormId': orderFormId,
-            'commercialConditionData': commercialConditionData,
-            'isCompleted': isCompleted,
-            'customData': customData,
-            'allowCancellation': allowCancellation,
-            'allowEdition': allowEdition,
-            'isCheckedIn': isCheckedIn,
-            'marketplace': marketplace,
-            'authorizedDate': authorizedDate,
-            'invoicedDate': invoicedDate,
-            'cancelReason': cancelReason,
-            'subscriptionData': subscriptionData,
-            'taxData': taxData,
-            'baseURL': baseURL,
-            'isCertified': isCertified,
-            'name': name,
-            'seller_id': seller_id,
-            'seller_name': seller_name,
-            'seller_logo': seller_logo,
-            'CancellationDate': str(CancellationDate),
-            'RequestedByUser': RequestedByUser,
-            'RequestedBySystem': RequestedBySystem,
-            'RequestedBySellerNotification': RequestedBySellerNotification,
-            'RequestedByPaymentNotification': RequestedByPaymentNotification,
-            'Reason': Reason,
-            'giftRegistryData': giftRegistryData,
-            'totals_id_items': total_id_items,
-            'totals_name_items': total_name_items,
-            'totals_value_items': total_value_items,
-            'totals_id_discounts': total_id_discounts,
-            'totals_name_discounts': total_name_discounts,
-            'totals_value_discounts': total_value_discounts,
-            'totals_id_shipping': total_id_shipping,
-            'totals_name_shipping': total_name_shipping,
-            'totals_value_shipping': total_value_shipping,
-            'totals_id_tax': total_id_tax,
-            'totals_name_tax': total_name_tax,
-            'totals_value_tax': total_value_tax}, index=[0])
-        init.df = init.df.append(df1)
-   # except:
-    #    print("vacio")
+        for y in items:
+            itemIndex = y["itemIndex"]
+            quantity = y["quantity"]
+            price = y["price"]
+            description = y["description"]
+            unitMultiplier = y["unitMultiplier"]
+            df1 = pd.DataFrame({
+                'orderId': id,
+                'package_courier': courier,
+                'package_invoiceNumber': invoiceNumber,
+                'package_invoiceValue': invoiceValue,
+                'package_invoiceUrl': invoiceUrl,
+                'package_issuanceDate': issuanceDate,
+                'package_trackingNumber': trackingNumber,
+                'package_invoiceKey': invoiceKey,
+                'package_trackingUrl': trackingUrl,
+                'package_embeddedInvoice': embeddedInvoice,
+                'package_type': package_type,
+                "package_cfop":cfop,
+                "package_volumes":volumes,
+                "package_EnableInferItems":EnableInferItems,
+                'package_itemIndex': itemIndex,
+                'package_quantity': quantity,
+                'package_price': price,
+                'package_description': description,
+                'package_package_unitMultiplier': unitMultiplier,
+                'client_email': client_email,
+                'userProfileId': userProfileId,
+                'sequence': sequence,
+                'marketplaceOrderId': str(marketplaceOrderId),
+                'marketplaceServicesEndpoint': marketplaceServicesEndpoint,
+                'sellerOrderId':sellerOrderId,
+                'origin': origin,
+                'affiliateId': affiliateId,
+                'checkedInPickupPointId': checkedInPickupPointId,
+                'salesChannel': salesChannel,
+                'merchantName': merchantName,
+                'status': status,
+                'statusDescription': statusDescription,
+                'value': value,
+                'creationDate': creationDate,
+                'lastChange': lastChange,
+                'orderGroup': orderGroup,
+                'callCenterOperatorData': callCenterOperatorData,
+                'followUpEmail': followUpEmail,
+                'lastMessage': lastMessage,
+                'hostname': hostname,
+                'changesAttachment': changesAttachment,
+                'openTextField': openTextField,
+                'roundingError': roundingError,
+                'orderFormId': orderFormId,
+                'commercialConditionData': commercialConditionData,
+                'isCompleted': isCompleted,
+                'customData': customData,
+                'allowCancellation': allowCancellation,
+                'allowEdition': allowEdition,
+                'isCheckedIn': isCheckedIn,
+                'marketplace': marketplace,
+                'authorizedDate': authorizedDate,
+                'invoicedDate': invoicedDate,
+                'cancelReason': cancelReason,
+                'subscriptionData': subscriptionData,
+                'taxData': taxData,
+                'baseURL': baseURL,
+                'isCertified': isCertified,
+                'name': name,
+                'seller_id': seller_id,
+                'seller_name': seller_name,
+                'seller_logo': seller_logo,
+                'CancellationDate': str(CancellationDate),
+                'RequestedByUser': RequestedByUser,
+                'RequestedBySystem': RequestedBySystem,
+                'RequestedBySellerNotification': RequestedBySellerNotification,
+                'RequestedByPaymentNotification': RequestedByPaymentNotification,
+                'Reason': Reason,
+                'giftRegistryData': giftRegistryData}, index=[0])
+            init.df = init.df.append(df1)
+    except:
+        print("No packages")    
 
+        
 def delete_duplicate():
-  try:
-    print("Eliminando duplicados")
-    client = bigquery.Client()
-    QUERY = ('CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_vtex_ft_orders` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_vtex_ft_orders`')
-    query_job = client.query(QUERY)
-    rows = query_job.result()
-    print(rows)
-  except:
-    print("Consulta SQL no ejecutada")
+    try:
+        print("Eliminando duplicados")
+        client = bigquery.Client()
+        QUERY = ('CREATE OR REPLACE TABLE `shopstar-datalake.test.shopstar_order_package` AS SELECT DISTINCT * FROM `shopstar-datalake.test.shopstar_order_package`')
+        query_job = client.query(QUERY)
+        rows = query_job.result()
+        print(rows)
+    except:
+        print("Consulta SQL no ejecutada")
 
 
 def run():
@@ -243,9 +244,84 @@ def run():
     df.reset_index(drop=True, inplace=True)
     json_data = df.to_json(orient = 'records')
     json_object = json.loads(json_data)
-    
     table_schema = [
         {
+            "name": "orderId",
+            "type": "STRING",
+            "mode": "NULLABLE"
+    },{
+        "name": "package_courier",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_invoiceNumber",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_invoiceValue",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_invoiceUrl",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_issuanceDate",
+        "type": "TIMESTAMP",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_trackingNumber",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_invoiceKey",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_trackingUrl",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_embeddedInvoice",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_type",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_cfop",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_volumes",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_EnableInferItems",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_itemIndex",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_quantity",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_price",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_description",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_unitMultiplier",
+        "type": "FLOAT",
+        "mode": "NULLABLE"
+    },{
           "name": "giftRegistryData",
           "type": "STRING",
           "mode": "NULLABLE"
@@ -295,7 +371,7 @@ def run():
         "mode": "NULLABLE"
     },{
         "name": "isCertified",
-        "type": "BOOLEAN",
+        "type": "FLOAT",
         "mode": "NULLABLE"
     },{
         "name": "lastMessage",
@@ -431,7 +507,7 @@ def run():
         "mode": "NULLABLE"
     },{
         "name": "Reason",
-        "type": "STRING",
+        "type": "FLOAT",
         "mode": "NULLABLE"
     },{
         "name": "client_email",
@@ -449,65 +525,22 @@ def run():
         "name": "marketplaceOrderId",
         "type": "STRING",
         "mode": "NULLABLE"
-    },{
-        "name": "totals_id_items",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "totals_name_items",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "total_value_items",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },{
-        "name": "totals_id_discounts",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "totals_name_discounts",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "totals_value_discounts",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },{
-        "name": "totals_id_shipping",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "totals_name_shipping",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "totals_value_shipping",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },{
-        "name": "totals_id_tax",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "totals_name_tax",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "totals_value_tax",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
     }]
     
+     
     project_id = '999847639598'
     dataset_id = 'test'
-    table_id = 'shopstar_ft_orders'
+    table_id = 'shopstar_order_test'
+    
+    client  = bigquery.Client(project = project_id)
+    dataset  = client.dataset(dataset_id)
+    table = dataset.table(table_id)
+    
     try:
         client  = bigquery.Client(project = project_id)
         dataset  = client.dataset(dataset_id)
         table = dataset.table(table_id)
         job_config = bigquery.LoadJobConfig()
-        job_config.write_disposition = "WRITE_TRUNCATE"
         job_config.schema = format_schema(table_schema)
         job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
         job = client.load_table_from_json(json_object, table, job_config = job_config)
@@ -518,64 +551,24 @@ def run():
         dataset  = client.dataset(dataset_id)
         table = dataset.table(table_id)
         job_config = bigquery.LoadJobConfig()
-        job_config.write_disposition = "WRITE_TRUNCATE"
         job_config.autodetect = True
         job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
         job = client.load_table_from_json(json_object, table, job_config = job_config)
         print(job.result())
         delete_duplicate()
-
+    
+    
 def get_params():
     print("Cargando consulta")
     client = bigquery.Client()
-    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.staging_zone.shopstar_vtex_ft_orders`))')
-    query_job = client.query(QUERY)  
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.test.shopstar_order_package`))')
+    query_job = client.query(QUERY)
     rows = query_job.result()
     registro = 0
     for row in rows:
         registro += 1
         get_order(row.orderId)
         print("Registro: "+str(registro))
-        if registro == 5:
-            run()
-        if registro == 10:
-            run()
-        if registro == 100:
-            run()
-        if registro == 200:
-            run()
-        if registro == 500:
-            run()
-        if registro == 10000:
-            run()
-        if registro == 12000:
-            run()
-        if registro == 15000:
-            run()
-        if registro == 20000:
-            run()
-        if registro == 30000:
-            run()
-        if registro == 35000:
-            run()
-        if registro == 40000:
-            run()
-        if registro == 45000:
-            run()
-        if registro == 50000:
-            run()
-        if registro == 60000:
-            run()
-        if registro == 70000:
-            run()
-        if registro == 80000:
-            run()
-        if registro == 85000:
-            run()
-        if registro == 90000:
-            run()
-        if registro == 95000:
-            run()
     run()
-
+    
 get_params()
---
+++ vtex/orders_api/ORDERS/descomposicion_orders/totals.py
@@ -0,0 +1,232 @@
+import pandas as pd
+import numpy as np
+from google.cloud import bigquery
+import os, json
+from datetime import datetime
+import requests
+from datetime import datetime, timezone
+from os.path import join
+
+class init:
+    df = pd.DataFrame()
+    headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+
+
+def format_schema(schema):
+    formatted_schema = []
+    for row in schema:
+        formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
+    return formatted_schema
+
+
+def get_order(id):
+    try:
+        url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
+        response = requests.request("GET", url, headers=init.headers)
+        Fjson = json.loads(response.text)
+        packageAttachment = Fjson["packageAttachment"]
+        packages = packageAttachment["packages"]
+        
+        items = ""
+        courier = ""
+        invoiceNumber = ""
+        invoiceValue = ""
+        invoiceUrl = ""
+        issuanceDate = ""
+        trackingNumber = ""
+        invoiceKey = ""
+        trackingUrl = ""
+        embeddedInvoice = ""
+        package_type = ""
+        cfop  = ""
+        volumes  = ""
+        EnableInferItems = ""
+        
+        for x in packages:
+            items = packages["items"]
+            courier = x["courier"]
+            invoiceNumber = x["invoiceNumber"]
+            invoiceValue = x["invoiceValue"]
+            invoiceUrl = x["invoiceUrl"]
+            issuanceDate = x["issuanceDate"]
+            trackingNumber = x["trackingNumber"]
+            invoiceKey = x["invoiceKey"]
+            trackingUrl = x["trackingUrl"]
+            embeddedInvoice = x["embeddedInvoice"]
+            package_type = x["type"]
+            cfop = x["cfop"]
+            volumes = x["volumes"]
+            EnableInferItems = x["EnableInferItems"]
+            
+        for y in items:
+            itemIndex = y["itemIndex"]
+            quantity = y["quantity"]
+            price = y["price"]
+            description = y["description"]
+            unitMultiplier = y["unitMultiplier"]
+            df1 = pd.DataFrame({
+                'orderId': id,
+                'courier': courier,
+                'invoiceNumber': invoiceNumber,
+                'invoiceValue': invoiceValue,
+                'invoiceUrl': invoiceUrl,
+                'issuanceDate': issuanceDate,
+                'trackingNumber': trackingNumber,
+                'invoiceKey': invoiceKey,
+                'trackingUrl': trackingUrl,
+                'embeddedInvoice': embeddedInvoice,
+                'package_type': package_type,
+                "cfop":cfop,
+                "volumes":volumes,
+                "EnableInferItems":EnableInferItems,
+                'itemIndex': itemIndex,
+                'quantity': quantity,
+                'price': price,
+                'description': description,
+                'unitMultiplier': unitMultiplier}, index=[0])
+            init.df = init.df.append(df1)
+    except:
+        print("No packages ")    
+
+        
+def delete_duplicate():
+    try:
+        print("Eliminando duplicados")
+        client = bigquery.Client()
+        QUERY = ('CREATE OR REPLACE TABLE `shopstar-datalake.test.shopstar_order_package` AS SELECT DISTINCT * FROM `shopstar-datalake.test.shopstar_order_package`')
+        query_job = client.query(QUERY)
+        rows = query_job.result()
+        print(rows)
+    except:
+        print("Consulta SQL no ejecutada")
+
+
+def run():
+    df = init.df
+    df.reset_index(drop=True, inplace=True)
+    json_data = df.to_json(orient = 'records')
+    json_object = json.loads(json_data)
+    table_schema = [
+        {
+            "name": "orderId",
+            "type": "STRING",
+            "mode": "NULLABLE"
+    },{
+        "name": "courier",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "invoiceNumber",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "invoiceValue",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "invoiceUrl",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "issuanceDate",
+        "type": "TIMESTAMP",
+        "mode": "NULLABLE"
+    },{
+        "name": "trackingNumber",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "invoiceKey",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "trackingUrl",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "embeddedInvoice",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "package_type",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "cfop",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "volumes",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "EnableInferItems",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "itemIndex",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "quantity",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "price",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "description",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "unitMultiplier",
+        "type": "FLOAT",
+        "mode": "NULLABLE"
+    }]
+    
+     
+    project_id = '999847639598'
+    dataset_id = 'test'
+    table_id = 'shopstar_totals'
+    
+    client  = bigquery.Client(project = project_id)
+    dataset  = client.dataset(dataset_id)
+    table = dataset.table(table_id)
+    
+    try:
+        client  = bigquery.Client(project = project_id)
+        dataset  = client.dataset(dataset_id)
+        table = dataset.table(table_id)
+        job_config = bigquery.LoadJobConfig()
+        job_config.schema = format_schema(table_schema)
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        job = client.load_table_from_json(json_object, table, job_config = job_config)
+        print(job.result())
+        delete_duplicate()
+    except:
+        client  = bigquery.Client(project = project_id)
+        dataset  = client.dataset(dataset_id)
+        table = dataset.table(table_id)
+        job_config = bigquery.LoadJobConfig()
+        job_config.autodetect = True
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        job = client.load_table_from_json(json_object, table, job_config = job_config)
+        print(job.result())
+        delete_duplicate()
+    
+    
+def get_params():
+    print("Cargando consulta")
+    client = bigquery.Client()
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.test.shopstar_order_package`))')
+    query_job = client.query(QUERY)
+    rows = query_job.result()
+    registro = 0
+    for row in rows:
+        registro += 1
+        get_order(row.orderId)
+        print("Registro: "+str(registro))
+    run()
+    
+get_params()
