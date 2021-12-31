015e9ae915936593396ae8831cf44b315551346b
bredg <bred_valenzuela@gmail.com>
2021-12-31 09:08:07 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/item.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/item.py
@@ -18,8 +18,81 @@ def format_schema(schema):
         formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
     return formatted_schema
 
+def items():
+    try:
+        url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
+        response = requests.request("GET", url, headers=init.headers)
+        Fjson = json.loads(response.text)
+        packageAttachment = Fjson["packageAttachment"]
+        packages = packageAttachment["packages"]
+        
+        items = ""
+        courier = ""
+        invoiceNumber = ""
+        invoiceValue = ""
+        invoiceUrl = ""
+        issuanceDate = ""
+        trackingNumber = ""
+        invoiceKey = ""
+        trackingUrl = ""
+        embeddedInvoice = ""
+        package_type = ""
+        cfop  = ""
+        volumes  = ""
+        EnableInferItems = ""
+        
+        for x in packages:
+            items = packages["items"]
+            courier = x["courier"]
+            invoiceNumber = x["invoiceNumber"]
+            invoiceValue = x["invoiceValue"]
+            invoiceUrl = x["invoiceUrl"]
+            issuanceDate = x["issuanceDate"]
+            trackingNumber = x["trackingNumber"]
+            invoiceKey = x["invoiceKey"]
+            trackingUrl = x["trackingUrl"]
+            embeddedInvoice = x["embeddedInvoice"]
+            package_type = x["type"]
+            cfop = x["cfop"]
+            volumes = x["volumes"]
+            EnableInferItems = x["EnableInferItems"]
+        
+        for y in items:
+            print(y)
+        '''  
+        for y in items:
+            itemIndex = y["itemIndex"]
+            quantity = y["quantity"]
+            price = y["price"]
+            description = y["description"]
+            unitMultiplier = y["unitMultiplier"]
+            df1 = pd.DataFrame({
+                'courier': courier,
+                'invoiceNumber': invoiceNumber,
+                'invoiceValue': invoiceValue,
+                'invoiceUrl': invoiceUrl,
+                'issuanceDate': issuanceDate,
+                'trackingNumber': trackingNumber,
+                'invoiceKey': invoiceKey,
+                'trackingUrl': trackingUrl,
+                'embeddedInvoice': embeddedInvoice,
+                'package_type': package_type,
+                "cfop":cfop,
+                "volumes":volumes,
+                "EnableInferItems":EnableInferItems,
+                'itemIndex': itemIndex,
+                'quantity': quantity,
+                'price': price,
+                'description': description,
+                'unitMultiplier': unitMultiplier}, index=[0])
+            init.df = init.df.append(df1)
+            '''
+    except:
+        print("No packages ")
+
+
 def get_order(id):
-   # try:
+    try:
         url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
         response = requests.request("GET", url, headers=init.headers)
         Fjson = json.loads(response.text)
@@ -146,81 +219,10 @@ def get_order(id):
                 'width': width,
                 'item_itemAttachment_name': item_itemAttachment_name}, index=[0])
             init.df = init.df.append(df1)
-            items()
-    #3except:
-      #  print("vacio")
-        
-def items():
-    try:
-        url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
-        response = requests.request("GET", url, headers=init.headers)
-        Fjson = json.loads(response.text)
-        packageAttachment = Fjson["packageAttachment"]
-        packages = packageAttachment["packages"]
-        
-        items = ""
-        courier = ""
-        invoiceNumber = ""
-        invoiceValue = ""
-        invoiceUrl = ""
-        issuanceDate = ""
-        trackingNumber = ""
-        invoiceKey = ""
-        trackingUrl = ""
-        embeddedInvoice = ""
-        package_type = ""
-        cfop  = ""
-        volumes  = ""
-        EnableInferItems = ""
-        
-        for x in packages:
-            items = packages["items"]
-            courier = x["courier"]
-            invoiceNumber = x["invoiceNumber"]
-            invoiceValue = x["invoiceValue"]
-            invoiceUrl = x["invoiceUrl"]
-            issuanceDate = x["issuanceDate"]
-            trackingNumber = x["trackingNumber"]
-            invoiceKey = x["invoiceKey"]
-            trackingUrl = x["trackingUrl"]
-            embeddedInvoice = x["embeddedInvoice"]
-            package_type = x["type"]
-            cfop = x["cfop"]
-            volumes = x["volumes"]
-            EnableInferItems = x["EnableInferItems"]
-        
-        for y in items:
-            print(y)
-        '''  
-        for y in items:
-            itemIndex = y["itemIndex"]
-            quantity = y["quantity"]
-            price = y["price"]
-            description = y["description"]
-            unitMultiplier = y["unitMultiplier"]
-            df1 = pd.DataFrame({
-                'courier': courier,
-                'invoiceNumber': invoiceNumber,
-                'invoiceValue': invoiceValue,
-                'invoiceUrl': invoiceUrl,
-                'issuanceDate': issuanceDate,
-                'trackingNumber': trackingNumber,
-                'invoiceKey': invoiceKey,
-                'trackingUrl': trackingUrl,
-                'embeddedInvoice': embeddedInvoice,
-                'package_type': package_type,
-                "cfop":cfop,
-                "volumes":volumes,
-                "EnableInferItems":EnableInferItems,
-                'itemIndex': itemIndex,
-                'quantity': quantity,
-                'price': price,
-                'description': description,
-                'unitMultiplier': unitMultiplier}, index=[0])
-            init.df = init.df.append(df1)
-            '''
+        items()
     except:
-        print("No packages ")
+        print("vacio")
+        
         
 
 
