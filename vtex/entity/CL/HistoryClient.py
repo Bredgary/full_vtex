12c74ab6f861ee6d03b983f249cfd42832eb4765
bredg <bred_valenzuela@gmail.com>
2021-12-31 08:40:57 -0300

    MOD: prueba

--- vtex/entity/CL/HistoryClient.py
+++ vtex/entity/CL/HistoryClient.py
@@ -227,6 +227,7 @@ def run(variFecha):
 	delete_duplicate()
 #Dia:  2021-10-19
 
+
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
