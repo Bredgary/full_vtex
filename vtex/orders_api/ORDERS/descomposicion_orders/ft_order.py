427becb7cbf80a4da86313277bd73fe2549687fc
bredg <bred_valenzuela@gmail.com>
2022-01-01 19:50:37 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
@@ -65,7 +65,7 @@ def get_order(id):
     allowCancellation = Fjson["allowCancellation"]
     allowEdition = Fjson["allowEdition"]
     isCheckedIn = Fjson["isCheckedIn"]
-    marketplace = Fjson["marketplace"]
+    marketplace = Fjson["marketplaceItems"]
     authorizedDate = Fjson["authorizedDate"]
     invoicedDate = Fjson["invoicedDate"]
     cancelReason = Fjson["cancelReason"]
