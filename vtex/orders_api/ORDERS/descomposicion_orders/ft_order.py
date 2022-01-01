5d35b01b8ddebf8e1ddaa1fbe1911bc9c3e3b530
bredg <bred_valenzuela@gmail.com>
2022-01-01 19:29:36 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
@@ -507,7 +507,6 @@ def run():
         dataset  = client.dataset(dataset_id)
         table = dataset.table(table_id)
         job_config = bigquery.LoadJobConfig()
-        job_config.write_disposition = "WRITE_TRUNCATE"
         job_config.schema = format_schema(table_schema)
         job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
         job = client.load_table_from_json(json_object, table, job_config = job_config)
@@ -518,7 +517,6 @@ def run():
         dataset  = client.dataset(dataset_id)
         table = dataset.table(table_id)
         job_config = bigquery.LoadJobConfig()
-        job_config.write_disposition = "WRITE_TRUNCATE"
         job_config.autodetect = True
         job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
         job = client.load_table_from_json(json_object, table, job_config = job_config)
