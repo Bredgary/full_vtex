8fe59525c9978c3f6e255763f78dd6cc698b16a6
bredg <bred_valenzuela@gmail.com>
2022-01-03 20:49:28 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/package.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/package.py
@@ -223,7 +223,8 @@ def run():
         dataset  = client.dataset(dataset_id)
         table = dataset.table(table_id)
         job_config = bigquery.LoadJobConfig()
-        #job_config.autodetect = True
+        job_config.write_disposition = "WRITE_TRUNCATE"
+        job_config.autodetect = True
         job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
         job = client.load_table_from_json(json_object, table, job_config = job_config)
         print(job.result())
@@ -233,7 +234,7 @@ def run():
 def get_params():
     print("Cargando consulta")
     client = bigquery.Client()
-    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.test.shopstar_order_package`))')
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order')
     query_job = client.query(QUERY)
     rows = query_job.result()
     registro = 0
