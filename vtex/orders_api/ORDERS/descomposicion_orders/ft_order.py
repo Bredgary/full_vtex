063041ce956fe54121bbe43954df3d2a8b0fd46f
bredg <bred_valenzuela@gmail.com>
2022-01-03 11:32:12 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
@@ -29,7 +29,7 @@ def decrypt_email(email):
     print(str(email))
 
 def get_order(id):
-    try:
+    #try:
         url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
         response = requests.request("GET", url, headers=init.headers)
         Fjson = json.loads(response.text)
@@ -281,8 +281,8 @@ def get_order(id):
             'shipping_complement': shipping_complement,
             'shipping_reference': shipping_reference}, index=[0])
         init.df = init.df.append(df1)
-    except:
-        print("terminado")
+    #except:
+     #   print("terminado")
 
 def delete_duplicate():
   try:
