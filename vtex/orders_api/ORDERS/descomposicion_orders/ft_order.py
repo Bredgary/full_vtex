dc54c0b5074eb62799cf8f777e4f620a2afafd4d
bredg <bred_valenzuela@gmail.com>
2022-01-03 14:43:38 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/ft_order.py
@@ -642,10 +642,10 @@ def get_params():
         registro += 1
         get_order(row.orderId)
         print("Registro: "+str(registro))
-        if registro == 5:
-            run()
-        if registro == 10:
-            run()
+        #if registro == 5:
+        #    run()
+        #if registro == 10:
+        #    run()
         if registro == 100:
             run()
         if registro == 200:
