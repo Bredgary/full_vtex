9d6bde80213b401e6a51111f01929f50e2cb7b97
bredg <bred_valenzuela@gmail.com>
2022-01-04 14:50:14 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/item.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/item.py
@@ -340,7 +340,7 @@ def run():
     
     project_id = '999847639598'
     dataset_id = 'test'
-    table_id = 'shopstar_order_item'
+    table_id = 'shopstar_order_items'
     
     client  = bigquery.Client(project = project_id)
     dataset  = client.dataset(dataset_id)
