571836090eb8fe6025cbcd5b40379a34b0ffe2a5
bredg <bred_valenzuela@gmail.com>
2022-01-04 22:29:23 -0300

    MOD: prueba

--- vtex/entity/CL/HistoryClient.py
+++ vtex/entity/CL/HistoryClient.py
@@ -33,7 +33,7 @@ def delete_duplicate():
 		print("Eliminando duplicados")
 		client = bigquery.Client()
 		QUERY = (
-			'CREATE OR REPLACE TABLE `shopstar-datalake.test.shopstar_vtex_client` AS SELECT DISTINCT * FROM `shopstar-datalake.test.shopstar_vtex_client`')
+			'CREATE OR REPLACE TABLE `shopstar-datalake.test.shopstar_vtex_client_test` AS SELECT DISTINCT * FROM `shopstar-datalake.test.shopstar_vtex_client_test`')
 		query_job = client.query(QUERY)
 		rows = query_job.result()
 		print(rows)
@@ -41,190 +41,193 @@ def delete_duplicate():
 		print("Consulta SQL no ejecutada")
 
 def run(variFecha):
-	df = pd.DataFrame(cl_client(variFecha),
-					columns=['beneficio','beneficio2','crearGiftcard','profilePicture','proteccionDatos','terminosCondiciones','terminosPago','tradeName','rclastcart','rclastsession','rclastsessiondate','homePhone','phone','stateRegistration','email','userId','firstName','lastName','document','localeDefault','attach','approved','birthDate','businessPhone','corporateDocument','corporateName','documentType','gender','customerClass','priceTables','id','accountId','accountName','dataEntityId','createdBy','createdIn','updatedBy','updatedIn','lastInteractionBy','lastInteractionIn'])
-	df.reset_index(drop=True, inplace=True)
-
-	json_data = df.to_json(orient = 'records')
-	json_object = json.loads(json_data)
+	try:
+		df = pd.DataFrame(cl_client(variFecha),
+						columns=['beneficio','beneficio2','crearGiftcard','profilePicture','proteccionDatos','terminosCondiciones','terminosPago','tradeName','rclastcart','rclastsession','rclastsessiondate','homePhone','phone','stateRegistration','email','userId','firstName','lastName','document','localeDefault','attach','approved','birthDate','businessPhone','corporateDocument','corporateName','documentType','gender','customerClass','priceTables','id','accountId','accountName','dataEntityId','createdBy','createdIn','updatedBy','updatedIn','lastInteractionBy','lastInteractionIn'])
+		df.reset_index(drop=True, inplace=True)
 	
-	table_schema = {
-	    "name": "id",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "email",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "userId",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "firstName",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "lastName",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "document",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "localeDefault",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "attach",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "accountId",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "accountName",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "dataEntityId",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "createdBy",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "createdIn",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "updatedBy",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "beneficio2",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "crearGiftcard",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "profilePicture",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "proteccionDatos",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "terminosCondiciones",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "terminosPago",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "tradeName",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "rclastcart",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "rclastsession",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "rclastsessiondate",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "homePhone",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "phone",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "stateRegistration",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "approved",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "birthDate",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "businessPhone",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "corporateDocument",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "corporateName",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "documentType",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "gender",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "customerClass",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "priceTables",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "beneficio",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "updatedIn",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "lastInteractionBy",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  },{
-	    "name": "lastInteractionIn",
-	    "type": "STRING",
-	    "mode": "NULLABLE"
-	  }
-
-	project_id = '999847639598'
-	dataset_id = 'test'
-	table_id = 'shopstar_vtex_client_test'
+		json_data = df.to_json(orient = 'records')
+		json_object = json.loads(json_data)
+		
+		table_schema = {
+		    "name": "id",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "email",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "userId",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "firstName",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "lastName",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "document",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "localeDefault",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "attach",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "accountId",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "accountName",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "dataEntityId",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "createdBy",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "createdIn",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "updatedBy",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "beneficio2",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "crearGiftcard",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "profilePicture",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "proteccionDatos",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "terminosCondiciones",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "terminosPago",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "tradeName",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "rclastcart",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "rclastsession",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "rclastsessiondate",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "homePhone",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "phone",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "stateRegistration",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "approved",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "birthDate",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "businessPhone",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "corporateDocument",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "corporateName",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "documentType",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "gender",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "customerClass",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "priceTables",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "beneficio",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "updatedIn",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "lastInteractionBy",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  },{
+		    "name": "lastInteractionIn",
+		    "type": "STRING",
+		    "mode": "NULLABLE"
+		  }
 	
-	client  = bigquery.Client(project = project_id)
-	dataset  = client.dataset(dataset_id)
-	table = dataset.table(table_id)
-	
-	job_config = bigquery.LoadJobConfig()
-	job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-	#job_config.schema = format_schema(table_schema)
-	job_config.autodetect = True
-	job = client.load_table_from_json(json_object, table, job_config = job_config)
-	print(job.result())
-	delete_duplicate()
+		project_id = '999847639598'
+		dataset_id = 'test'
+		table_id = 'shopstar_vtex_client_test'
+		
+		client  = bigquery.Client(project = project_id)
+		dataset  = client.dataset(dataset_id)
+		table = dataset.table(table_id)
+		
+		job_config = bigquery.LoadJobConfig()
+		job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+		#job_config.schema = format_schema(table_schema)
+		job_config.autodetect = True
+		job = client.load_table_from_json(json_object, table, job_config = job_config)
+		print(job.result())
+		delete_duplicate()
+	except:
+		print("400 Schema has no fields.")
 #Dia:  2021-10-19
 
 
