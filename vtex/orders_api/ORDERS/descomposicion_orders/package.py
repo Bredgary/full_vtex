2f94332f915f2cd5ec51e51cfc78267d16fb2fbd
bredg <bred_valenzuela@gmail.com>
2022-01-05 03:07:14 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/package.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/package.py
@@ -7,6 +7,7 @@ import requests
 from datetime import datetime, timezone
 from os.path import join
 from pip._vendor.pyparsing import empty
+import logging
 
 class init:
     df = pd.DataFrame()
@@ -86,114 +87,120 @@ def delete_duplicate():
 
 
 def run():
-    df = init.df
-    df.reset_index(drop=True, inplace=True)
-    json_data = df.to_json(orient = 'records')
-    json_object = json.loads(json_data)
-    table_schema = [
-        {
-            "name": "orderId",
+    try:
+        df = init.df
+        df.reset_index(drop=True, inplace=True)
+        json_data = df.to_json(orient = 'records')
+        json_object = json.loads(json_data)
+        table_schema = [
+            {
+                "name": "orderId",
+                "type": "STRING",
+                "mode": "NULLABLE"
+        },{
+            "name": "courier",
             "type": "STRING",
             "mode": "NULLABLE"
-    },{
-        "name": "courier",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "invoiceNumber",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "invoiceValue",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },{
-        "name": "invoiceUrl",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "issuanceDate",
-        "type": "TIMESTAMP",
-        "mode": "NULLABLE"
-    },{
-        "name": "trackingNumber",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "invoiceKey",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "trackingUrl",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "embeddedInvoice",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "package_type",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "cfop",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "volumes",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },{
-        "name": "EnableInferItems",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "itemIndex",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },{
-        "name": "quantity",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },{
-        "name": "price",
-        "type": "INTEGER",
-        "mode": "NULLABLE"
-    },{
-        "name": "description",
-        "type": "STRING",
-        "mode": "NULLABLE"
-    },{
-        "name": "unitMultiplier",
-        "type": "FLOAT",
-        "mode": "NULLABLE"
-    }]
-    
-     
-    project_id = '999847639598'
-    dataset_id = 'test'
-    table_id = 'shopstar_order_package'
-    
-    client  = bigquery.Client(project = project_id)
-    dataset  = client.dataset(dataset_id)
-    table = dataset.table(table_id)
-    
-    client  = bigquery.Client(project = project_id)
-    dataset  = client.dataset(dataset_id)
-    table = dataset.table(table_id)
-    job_config = bigquery.LoadJobConfig()
-    job_config.write_disposition = "WRITE_TRUNCATE"
-    job_config.autodetect = True
-    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-    job = client.load_table_from_json(json_object, table, job_config = job_config)
-    print(job.result())
-    delete_duplicate()
+        },{
+            "name": "invoiceNumber",
+            "type": "STRING",
+            "mode": "NULLABLE"
+        },{
+            "name": "invoiceValue",
+            "type": "INTEGER",
+            "mode": "NULLABLE"
+        },{
+            "name": "invoiceUrl",
+            "type": "STRING",
+            "mode": "NULLABLE"
+        },{
+            "name": "issuanceDate",
+            "type": "TIMESTAMP",
+            "mode": "NULLABLE"
+        },{
+            "name": "trackingNumber",
+            "type": "STRING",
+            "mode": "NULLABLE"
+        },{
+            "name": "invoiceKey",
+            "type": "STRING",
+            "mode": "NULLABLE"
+        },{
+            "name": "trackingUrl",
+            "type": "STRING",
+            "mode": "NULLABLE"
+        },{
+            "name": "embeddedInvoice",
+            "type": "STRING",
+            "mode": "NULLABLE"
+        },{
+            "name": "package_type",
+            "type": "STRING",
+            "mode": "NULLABLE"
+        },{
+            "name": "cfop",
+            "type": "STRING",
+            "mode": "NULLABLE"
+        },{
+            "name": "volumes",
+            "type": "INTEGER",
+            "mode": "NULLABLE"
+        },{
+            "name": "EnableInferItems",
+            "type": "STRING",
+            "mode": "NULLABLE"
+        },{
+            "name": "itemIndex",
+            "type": "INTEGER",
+            "mode": "NULLABLE"
+        },{
+            "name": "quantity",
+            "type": "INTEGER",
+            "mode": "NULLABLE"
+        },{
+            "name": "price",
+            "type": "INTEGER",
+            "mode": "NULLABLE"
+        },{
+            "name": "description",
+            "type": "STRING",
+            "mode": "NULLABLE"
+        },{
+            "name": "unitMultiplier",
+            "type": "FLOAT",
+            "mode": "NULLABLE"
+        }]
+        
+         
+        project_id = '999847639598'
+        dataset_id = 'test'
+        table_id = 'shopstar_order_package'
+        
+        client  = bigquery.Client(project = project_id)
+        dataset  = client.dataset(dataset_id)
+        table = dataset.table(table_id)
+        
+        if df.empty:
+            print('DataFrame is empty!')
+        else:
+            client  = bigquery.Client(project = project_id)
+            dataset  = client.dataset(dataset_id)
+            table = dataset.table(table_id)
+            job_config = bigquery.LoadJobConfig()
+            job_config.schema = format_schema(table_schema)
+            job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+            job = client.load_table_from_json(json_object, table, job_config = job_config)
+            print(job.result())
+            delete_duplicate()
+    except:
+        print("Error.")
+        logging.exception("message")
     
     
 def get_params():
     print("Cargando consulta")
     client = bigquery.Client()
-    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`')
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.test.shopstar_order_package`))')
     query_job = client.query(QUERY)
     rows = query_job.result()
     registro = 0
@@ -201,10 +208,43 @@ def get_params():
         registro += 1
         get_order_package(row.orderId)
         print("Registro: "+str(registro))
+        if registro == 100:
+            run()
+        if registro == 200:
+            run()
+        if registro == 300:
+            run()
+        if registro == 400:
+            run()
+        if registro == 500:
+            run()
+        if registro == 600:
+            run()
+        if registro == 700:
+            run()
+        if registro == 800:
+            run()
+        if registro == 900:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1100:
+            run()
+        if registro == 2200:
+            run()
+        if registro == 3300:
+            run()
+        if registro == 4400:
+            run()
+        if registro == 5500:
+            run()
+        if registro == 6500:
+            run()
+        if registro == 7500:
+            run()
         if registro == 10000:
             run()
-        if registro == 50000:
+        if registro == 12000:
             run()
     run()
-    
 get_params()
