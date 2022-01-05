5aacc9881b5ffbff8da548b831b79ae2d8bea03c
bredg <bred_valenzuela@gmail.com>
2022-01-04 23:36:37 -0300

    MOD: prueba

--- vtex/entity/CL/HistoryClient.py
+++ vtex/entity/CL/HistoryClient.py
@@ -242,7 +242,8 @@ def run():
 def daterange(start_date, end_date):
 	for n in range(int((end_date - start_date).days)):
 		yield start_date + timedelta(n)
-	
+
+def time():	
 	start_date = date(2019, 1, 1)
 	end_date = date(2020, 1, 1)
 	for single_date in daterange(start_date, end_date):
