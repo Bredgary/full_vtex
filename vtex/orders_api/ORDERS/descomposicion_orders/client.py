cab86f865666fe7f71f524c09ae8edf30b5a5702
bredg <bred_valenzuela@gmail.com>
2022-01-06 17:04:05 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/client.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/client.py
@@ -85,7 +85,7 @@ def delete_duplicate():
 
 
 def run():
-   # try:
+    try:
         df = init.df
         df.reset_index(drop=True, inplace=True)
         json_data = df.to_json(orient = 'records')
@@ -159,16 +159,16 @@ def run():
         dataset  = client.dataset(dataset_id)
         table = dataset.table(table_id)
         job_config = bigquery.LoadJobConfig()
-        job_config.write_disposition = "WRITE_TRUNCATE"
-        job_config.autodetect = True
+        #job_config.write_disposition = "WRITE_TRUNCATE"
+        #job_config.autodetect = True
         #job_config.schema = format_schema(table_schema)
         job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
         job = client.load_table_from_json(json_object, table, job_config = job_config)
         print(job.result())
         delete_duplicate()
-    #except:
-    #    print("Error")
-    #    logging.exception("message")
+    except:
+        print("Error")
+        logging.exception("message")
 
 def get_params():
     print("Cargando consulta")
@@ -182,35 +182,35 @@ def get_params():
         registro += 1
         get_order(row.orderId)
         print("Registro: "+str(registro))
+        if registro == 300:
+            run()
+        if registro == 400:
+            run()
+        if registro == 500:
+            run()
+        if registro == 600:
+            run()
+        if registro == 700:
+            run()
+        if registro == 800:
+            run()
+        if registro == 900:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1100:
+            run()
+        if registro == 1200:
+            run()
+        if registro == 1300:
+            run()
+        if registro == 1400:
+            run()
+        if registro == 1500:
+            run()
         if registro == 10000:
             run()
-        if registro == 12000:
-            run()
-        if registro == 15000:
-            run()
         if registro == 20000:
-            run()
-        if registro == 30000:
-            run()
-        if registro == 35000:
-            run()
-        if registro == 40000:
-            run()
-        if registro == 45000:
-            run()
-        if registro == 50000:
-            run()
-        if registro == 60000:
-            run()
-        if registro == 70000:
-            run()
-        if registro == 80000:
-            run()
-        if registro == 85000:
-            run()
-        if registro == 90000:
-            run()
-        if registro == 95000:
             run()
     run()
            
