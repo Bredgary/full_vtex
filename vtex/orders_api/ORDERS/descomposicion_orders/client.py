f480d7cb162470881a669732b8a3058c9c7aea82
bredg <bred_valenzuela@gmail.com>
2022-01-06 09:28:01 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/client.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/client.py
@@ -160,7 +160,7 @@ def run():
         table = dataset.table(table_id)
         job_config = bigquery.LoadJobConfig()
         #job_config.write_disposition = "WRITE_TRUNCATE"
-        j#ob_config.autodetect = True
+        #job_config.autodetect = True
         #job_config.schema = format_schema(table_schema)
         job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
         job = client.load_table_from_json(json_object, table, job_config = job_config)
