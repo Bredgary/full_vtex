2b6e54507687f5197e114d648026d596f18cd2bd
bredg <bred_valenzuela@gmail.com>
2022-01-06 21:24:25 -0300

    MOD: prueba

--- vtex/modeloPersona/modeloPersona.py
+++ vtex/modeloPersona/modeloPersona.py
@@ -9,9 +9,9 @@ from os.path import join
 import logging
 
 
-def create_customer():
+def run():
   try:
-    print("Eliminando duplicados")
+    print("Creacion dm_customer")
     client = bigquery.Client()
     QUERY = ('''
     CREATE OR REPLACE TABLE `shopstar-datalake.cons_zone.dm_customer` AS
@@ -59,10 +59,30 @@ def create_customer():
     FROM `shopstar-datalake.staging_zone.shopstar_vtex_client`''')
     query_job = client.query(QUERY)
     rows = query_job.result()
-    print(rows)
     print("dm_cosutomer actualizado exitosamente")
+    create_customer_history()
   except:
     print("Error create_customer!!")
     logging.exception("message")
     
-create_customer()
+def create_customer_history():
+  try:
+    print("Creacion dm_customer_history")
+    client = bigquery.Client()
+    QUERY = ('''
+    CREATE OR REPLACE TABLE `shopstar-datalake.cons_zone.dm_customer_history` AS 
+    SELECT *
+    FROM (
+    SELECT
+    *,
+    ROW_NUMBER() OVER (PARTITION BY createdIn) row_number
+    FROM `shopstar-datalake.cons_zone.dm_customer`)
+    WHERE row_number = 1 ''')
+    query_job = client.query(QUERY)
+    rows = query_job.result()
+    print("dm_cosutomer_history actualizado exitosamente")
+  except:
+    print("Error create_customer_history!!")
+    logging.exception("message")
+    
+run()
