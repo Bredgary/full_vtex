feab6872006a24cc9cb394046e14a790a7ed5109
bredg <bred_valenzuela@gmail.com>
2022-01-11 17:02:13 -0300

    MOD: prueba

---
+++ vtex/orders_api/ORDERS/descomposicion_orders/shippingData_logisticsInfo_slas.py
@@ -0,0 +1,166 @@
+import pandas as pd
+import numpy as np
+from google.cloud import bigquery
+import os, json
+from datetime import datetime
+import requests
+from os.path import join
+
+class init:
+  df = pd.DataFrame()
+
+def get_order(id):
+    try:
+        headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+        url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
+        response = requests.request("GET", url, headers=headers)
+        Fjson = json.loads(response.text)
+        
+        shippingData = Fjson["shippingData"]
+        shipping_logisticsInfo = shippingData["logisticsInfo"]
+        
+        for x in shipping_logisticsInfo:
+            slas = x["slas"]
+            for y in slas:
+                id_slas = y["id"]
+                name = y["name"]
+                shippingEstimate = y["shippingEstimate"]
+                deliveryWindow = y["deliveryWindow"]
+                price = y["price"]
+                deliveryChannel = y["deliveryChannel"]
+                polygonName = y["polygonName"]
+                lockTTL = y["lockTTL"]
+                pickupPointId = y["pickupPointId"]
+                transitTime = y["transitTime"]
+                
+                df1 = pd.DataFrame({
+                    'orderId': id,
+                    'id_slas': id_slas,
+                    'name': name,
+                    'shippingEstimate': shippingEstimate,
+                    'deliveryWindow': deliveryWindow,
+                    'price': price,
+                    'deliveryChannel': deliveryChannel,
+                    'polygonName': polygonName,
+                    'lockTTL': lockTTL,
+                    'pickupPointId': pickupPointId,
+                    'transitTime': transitTime}, index=[0])
+            init.df = init.df.append(df1)
+    except:
+        print(id)
+        print("Vacio")
+
+def delete_duplicate():
+  try:
+    print("Eliminando duplicados")
+    client = bigquery.Client()
+    QUERY = ('CREATE OR REPLACE TABLE `shopstar-datalake.test.shippingData_logisticsInfo_slas` AS SELECT DISTINCT * FROM `shopstar-datalake.test.shippingData_logisticsInfo_slas`')
+    query_job = client.query(QUERY)
+    rows = query_job.result()
+    print(rows)
+  except:
+    print("Consulta SQL no ejecutada")
+
+
+def run():
+    try:
+        df = init.df
+        df.reset_index(drop=True, inplace=True)
+        json_data = df.to_json(orient = 'records')
+        json_object = json.loads(json_data)
+        
+        project_id = '999847639598'
+        dataset_id = 'test'
+        table_id = 'shippingData_logisticsInfo_slas'
+        
+        if df.empty:
+            print('DataFrame is empty!')
+        else:
+            client  = bigquery.Client(project = project_id)
+            dataset  = client.dataset(dataset_id)
+            table = dataset.table(table_id)
+            job_config = bigquery.LoadJobConfig()
+            job_config.write_disposition = "WRITE_TRUNCATE"
+            job_config.autodetect = True
+            job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+            job = client.load_table_from_json(json_object, table, job_config = job_config)
+            print(job.result())
+            delete_duplicate()
+    except:
+        print("Error.")
+        logging.exception("message")
+
+def get_params():
+    print("Cargando consulta")
+    client = bigquery.Client()
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`')
+    query_job = client.query(QUERY)  
+    rows = query_job.result()
+    registro = 0
+    for row in rows:
+        registro += 1
+        get_order(row.orderId)
+        print("Registro: "+str(registro))
+        if registro == 300:
+            run()
+        if registro == 400:
+            run()
+        if registro == 500:
+            run()
+        if registro == 600:
+            run()
+        if registro == 700:
+            run()
+        if registro == 800:
+            run()
+        if registro == 900:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1100:
+            run()
+        if registro == 1200:
+            run()
+        if registro == 1300:
+            run()
+        if registro == 1400:
+            run()
+        if registro == 1500:
+            run()
+        if registro == 10000:
+            run()
+        if registro == 15000:
+            run()
+        if registro == 20000:
+            run()
+        if registro == 25000:
+            run()
+        if registro == 30000:
+            run()
+        if registro == 35000:
+            run()
+        if registro == 40000:
+            run()
+        if registro == 45000:
+            run()
+        if registro == 50000:
+            run()
+        if registro == 55000:
+            run()
+        if registro == 60000:
+            run()
+        if registro == 65000:
+            run()
+        if registro == 70000:
+            run()
+        if registro == 75000:
+            run()
+        if registro == 80000:
+            run()
+        if registro == 85000:
+            run()
+        if registro == 90000:
+            run()
+    run()
+
+get_params()
