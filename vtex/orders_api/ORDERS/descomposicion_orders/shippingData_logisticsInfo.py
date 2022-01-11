82b8a4355a31d04074feae843d2e91a80e590131
bredg <bred_valenzuela@gmail.com>
2022-01-11 16:13:20 -0300

    MOD: prueba

---
+++ vtex/orders_api/ORDERS/descomposicion_orders/shippingData_logisticsInfo.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import numpy as np
+from google.cloud import bigquery
+import os, json
+from datetime import datetime
+import requests
+from datetime import datetime, timezone
+from os.path import join
+from _queue import Empty
+import datetime
+import time
+
+class init:
+  productList = []
+  df = pd.DataFrame()
+  headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+
+def format_schema(schema):
+  formatted_schema = []
+  for row in schema:
+    formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
+    return formatted_schema
+
+def decrypt_email(email):
+  try:
+    url = "https://conversationtracker.vtex.com.br/api/pvt/emailMapping?an=mercury&alias="+email+""
+    response = requests.request("GET", url, headers=init.headers)
+    formatoJ = json.loads(response.text)
+    return formatoJ["email"]
+  except:
+    print(str(email))
+
+def get_order(id):
+    try:
+        headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+        url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
+        response = requests.request("GET", url, headers=headers)
+        Fjson = json.loads(response.text)
+        
+        shippingData = Fjson["shippingData"]
+        shipping_logisticsInfo = shippingData["logisticsInfo"]
+        
+        for x in shipping_logisticsInfo:
+            shipping_logisticsInfo_itemIndex = x["itemIndex"]
+            shipping_logisticsInfo_selectedSla = x["selectedSla"]
+            shipping_logisticsInfo_lockTTL = x["lockTTL"]
+            shipping_logisticsInfo_price = x["price"]
+            shipping_logisticsInfo_listPrice = x["listPrice"]
+            shipping_logisticsInfo_sellingPrice = x["sellingPrice"]
+            shipping_logisticsInfo_deliveryWindow = x["deliveryWindow"]
+            shipping_logisticsInfo_deliveryCompany = x["deliveryCompany"]
+            shipping_logisticsInfo_shippingEstimate = x["shippingEstimate"]
+            shipping_logisticsInfo_shippingEstimateDate = x["shippingEstimateDate"]
+            shipping_logisticsInfo_deliveryChannel = x["deliveryChannel"]
+            shipping_logisticsInfo_addressId = x["addressId"]
+            shipping_logisticsInfo_polygonName = x["polygonName"]
+            
+            df1 = pd.DataFrame({
+                'orderId': id,
+                'itemIndex': shipping_logisticsInfo_itemIndex,
+                'selectedSla': shipping_logisticsInfo_selectedSla,
+                'lockTTL': shipping_logisticsInfo_lockTTL,
+                'price': shipping_logisticsInfo_price,
+                'listPrice': shipping_logisticsInfo_listPrice,
+                'sellingPrice':shipping_logisticsInfo_sellingPrice,
+                'deliveryWindow': shipping_logisticsInfo_deliveryWindow,
+                'deliveryCompany': shipping_logisticsInfo_deliveryCompany,
+                'shippingEstimate': shipping_logisticsInfo_shippingEstimate,
+                'shippingEstimateDate': shipping_logisticsInfo_shippingEstimateDate,
+                'deliveryChannel': shipping_logisticsInfo_deliveryChannel,
+                'addressId': shipping_logisticsInfo_addressId,
+                'polygonName': shipping_logisticsInfo_polygonName}, index=[0])
+            init.df = init.df.append(df1)
+    except:
+        print("Vacio")
+
+def delete_duplicate():
+  try:
+    print("Eliminando duplicados")
+    client = bigquery.Client()
+    QUERY = ('CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_ft_orders` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_ft_orders`')
+    query_job = client.query(QUERY)
+    rows = query_job.result()
+    print(rows)
+  except:
+    print("Consulta SQL no ejecutada")
+
+
+def run():
+    try:
+        df = init.df
+        df.reset_index(drop=True, inplace=True)
+        json_data = df.to_json(orient = 'records')
+        json_object = json.loads(json_data)
+        
+        
+        project_id = '999847639598'
+        dataset_id = 'test'
+        table_id = 'shipping_logisticsInfo'
+        
+        if df.empty:
+            print('DataFrame is empty!')
+        else:
+            client  = bigquery.Client(project = project_id)
+            dataset  = client.dataset(dataset_id)
+            table = dataset.table(table_id)
+            job_config = bigquery.LoadJobConfig()
+            job_config.write_disposition = "WRITE_TRUNCATE"
+            job_config.autodetect = True
+            job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+            job = client.load_table_from_json(json_object, table, job_config = job_config)
+            print(job.result())
+            delete_duplicate()
+    except:
+        print("Error.")
+        logging.exception("message")
+
+def get_params():
+    print("Cargando consulta")
+    client = bigquery.Client()
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.staging_zone.shopstar_ft_orders`))')
+    query_job = client.query(QUERY)  
+    rows = query_job.result()
+    registro = 0
+    for row in rows:
+        registro += 1
+        get_order(row.orderId)
+        print("Registro: "+str(registro))
+        if registro == 100:
+            run()
+            break
+    run()
+
+get_params()
