2793b55fffa1bef5ff58daf14a67a161171a72eb
bredg <bred_valenzuela@gmail.com>
2022-01-11 10:03:06 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/seller.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/seller.py
@@ -42,7 +42,7 @@ def delete_duplicate():
   try:
     print("Eliminando duplicados")
     client = bigquery.Client()
-    QUERY = ('CREATE OR REPLACE TABLE `shopstar-datalake.test.shopstar_order_seller` AS SELECT DISTINCT * FROM `shopstar-datalake.test.shopstar_order_seller`')
+    QUERY = ('CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_order_seller` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_order_seller`')
     query_job = client.query(QUERY)
     rows = query_job.result()
     print(rows)
@@ -58,7 +58,7 @@ def run():
         json_object = json.loads(json_data)
         
         project_id = '999847639598'
-        dataset_id = 'test'
+        dataset_id = 'staging_zone'
         table_id = 'shopstar_order_seller'
         
         if df.empty:
@@ -82,75 +82,11 @@ def run():
 def get_params():
     print("Cargando consulta")
     client = bigquery.Client()
-    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.test.shopstar_order_seller`))')
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.order_write`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.staging_zone.shopstar_order_seller`))')
     query_job = client.query(QUERY)  
     rows = query_job.result()
     registro = 0
     for row in rows:
-        registro += 1
-        get_order(row.orderId)
-        if registro == 100:
-            run()
-        if registro == 300:
-            run()
-        if registro == 400:
-            run()
-        if registro == 500:
-            run()
-        if registro == 600:
-            run()
-        if registro == 700:
-            run()
-        if registro == 800:
-            run()
-        if registro == 900:
-            run()
-        if registro == 1000:
-            run()
-        if registro == 1100:
-            run()
-        if registro == 1200:
-            run()
-        if registro == 1300:
-            run()
-        if registro == 1400:
-            run()
-        if registro == 1500:
-            run()
-        if registro == 10000:
-            run()
-        if registro == 15000:
-            run()
-        if registro == 20000:
-            run()
-        if registro == 25000:
-            run()
-        if registro == 30000:
-            run()
-        if registro == 35000:
-            run()
-        if registro == 40000:
-            run()
-        if registro == 45000:
-            run()
-        if registro == 50000:
-            run()
-        if registro == 55000:
-            run()
-        if registro == 60000:
-            run()
-        if registro == 65000:
-            run()
-        if registro == 70000:
-            run()
-        if registro == 75000:
-            run()
-        if registro == 80000:
-            run()
-        if registro == 85000:
-            run()
-        if registro == 90000:
-            run()
         print("Registro: "+str(registro))
     run()
 
