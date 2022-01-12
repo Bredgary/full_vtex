38b07d98cf3ed4c2a15ba63aba4f196afc926a1b
bredg <bred_valenzuela@gmail.com>
2022-01-12 08:20:05 -0300

    MOD: prueba

--- vtex/entity/CL/HistoryClient.py
+++ vtex/entity/CL/HistoryClient.py
@@ -246,7 +246,7 @@ def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-start_date = date(2016, 1, 1)
+start_date = date(2019, 1, 1)
 end_date = date(2021, 12, 15)
 for single_date in daterange(start_date, end_date):
     variFecha = single_date.strftime("%Y-%m-%d")
