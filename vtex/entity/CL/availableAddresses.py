e5fde873680da58643c763da546926c7df492236
bredg <bred_valenzuela@gmail.com>
2022-01-17 17:26:26 -0300

    MOD: prueba

--- vtex/entity/CL/availableAddresses.py
+++ vtex/entity/CL/availableAddresses.py
@@ -10,7 +10,6 @@ from os.path import join
 class init:
   productList = []
   df = pd.DataFrame()
-  df2 = pd.DataFrame()
   
   headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 
@@ -66,7 +65,7 @@ def get_order(email):
                 'reference': reference}, index=[0])
             init.df = init.df.append(df1)
     except:
-        print("Vacio")
+        print("No availableAddresses")     
 
 def run():
     try:
@@ -75,90 +74,17 @@ def run():
         json_data = df.to_json(orient = 'records')
         json_object = json.loads(json_data)
         
-        table_schema = [
-        {
-            "name": "email",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "userProfileId",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "profileProvider",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "addressType",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "receiverName",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "addressId",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "isDisposable",
-            "type": "BOOLEAN",
-            "mode": "NULLABLE"
-        },{
-            "name": "postalCode",
-            "type": "INTEGER",
-            "mode": "NULLABLE"
-        },{
-            "name": "city",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "state",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "country",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "street",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "number",
-            "type": "INTEGER",
-            "mode": "NULLABLE"
-        },{
-            "name": "neighborhood",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "complement",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "reference",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "isComplete",
-            "type": "BOOLEAN",
-            "mode": "NULLABLE"
-        }]
-        
         project_id = '999847639598'
         dataset_id = 'test'
         table_id = 'shopstar_vtex_client_availableAddresses'
-        try:
-            client  = bigquery.Client(project = project_id)
-            dataset  = client.dataset(dataset_id)
-            table = dataset.table(table_id)
-            job_config = bigquery.LoadJobConfig()
-            job_config.schema = format_schema(table_schema)
-            job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-            job = client.load_table_from_json(json_object, table, job_config = job_config)
-            print(job.result())
-        except:
+        
+        client  = bigquery.Client(project = project_id)
+        dataset  = client.dataset(dataset_id)
+        table = dataset.table(table_id)
+        
+        if df.empty:
+            print('DataFrame is empty!')
+        else:
             client  = bigquery.Client(project = project_id)
             dataset  = client.dataset(dataset_id)
             table = dataset.table(table_id)
@@ -167,140 +93,78 @@ def run():
             job_config.autodetect = True
             job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
             job = client.load_table_from_json(json_object, table, job_config = job_config)
+            print(job.result())
     except:
-        print("Vacio")
+        print("Error.")
+        logging.exception("message")
 
-def run2(email):
-    df = init.df
-    if df.empty:
-        df = pd.DataFrame({
-            'email': email,
-            'Data': False}, index=[0])
-        
-        df.reset_index(drop=True, inplace=True)
-        json_data = df.to_json(orient = 'records')
-        json_object = json.loads(json_data)
-        
-        project_id = '999847639598'
-        dataset_id = 'test'
-        table_id = 'shopstar_vtex_client_availableAddresses_No_Data'
-        client  = bigquery.Client(project = project_id)
-        dataset  = client.dataset(dataset_id)
-        table = dataset.table(table_id)
-        job_config = bigquery.LoadJobConfig()
-        job_config.write_disposition = "WRITE_TRUNCATE"
-        job_config.autodetect = True
-        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-        job = client.load_table_from_json(json_object, table, job_config = job_config)
-        print(job.result())
-        
-        
 def get_params():
-  print("Cargando consulta")
-  client = bigquery.Client()
-  QUERY = ('SELECT DISTINCT email FROM `shopstar-datalake.cons_zone.dm_customer`WHERE (email NOT IN (SELECT email FROM `shopstar-datalake.test.shopstar_vtex_client_availableAddresses`))')
-  query_job = client.query(QUERY)
-  rows = query_job.result()
-  registro = 0
-  for row in rows:
-    registro += 1
-    get_order(row.email)
-    print("Registro: "+str(registro))
-    if registro == 1:
-        run()
-        run2(row.email)
-    if registro == 300:
-        run()
-        run2(row.email)
-    if registro == 400:
-        run()
-        run2(row.email)
-    if registro == 500:
-        run()
-        run2(row.email)
-    if registro == 600:
-        run()
-        run2(row.email)
-    if registro == 700:
-        run()
-        run2(row.email)
-    if registro == 800:
-        run()
-        run2(row.email)
-    if registro == 900:
-        run()
-        run2(row.email)
-    if registro == 1000:
-        run()
-        run2(row.email)
-    if registro == 1100:
-        run()
-        run2(row.email)
-    if registro == 1200:
-        run()
-        run2(row.email)
-    if registro == 1300:
-        run()
-        run2(row.email)
-    if registro == 1400:
-        run()
-        run2(row.email)
-    if registro == 1500:
-        run()
-        run2(row.email)
-    if registro == 10000:
-        run()
-        run2(row.email)
-    if registro == 15000:
-        run()
-        run2(row.email)
-    if registro == 20000:
-        run()
-        run2(row.email)
-    if registro == 25000:
-        run()
-        run2(row.email)
-    if registro == 30000:
-        run()
-        run2(row.email)
-    if registro == 35000:
-        run()
-        run2(row.email)
-    if registro == 40000:
-        run()
-        run2(row.email)
-    if registro == 45000:
-        run()
-        run2(row.email)
-    if registro == 50000:
-        run()
-        run2(row.email)
-    if registro == 55000:
-        run()
-        run2(row.email)
-    if registro == 60000:
-        run()
-        run2(row.email)
-    if registro == 65000:
-        run()
-        run2(row.email)
-    if registro == 70000:
-        run()
-        run2(row.email)
-    if registro == 75000:
-        run()
-        run2(row.email)
-    if registro == 80000:
-        run()
-        run2(row.email)
-    if registro == 85000:
-        run()
-        run2(row.email)
-    if registro == 90000:
-        run()
-        run2(row.email)
-  run()
-  run2(row.email)
+    print("Cargando consulta")
+    client = bigquery.Client()
+    QUERY = ('SELECT email FROM `shopstar-datalake.cons_zone.dm_customer`')
+    query_job = client.query(QUERY)
+    rows = query_job.result()
+    registro = 0
+    for row in rows:
+        registro += 1
+        get_order(row.email,registro)
+        if registro == 100:
+            run()
+        if registro == 200:
+            run()
+        if registro == 300:
+            run()
+        if registro == 400:
+            run()
+        if registro == 500:
+            run()
+        if registro == 600:
+            run()
+        if registro == 700:
+            run()
+        if registro == 800:
+            run()
+        if registro == 900:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 10000:
+            run()
+        if registro == 20000:
+            run()
+        if registro == 30000:
+            run()
+        if registro == 40000:
+            run()
+        if registro == 50000:
+            run()
+        if registro == 60000:
+            run()
+        if registro == 70000:
+            run()
+        if registro == 80000:
+            run()
+        if registro == 90000:
+            run()
+        if registro == 100000:
+            run()
+    run()
 
   
 get_params()
