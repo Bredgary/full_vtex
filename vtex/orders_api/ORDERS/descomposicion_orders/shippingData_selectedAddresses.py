776f90873f58771457cf34f2e56407c12898c6e6
bredg <bred_valenzuela@gmail.com>
2022-01-17 12:54:51 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/shippingData_selectedAddresses.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/shippingData_selectedAddresses.py
@@ -83,7 +83,7 @@ def run():
         json_object = json.loads(json_data)
         
         project_id = '999847639598'
-        dataset_id = 'staging_zone'
+        dataset_id = 'test'
         table_id = 'shippingData_selectedAddresses'
         
         if df.empty:
@@ -93,8 +93,8 @@ def run():
             dataset  = client.dataset(dataset_id)
             table = dataset.table(table_id)
             job_config = bigquery.LoadJobConfig()
-            #job_config.write_disposition = "WRITE_TRUNCATE"
-            #job_config.autodetect = True
+            job_config.write_disposition = "WRITE_TRUNCATE"
+            job_config.autodetect = True
             job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
             job = client.load_table_from_json(json_object, table, job_config = job_config)
             print(job.result())
@@ -106,7 +106,7 @@ def run():
 def get_params():
     print("Cargando consulta")
     client = bigquery.Client()
-    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.staging_zone.shippingData_selectedAddresses`))')
+    QUERY = ('SELECT DISTINCT orderId  FROM `shopstar-datalake.test.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.test.shippingData_selectedAddresses`))')
     query_job = client.query(QUERY)  
     rows = query_job.result()
     registro = 0
