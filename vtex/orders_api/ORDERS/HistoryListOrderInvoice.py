f5da538853178ce9365678ae06407ced67e3c197
bredg <bred_valenzuela@gmail.com>
2022-01-18 15:36:43 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/HistoryListOrderInvoice.py
+++ vtex/orders_api/ORDERS/HistoryListOrderInvoice.py
@@ -56,7 +56,6 @@ def get_order_list(page):
             callCenterOperatorName = x["callCenterOperatorName"]
             totalItems = x["totalItems"]
             currencyCode = x["currencyCode"]
-            print(ShippingEstimatedDate)
             df1 = pd.DataFrame({
                 'orderId': orderId,
                 'creationDate': creationDate,
@@ -104,49 +103,140 @@ def delete_duplicate():
 
 
 def run():
-    try:
-        df = init.df
-        
-        if df.empty:
-            print('DataFrame is empty!')
-        else:
-            df.reset_index(drop=True, inplace=True)
-            json_data = df.to_json(orient = 'records')
-            json_object = json.loads(json_data)
-            
-            project_id = '999847639598'
-            dataset_id = 'test'
-            table_id = 'shopstar_vtex_list_order_'
-            table_temp = 'order_write'
-            
-            client  = bigquery.Client(project = project_id)
-            
-            dataset  = client.dataset(dataset_id)
-            tableO = dataset.table(table_id)
-            job_config = bigquery.LoadJobConfig()
-            job_config.write_disposition = "WRITE_TRUNCATE"
-            job_config.autodetect = True
-            job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-            job = client.load_table_from_json(json_object, tableO, job_config = job_config)
-            print(job.result())
-            
-            tableT = dataset.table(table_temp)
-            job_config_temp = bigquery.LoadJobConfig()
-            job_config_temp.write_disposition = "WRITE_TRUNCATE"
-            job_config_temp.autodetect = True
-            job_config_temp.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-            job = client.load_table_from_json(json_object, tableT, job_config = job_config_temp)
-            print(job.result())
-            delete_duplicate()
-    except:
-        print("Error.")
-        logging.exception("message")
+    df = init.df
+    df.reset_index(drop=True, inplace=True)
+    json_data = df.to_json(orient = 'records')
+    json_object = json.loads(json_data)
+    
+    table_schema =[{
+        "name": "currencyCode",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "totalItems",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "workflowInErrorSta1te",
+        "type": "BOOLEAN",
+        "mode": "NULLABLE"
+    },{
+        "name": "callCenterOperatorName",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "marketPlaceOrderId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "listType",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "authorizedDate",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },{
+        "name": "orderIsComplete",
+        "type": "BOOLEAN",
+        "mode": "NULLABLE"
+    },{
+        "name": "listId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "ShippingEstimatedDateMin",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },{
+        "name": "ShippingEstimatedDate",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },{
+        "name": "origin",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "affiliateId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "sequence",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "paymentNames",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "salesChannel",
+        "type": "INTEGER",
+        "mode": "NULLABLE"
+    },{
+        "name": "status",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "workflowInRetry",
+        "type": "BOOLEAN",
+        "mode": "NULLABLE"
+    },{
+        "name": "clientName",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "ShippingEstimatedDateMax",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },{
+        "name": "creationDate",
+        "type": "DATE",
+        "mode": "NULLABLE"
+    },{
+        "name": "lastMessageUnread",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "totalValue",
+        "type": "FLOAT",
+        "mode": "NULLABLE"
+    },{
+        "name": "statusDescription",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    },{
+        "name": "orderId",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    }]
+
+    
+    project_id = '999847639598'
+    dataset_id = 'staging_zone'
+    table_id = 'shopstar_vtex_list_order'
+    table_temp = 'order_write'
+    
+    client  = bigquery.Client(project = project_id)
+    dataset  = client.dataset(dataset_id)
+    tableO = dataset.table(table_id)
+    job_config = bigquery.LoadJobConfig()
+    job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+    job_config.schema = format_schema(table_schema)
+    job = client.load_table_from_json(json_object, tableO, job_config = job_config)
+    print(job.result())
+    
+    tableT = dataset.table(table_temp)
+    job_config_temp = bigquery.LoadJobConfig()
+    job_config_temp.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+    job_config.schema = format_schema(table_schema)
+    job = client.load_table_from_json(json_object, tableT, job_config = job_config_temp)
+    print(job.result())
+    delete_duplicate()
 
 def get_params():
-    print("Cargando consulta")
     for x in range(1):
         init.registro += 1
         get_order_list(init.registro)
-    run()
+
 
 get_params()
