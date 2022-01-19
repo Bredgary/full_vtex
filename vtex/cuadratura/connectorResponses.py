c344f4c45537f84299a176190302e212f49b5e7b
bredg <bred_valenzuela@gmail.com>
2022-01-19 13:26:13 -0300

    MOD: prueba

--- vtex/cuadratura/connectorResponses.py
+++ vtex/cuadratura/connectorResponses.py
@@ -47,7 +47,7 @@ def get_order(id,reg):
                     'C_orderId': orderId,
                     'state': state}, index=[0])
                 init.df = init.df.append(df1)
-            print("Registro: "+str(reg))
+                print("Registro: "+str(reg))
     except:
         print("No data. "+str(reg))
         
