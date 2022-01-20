da186e215be8287197be08a36378c8735447ad2b
bredg <bred_valenzuela@gmail.com>
2022-01-20 00:06:22 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/descomposicion_orders/shippingData_selectedAddresses.py
+++ vtex/orders_api/ORDERS/descomposicion_orders/shippingData_selectedAddresses.py
@@ -93,8 +93,8 @@ def run():
             dataset  = client.dataset(dataset_id)
             table = dataset.table(table_id)
             job_config = bigquery.LoadJobConfig()
-            job_config.write_disposition = "WRITE_TRUNCATE"
-            job_config.autodetect = True
+            #job_config.write_disposition = "WRITE_TRUNCATE"
+            #job_config.autodetect = True
             job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
             job = client.load_table_from_json(json_object, table, job_config = job_config)
             print(job.result())
@@ -114,6 +114,66 @@ def get_params():
         registro += 1
         get_order(row.orderId)
         print("Registro: "+str(registro))
+        if registro == 300:
+            run()
+        if registro == 400:
+            run()
+        if registro == 500:
+            run()
+        if registro == 600:
+            run()
+        if registro == 700:
+            run()
+        if registro == 800:
+            run()
+        if registro == 900:
+            run()
+        if registro == 1000:
+            run()
+        if registro == 1100:
+            run()
+        if registro == 1200:
+            run()
+        if registro == 1300:
+            run()
+        if registro == 1400:
+            run()
+        if registro == 1500:
+            run()
+        if registro == 10000:
+            run()
+        if registro == 15000:
+            run()
+        if registro == 20000:
+            run()
+        if registro == 25000:
+            run()
+        if registro == 30000:
+            run()
+        if registro == 35000:
+            run()
+        if registro == 40000:
+            run()
+        if registro == 45000:
+            run()
+        if registro == 50000:
+            run()
+        if registro == 55000:
+            run()
+        if registro == 60000:
+            run()
+        if registro == 65000:
+            run()
+        if registro == 70000:
+            run()
+        if registro == 75000:
+            run()
+        if registro == 80000:
+            run()
+        if registro == 85000:
+            run()
+        if registro == 90000:
+            run()
     run()
 
 get_params()
