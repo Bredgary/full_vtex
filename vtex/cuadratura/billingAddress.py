4e11244b9d38d28848d95fd1aeae5f3414a42299
bredg <bred_valenzuela@gmail.com>
2022-01-19 22:56:30 -0300

    MOD: prueba

--- vtex/cuadratura/billingAddress.py
+++ vtex/cuadratura/billingAddress.py
@@ -10,22 +10,11 @@ from os.path import join
 class init:
     productList = []
     df = pd.DataFrame()
-    postalCode = None
-    city = None
-    state = None
-    country = None
-    street = None
-    number = None
-    neighborhood = None
-    complement = None
-    reference = None
-    lat = None
-    lon = None
     
     headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
     
 def get_order(id,reg):
-    try:
+    #try:
         reg +=1
         url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
         response = requests.request("GET", url, headers=init.headers)
@@ -64,9 +53,9 @@ def get_order(id,reg):
                     'lat': lat}, index=[0])
                 init.df = init.df.append(df1)
         print("Registro: "+str(reg))
-    except:
-        print("Vacio")
-        print("Registro: "+str(reg))
+    #except:
+    #    print("Vacio")
+    #    print("Registro: "+str(reg))
         
         
 def get_params():
