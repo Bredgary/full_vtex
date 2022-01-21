baf1fb44e14ffa82a5055997e1a3624c40c335e3
bredg <bred_valenzuela@gmail.com>
2022-01-21 05:51:32 -0300

    MOD: prueba

--- vtex/entity/CL/Client_History.py
+++ vtex/entity/CL/Client_History.py
@@ -115,6 +115,7 @@ def get_order(email,reg):
                 'lastInteractionBy':lastInteractionBy,
                 'lastInteractionIn':lastInteractionIn}, index=[0])
             init.df = init.df.append(df1)
+            print(init.df)
         print("Registro: "+str(reg))
     except:
         print("No data "+str(reg))
