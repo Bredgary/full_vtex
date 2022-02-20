0d4fa4a62524197c4397c154fc810bc2f3fe43ac
bredg <bred_valenzuela@gmail.com>
2022-02-20 19:31:02 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/test.py
+++ vtex/orders_api/ORDERS/test.py
@@ -43,7 +43,11 @@ def get_order(id):
             item_imageUrl = x["imageUrl"]
             item_detailUrl = x["detailUrl"]
             item_sellerSku = x["sellerSku"]
-            item_priceValidUntil = x["priceValidUntil"]
+            if x["priceValidUntil"] is not None:
+                item_priceValidUntil = x["priceValidUntil"]
+            else:
+                item_priceValidUntil = '1900-01-01 15:15:18.051893 UTC'
+            
             print(item_priceValidUntil)
             item_commission = x["commission"]
             item_tax = x["tax"]
