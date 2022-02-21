54cd4b00006b5188376ad217a4952d5c97122272
bredg <bred_valenzuela@gmail.com>
2022-02-20 23:23:49 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/test.py
+++ vtex/orders_api/ORDERS/test.py
@@ -186,7 +186,7 @@ def run():
 def get_params():
     print("Cargando consulta")
     client = bigquery.Client()
-    QUERY = ('SELECT orderId  FROM `shopstar-datalake.staging_zone.order_write`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.staging_zone.shopstar_order_billingAddress`))')
+    QUERY = ('SELECT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.staging_zone.shopstar_order_billingAddress`))')
     query_job = client.query(QUERY)
     rows = query_job.result()
     registro = 0
