7e9b7a46d2a874aae1b39667e4f9628bc255b265
bredg <bred_valenzuela@gmail.com>
2022-02-28 13:10:56 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/test.py
+++ vtex/orders_api/ORDERS/test.py
@@ -4,74 +4,142 @@ from google.cloud import bigquery
 import os, json
 from datetime import datetime
 import requests
-from datetime import datetime, timezone
 from os.path import join
 import logging
 
 class init:
-    productList = []
-    df = pd.DataFrame()
-    
-    headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+  df = pd.DataFrame()
+  headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
 
-def format_schema(schema):
-    formatted_schema = []
-    for row in schema:
-        formatted_schema.append(bigquery.SchemaField(row['name'], row['type'], row['mode']))
-    return formatted_schema
-
-def get_order(id,reg):
+def get_inventory(id):
     try:
-        df1 = pd.DataFrame
-        url = "https://mercury.vtexcommercestable.com.br/api/oms/pvt/orders/"+str(id)+""
-        response = requests.request("GET", url, headers=init.headers)
-        Fjson = json.loads(response.text)
-        paymentData = Fjson["paymentData"]
-        transactions = paymentData["transactions"]
-        for x in transactions:
-            payments = x["payments"]
-            for x in payments:
-                connectorResponses = x["connectorResponses"]
-                Tid = connectorResponses["Tid"]
-                ReturnCode= connectorResponses["ReturnCode"]
-                Message= connectorResponses["Message"]
-                try:
-                    authId= connectorResponses["authId"]
-                    orderId= connectorResponses["orderId"]
-                except:
-                    authId= None
-                    orderId= None
-                state= connectorResponses["state"]
-                
-                df1 = pd.DataFrame({
-                    'orderId': id,
-                    'Tid': Tid,
-                    'ReturnCode': ReturnCode,
-                    'Message': Message,
-                    'authId': authId,
-                    'C_orderId': orderId,
-                    'state': state}, index=[0])
-                init.df = init.df.append(df1)
-        if df1.empty:
-            df1 = pd.DataFrame({
-                'orderId': id}, index=[0])
-            init.df = init.df.append(df1)
-    except:
-        df1 = pd.DataFrame({
-            'orderId': id}, index=[0])
-        init.df = init.df.append(df1)
-        
-def delete_duplicate():
-    try:
-        print("Eliminando duplicados")
+        print("Cargando consulta")
         client = bigquery.Client()
-        QUERY = (
-            'CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_order_connectorResponses` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_order_connectorResponses`')
+        QUERY = ('SELECT id  FROM `shopstar-datalake.staging_zone.shopstar_vtex_sku`')
         query_job = client.query(QUERY)
         rows = query_job.result()
-        print(rows)
+        registro = 0
+        for row in rows:
+            url = "https://mercury.vtexcommercestable.com.br/api/logistics/pvt/inventory/skus/"+str(row.id)+""
+            response = requests.request("GET", url, headers=init.headers)
+            Fjson = json.loads(response.text)
+            balance = Fjson["balance"]
+            for x in balance:
+                warehouseId = x["warehouseId"]
+                warehouseName = x["warehouseName"]
+                totalQuantity = x["totalQuantity"]
+                reservedQuantity = x["reservedQuantity"]
+                hasUnlimitedQuantity = x["hasUnlimitedQuantity"]
+                timeToRefill = x["timeToRefill"]
+                dateOfSupplyUtc = x["dateOfSupplyUtc"]
+                
+                df1 = pd.DataFrame({
+                    'SKU_ID': id,
+                    'warehouseId': warehouseId,
+                    'warehouseName': warehouseName,
+                    'totalQuantity': totalQuantity,
+                    'reservedQuantity': reservedQuantity,
+                    'hasUnlimitedQuantity': hasUnlimitedQuantity,
+                    'timeToRefill': timeToRefill,
+                    'dateOfSupplyUtc': dateOfSupplyUtc}, index=[0])
+                init.df = init.df.append(df1)
+                registro += 1
+                print("Registro: "+str(registro))
+                if registro == 1000:
+                    run()
+                if registro == 2000:
+                    run()
+                if registro == 3000:
+                    run()
+                if registro == 4000:
+                    run()
+                if registro == 5000:
+                    run()
+                if registro == 6000:
+                    run()
+                if registro == 7000:
+                    run()
+                if registro == 8000:
+                    run()
+                if registro == 9000:
+                    run()
+                if registro == 9500:
+                    run()
+                if registro == 10000:
+                    run()
+                if registro == 20000:
+                    run()
+                if registro == 30000:
+                    run()
+                if registro == 40000:
+                    run()
+                if registro == 50000:
+                    run()
+                if registro == 60000:
+                    run()
+                if registro == 70000:
+                    run()
+                if registro == 80000:
+                    run()
+                if registro == 90000:
+                    run()
+                if registro == 100000:
+                    run()
+                if registro == 110000:
+                    run()
+                if registro == 120000:
+                    run()
+                if registro == 130000:
+                    run()
+                if registro == 140000:
+                    run()
+                if registro == 150000:
+                    run()
+                if registro == 160000:
+                    run()
+                if registro == 170000:
+                    run()
+                if registro == 180000:
+                    run()
+                if registro == 190000:
+                    run()
+                if registro == 200000:
+                    run()
+                if registro == 210000:
+                    run()
+                if registro == 220000:
+                    run()
+                if registro == 230000:
+                    run()
+                if registro == 240000:
+                    run()
+                if registro == 250000:
+                    run()
+                if registro == 260000:
+                    run()
+                if registro == 270000:
+                    run()
+                if registro == 280000:
+                    run()
+                if registro == 290000:
+                    run()
+                if registro == 300000:
+                    run()
+            run()
     except:
-        print("Consulta SQL no ejecutada")
+        print("Vacio")
+
+
+def delete_duplicate():
+  try:
+    print("Eliminando duplicados")
+    client = bigquery.Client()
+    QUERY = ('CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_vtex_list_inventory_by_sku` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_inventory_by_sku`')
+    query_job = client.query(QUERY)
+    rows = query_job.result()
+    print(rows)
+  except:
+    print("Consulta SQL no ejecutada")
 
 
 def run():
@@ -81,87 +149,24 @@ def run():
         json_data = df.to_json(orient = 'records')
         json_object = json.loads(json_data)
         
-        table_schema = [{
-            "name": "state",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "C_orderId",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "Tid",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "authId",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "Message",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "ReturnCode",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        },{
-            "name": "orderId",
-            "type": "STRING",
-            "mode": "NULLABLE"
-        }]    
-        
         project_id = '999847639598'
         dataset_id = 'staging_zone'
-        table_id = 'shopstar_order_connectorResponses'
+        table_id = 'shopstar_vtex_list_inventory_by_sku2'
         
         if df.empty:
             print('DataFrame is empty!')
         else:
-            try:
-                client  = bigquery.Client(project = project_id)
-                dataset  = client.dataset(dataset_id)
-                table = dataset.table(table_id)
-                job_config = bigquery.LoadJobConfig()
-                job_config.schema = format_schema(table_schema)
-                job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-                job = client.load_table_from_json(json_object, table, job_config = job_config)
-                print(job.result())
-                delete_duplicate()
-            except:
-                client  = bigquery.Client(project = project_id)
-                dataset  = client.dataset(dataset_id)
-                table = dataset.table(table_id)
-                job_config = bigquery.LoadJobConfig()
-                job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
-                job = client.load_table_from_json(json_object, table, job_config = job_config)
-                print(job.result())
-                delete_duplicate()
+            client  = bigquery.Client(project = project_id)
+            dataset  = client.dataset(dataset_id)
+            table = dataset.table(table_id)
+            job_config = bigquery.LoadJobConfig()
+            write_disposition = "WRITE_TRUNCATE"
+            job_config.autodetect = True
+            job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+            job = client.load_table_from_json(json_object, table, job_config = job_config)
+            print(job.result())
+            delete_duplicate()
     except:
         print("Error.")
         logging.exception("message")
 
-def get_params():
-    print("Cargando consulta")
-    client = bigquery.Client()
-    QUERY = ('SELECT orderId  FROM `shopstar-datalake.staging_zone.shopstar_vtex_list_order`WHERE (orderId NOT IN (SELECT orderId FROM `shopstar-datalake.staging_zone.shopstar_order_connectorResponses`))')
-    query_job = client.query(QUERY)  
-    rows = query_job.result()
-    registro = 0
-    for row in rows:
-        registro += 1
-        get_order(row.orderId,registro)
-        print("Registro: "+str(registro))
-        if registro == 10:
-            run()
-        if registro == 20:
-            run()
-        if registro == 30:
-            run()
-        if registro == 40:
-            run()
-        if registro == 50:
-            run()
-    run()
-    
-get_params()
