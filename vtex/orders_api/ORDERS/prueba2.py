f4fabaca4ce7b2a2f4360825ae9433b324ac08f2
bredg <bred_valenzuela@gmail.com>
2022-03-01 12:39:16 -0300

    MOD: prueba

---
+++ vtex/orders_api/ORDERS/prueba2.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+from google.cloud import bigquery
+import os, json
+from datetime import datetime
+import requests
+from os.path import join
+import logging
+
+class init:
+  df = pd.DataFrame()
+  headers = {"Content-Type": "application/json","Accept": "application/json","X-VTEX-API-AppKey": "vtexappkey-mercury-PKEDGA","X-VTEX-API-AppToken": "OJMQPKYBXPQSXCNQHWECEPDPMNVWAEGFBKKCNRLANUBZGNUWAVLSCIPZGWDCOCBTIKQMSLDPKDOJOEJZTYVFSODSVKWQNJLLTHQVWHEPRVHYTFLBNEJPGWAUHYQIPMBA"}
+
+def get_stores():
+    try:
+        url = "https://mercury.vtexcommercestable.com.br/api/vlm/account/stores/"
+        response = requests.request("GET", url, headers=init.headers)
+        Fjson = json.loads(response.text)
+        for x in Fjson:
+            id = x["id"]
+            name = x["name"]
+            hosts = x["hosts"]
+            df1 = pd.DataFrame({
+                'id': id,
+                'name': name,
+                'hosts': hosts}, index=[0])
+            init.df = init.df.append(df1)
+        run()
+    except:
+        print("Error.")
+        logging.exception("message")
+
+
+def delete_duplicate():
+  try:
+    print("Eliminando duplicados")
+    client = bigquery.Client()
+    QUERY = ('CREATE OR REPLACE TABLE `shopstar-datalake.staging_zone.shopstar_vtex_product_store` AS SELECT DISTINCT * FROM `shopstar-datalake.staging_zone.shopstar_vtex_product_store`')
+    query_job = client.query(QUERY)
+    rows = query_job.result()
+    print(rows)
+  except:
+    print("Consulta SQL no ejecutada")
+
+
+def run():
+    try:
+        df = init.df
+        df.reset_index(drop=True, inplace=True)
+        json_data = df.to_json(orient = 'records')
+        json_object = json.loads(json_data)
+        
+        project_id = '999847639598'
+        dataset_id = 'staging_zone'
+        table_id = 'shopstar_vtex_product_store'
+        
+        if df.empty:
+            print('DataFrame is empty!')
+        else:
+            client  = bigquery.Client(project = project_id)
+            dataset  = client.dataset(dataset_id)
+            table = dataset.table(table_id)
+            job_config = bigquery.LoadJobConfig()
+            write_disposition = "WRITE_TRUNCATE"
+            job_config.autodetect = True
+            job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+            job = client.load_table_from_json(json_object, table, job_config = job_config)
+            print(job.result())
+            delete_duplicate()
+    except:
+        print("Error.")
+        logging.exception("message")
+
+get_stores()
