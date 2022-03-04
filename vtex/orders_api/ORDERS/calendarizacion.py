d914bc925268ebdb2ae23dd95b90e6b28df4bcd2
bredg <bred_valenzuela@gmail.com>
2022-03-04 00:57:51 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/calendarizacion.py
+++ vtex/orders_api/ORDERS/calendarizacion.py
@@ -32,6 +32,7 @@ def get_product_cal():
         rows = query_job.result()
         registro = 0
         for row in rows:
+            print(row.productId)
             url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/products/productget/1"+str(row.productId)+""
             response = requests.request("GET", url, headers=init.headers)
             Fjson = json.loads(response.text)
