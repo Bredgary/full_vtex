fdbd7815abc59783e1288d889e9049e35f77c6ce
bredg <bred_valenzuela@gmail.com>
2022-03-07 16:19:52 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/calendarizacion.py
+++ vtex/orders_api/ORDERS/calendarizacion.py
@@ -21,7 +21,7 @@ def salespolicy():
     try:
         print("Cargando consulta")
         client = bigquery.Client()
-        QUERY = ('SELECT distinct productId, lastChange FROM `shopstar-datalake.staging_zone.shopstar_order_items` WHERE lastChange BETWEEN "'+str(init.year)+'-'+str(init.month)+'-'+str(init.day)+' 00:00:00" AND "'+str(init.year)+'-'+str(init.month)+'-'+str(init.day)+' 06:00:00"')
+        QUERY = ('SELECT distinct productId, lastChange FROM `shopstar-datalake.staging_zone.shopstar_order_items` WHERE lastChange BETWEEN "'+str(init.year)+'-'+str(init.month)+'-'+str(init.day)+' 12:00:00" AND "'+str(init.year)+'-'+str(init.month)+'-'+str(init.day)+' 18:00:00"')
         query_job = client.query(QUERY)
         rows = query_job.result()
         registro = 0
