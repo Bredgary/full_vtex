5e3fa154ec9051fa13df0d4b1685e569793932f0
bredg <bred_valenzuela@gmail.com>
2022-03-08 05:23:36 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/calendarizacion.py
+++ vtex/orders_api/ORDERS/calendarizacion.py
@@ -37,15 +37,11 @@ def product_specification():
                 init.df = init.df.append(df1)
                 registro += 1
                 print("Registro: "+str(registro))
-        #run()
+        run()
                 
     except:
-        df1 = pd.DataFrame({
-            'id': x["Id"],
-            'name': x["Name"],
-            'value': x["Value"]}, index=[0])
-        init.df = init.df.append(df1)
-        #run()
+        print("Error.")
+        logging.exception("message")
 
 def format_schema(schema):
     formatted_schema = []
@@ -60,7 +56,7 @@ def delete_duplicate():
     query_job = client.query(QUERY)  
     rows = query_job.result()
     print(rows)
-'''
+
 def run():
     try:
         df = init.df
@@ -114,4 +110,3 @@ def run():
         logging.exception("message")
 
 product_specification()
-'''
