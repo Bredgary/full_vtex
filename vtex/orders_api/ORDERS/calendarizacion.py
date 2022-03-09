91e6db696b6ae38833aa08c2f058353a26b976d9
bredg <bred_valenzuela@gmail.com>
2022-03-09 10:12:00 -0300

    }MOD: prueba

--- vtex/orders_api/ORDERS/calendarizacion.py
+++ vtex/orders_api/ORDERS/calendarizacion.py
@@ -31,7 +31,7 @@ def sku():
             url = "https://mercury.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitByProductId/"+str(row.productId)+""
             response = requests.request("GET", url, headers=init.headers)
             Fjson = json.loads(response.text)
-            if Fjson is not 'Sku não encontrado.':
+            if Fjson is 'Sku não encontrado.':
                 for x in Fjson:
                     IsPersisted = x["IsPersisted"]
                     Id = x["Id"]
