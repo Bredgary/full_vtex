fe5a4300534c1f1e96c2b95fcf435004d0fddd1a
bredg <bred_valenzuela@gmail.com>
2022-03-11 00:01:17 -0300

    MOD: prueba

--- vtex/orders_api/ORDERS/calendarizacion.py
+++ vtex/orders_api/ORDERS/calendarizacion.py
@@ -127,7 +127,6 @@ def sku():
                         'SkuSellers_IsActive': SkuSellers_IsActive,
                         'SkuSellers_SellerStockKeepingUnitId': SkuSellers_SellerStockKeepingUnitId,
                         'Attachments_Name': Attachments_Name,
-                        'dimension_height': None,
                         'RealDimension_realHeight': RealDimension_realHeight,
                         'ProductName': ProductName,
                         'ImageUrl': ImageUrl,
