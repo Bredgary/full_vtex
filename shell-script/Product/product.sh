03882424ed92d9b5891addd60110e5ddd25d3132
bredg <bred_valenzuela@gmail.com>
2022-05-06 03:46:50 -0400

    MOD: prueba

---
+++ shell-script/Product/product.sh
@@ -0,0 +1,10 @@
+python3 /home/bred_valenzuela/full_vtex/vtex/catalog_api/CATEGORY/Get_Category_Tree.py
+echo "Carga Categoria Finalizado"
+
+python3 /home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/Get_Product_and_SKU_IDs.py
+echo "Carga Temporal ID API Product Finalizado"
+
+python3 /home/bred_valenzuela/full_vtex/vtex/catalog_api/PRODUCT/Get_Product_by_ID.py.py
+echo "Carga API Product Finalizado"
+
+echo "Finalizado"
