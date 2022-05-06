c3dc75c66fbc1c64aed0f453c690e6cbd8c0e4bb
bredg <bred_valenzuela@gmail.com>
2022-05-06 01:25:42 -0400

    MOD: prueba

--- shell-script/orders_api.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/bred_valenzuela/full_vtex/vtex/orders_api/ORDERS/
-python3 List_Order.py #Se implemento list order y get order en la misma funcion
-#python3 /home/bred_valenzuela/full_vtex/stop_compute_engine.py 
-echo "Finalizado"
